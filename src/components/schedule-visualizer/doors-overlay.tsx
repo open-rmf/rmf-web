ebfc28d6ed2931027b2512cb27ac0074ae6b1291
Matias Bavera <matiasbavera@gmail.com>
2020-05-06 18:49:37 -0400

    Fix leftovers

--- src/components/schedule-visualizer/doors-overlay.tsx
+++ src/components/schedule-visualizer/doors-overlay.tsx
@@ -1,14 +1,12 @@
 /**
  * TODO: Need door location, (v1_x, v1_y) only defines the location of the hinge, we also need the
- * length and orientationo of the door to draw it on the map.
+ * length and orientation of the door to draw it on the map.
  */
 
 import * as RomiCore from '@osrf/romi-js-core-interfaces';
-// import * as L from 'leaflet';
 import React from 'react';
 import { viewBoxFromLeafletBounds } from '../../util/css-utils';
 import SVGOverlay, { SVGOverlayProps } from './svg-overlay';
-import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Door from './door';
 export interface DoorsOverlayProps extends SVGOverlayProps {
@@ -19,10 +17,10 @@ export interface DoorsOverlayProps extends SVGOverlayProps {
 export default function DoorsOverlay(props: DoorsOverlayProps): React.ReactElement {
   const { doors, onDoorClick, ...otherProps } = props;
   const viewBox = viewBoxFromLeafletBounds(props.bounds);
+
   return (
     <>
       <SVGOverlay {...otherProps}>
-        <CircularProgress color="secondary"></CircularProgress>
         <svg viewBox={viewBox}>
           {doors.map(door => (
             <Door
