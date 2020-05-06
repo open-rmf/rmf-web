0149a2aee2bfb4acaa09feacf3798de5ba6c4b65
Matias Bavera <matiasbavera@gmail.com>
2020-05-06 17:28:17 -0400

    Modify doors positions, Add doors styles, added DobleHingeDoor component

--- src/components/schedule-visualizer/door.tsx
+++ src/components/schedule-visualizer/door.tsx
@@ -4,9 +4,9 @@ import React, { useContext } from 'react';
 import { DoorStateContext } from '../app';
 import { Spinner } from './spinner';
 
-const OPEN = 0;
+const CLOSE = 0;
 const PROCESS = 1;
-const CLOSE = 2;
+const OPEN = 2;
 
 // Door Type
 const DOOR_TYPE_SINGLE_SLIDING = 1;
@@ -14,90 +14,77 @@ const DOOR_TYPE_DOUBLE_SLIDING = 2;
 export interface DoorProps {
   door: RomiCore.Door;
   onClick?(e: React.MouseEvent<SVGGElement>, place: RomiCore.Door): void;
+  currentMode?: number;
 }
 
-class DoorManager {
-  static getNewCoords(v1: number[], v2: number[], angle: number, direction: number, process = 0) {
-    const [x1, y1] = v1;
-    const distance = this.getDistance(v1, v2);
-    const initialAngle = this.getAngleBetweenTwoPointsDegrees(v1, v2);
-    const coordX = x1 + distance * Math.cos(initialAngle + angle * direction * process);
-    const coordY = y1 + distance * Math.sin(initialAngle + angle * direction * process);
-    return [coordX, coordY];
-  }
-
-  static getDistance(v1: number[], v2: number[]) {
-    const [x1, y1] = v1;
-    const [x2, y2] = v2;
-    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
-  }
-
-  static getAngleBetweenTwoPointsDegrees(v1: number[], v2: number[]) {
-    const [x1, y1] = v1;
-    const [x2, y2] = v2;
-    return (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
-  }
-
-  static getAngleBetweenTwoPointsRadians(v1: number[], v2: number[]) {
-    const [x1, y1] = v1;
-    const [x2, y2] = v2;
-    return Math.atan2(y2 - y1, x2 - x1);
-  }
-}
+const getDoorStyle = (classes: any, currentMode: number | undefined) => {
+  return currentMode === OPEN
+    ? classes.doorOpen
+    : currentMode === PROCESS
+    ? classes.doorProcess
+    : classes.doorClose;
+};
 
 const SingleHingeDoor = React.forwardRef(function(
   props: DoorProps,
   ref: React.Ref<SVGGElement>,
 ): React.ReactElement {
-  const { door, onClick } = props;
-  const { motion_range, motion_direction, v1_x, v1_y, v2_x, v2_y } = door;
+  const { door, onClick, currentMode } = props;
+  const { v1_x, v1_y, v2_x, v2_y } = door;
   const [hingeX, hingeY, extendX, extendY] = [v1_x, v1_y, v2_x, v2_y];
-
   const classes = useStyles();
-  const useStateValue = () => useContext(DoorStateContext);
-
-  console.log(props);
-  console.log(useStateValue());
-
-  // Door state
-  const doorState = useStateValue() as any;
-  const currentDoor = doorState && doorState[door.name];
-  let currentMode = OPEN;
-  let fillColor = 'brown';
-  let doorTime = 0;
-  let newCoordX: number | undefined, newCoordY: number | undefined;
-
-  if (currentDoor) {
-    doorTime = currentDoor.door_time;
-    currentMode = currentDoor.current_mode.value;
-    fillColor = currentMode === OPEN ? 'brown' : currentMode === PROCESS ? 'blue' : 'red';
-    [newCoordX, newCoordY] = DoorManager.getNewCoords(
-      [hingeX, hingeY],
-      [extendX, extendY],
-      motion_range,
-      motion_direction,
-      doorTime,
-    );
-  }
 
   return (
     <>
       <g ref={ref} onClick={e => onClick && onClick(e as any, door)}>
         <line
-          className={classes.doorMarker}
+          className={`${classes.doorMarker} ${classes.door} ${getDoorStyle(classes, currentMode)}`}
           x1={hingeX}
           y1={-hingeY}
-          x2={newCoordX || extendX}
-          y2={(newCoordY || extendY) * -1}
-          stroke={fillColor}
-          stroke-width="0.2"
+          x2={extendX}
+          y2={-extendY}
         />
       </g>
-      {currentMode === PROCESS && (
-        <g>
-          <Spinner cx={hingeX} cy={-hingeY} r={0.4} strokeWidth={0.1}></Spinner>
-        </g>
-      )}
+    </>
+  );
+});
+
+/**
+ * Double hinge doors:
+ * - hinges are located at both (v1_x, v1_y) and (v2_x, v2_y)
+ * - motion range = door swing ranges in DEGREES (assume symmetric)
+ * - same motion-direction selection as single hinge
+ */
+const DoubleHingeDoor = React.forwardRef(function(
+  props: DoorProps,
+  ref: React.Ref<SVGGElement>,
+): React.ReactElement {
+  const { door, onClick, currentMode } = props;
+  const { v1_x, v1_y, v2_x, v2_y } = door;
+  const [hingeX1, hingeY1, hingeX2, hingeY2] = [v1_x, v1_y, v2_x, v2_y];
+  const [extendX1, extendY1] = [hingeX1 + (v2_x - v1_x) / 2, hingeY1 + (v2_y - v1_y) / 2];
+  const classes = useStyles();
+
+  return (
+    <>
+      <g ref={ref} onClick={e => onClick && onClick(e as any, door)}>
+        <line
+          className={`${classes.doorMarker} ${classes.door} ${getDoorStyle(classes, currentMode)}`}
+          x1={hingeX1}
+          y1={-hingeY1}
+          x2={extendX1}
+          y2={-extendY1}
+        />
+      </g>
+      <g ref={ref} onClick={e => onClick && onClick(e as any, door)}>
+        <line
+          className={`${classes.doorMarker} ${classes.door} ${getDoorStyle(classes, currentMode)}`}
+          x1={extendX1}
+          y1={-extendY1}
+          x2={hingeX2}
+          y2={-hingeY2}
+        />
+      </g>
     </>
   );
 });
@@ -108,22 +95,24 @@ const Door = React.forwardRef(function(
 ): React.ReactElement {
   const { door, onClick } = props;
   const { door_type: doorType } = door;
-  // Door extends till
-
-  // const x = Math.min(door.v1_x, door.v2_x);
-  // const y = -1 * Math.min(door.v1_y, door.v2_y);
-  // const maxX = Math.max(door.v2_x, door.v1_x);
-  // const maxY = Math.max(door.v2_y, door.v1_y);
-  // const width = Math.abs(door.v2_x - door.v1_x);
-  // const height = Math.abs(door.v2_y - door.v1_y);
-
+  // Door state
+  const useStateValue = () => useContext(DoorStateContext);
+  const doorState = useStateValue() as any;
+  const currentDoor = doorState && doorState[door.name];
+  const currentMode = currentDoor && currentDoor.current_mode.value;
+  console.log(doorState);
   return (
     <>
       {doorType === DOOR_TYPE_SINGLE_SLIDING && (
-        <SingleHingeDoor door={door} onClick={onClick}></SingleHingeDoor>
+        <SingleHingeDoor door={door} onClick={onClick} currentMode={currentMode} />
       )}
       {doorType === DOOR_TYPE_DOUBLE_SLIDING && (
-        <SingleHingeDoor door={door} onClick={onClick}></SingleHingeDoor>
+        <DoubleHingeDoor door={door} onClick={onClick} currentMode={currentMode} />
+      )}
+      {currentMode === PROCESS && (
+        <g>
+          <Spinner cx={door.v1_x} cy={-door.v1_y} r={0.4} strokeWidth={0.1}></Spinner>
+        </g>
       )}
     </>
   );
@@ -135,5 +124,19 @@ const useStyles = makeStyles(() => ({
   doorMarker: {
     cursor: 'pointer',
     pointerEvents: 'auto',
+  },
+  door: {
+    strokeWidth: '0.2',
+  },
+  doorOpen: {
+    stroke: '#33CC33',
+    strokeDasharray: 0.1,
+  },
+  doorClose: {
+    stroke: 'red',
+  },
+  doorProcess: {
+    stroke: 'blue',
+    strokeDasharray: 0.3,
   },
 }));
