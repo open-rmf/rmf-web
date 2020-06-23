27e28189a3c317aafff51a1b35b83b8b532cbd78
koonpeng <koonpeng@openrobotics.org>
2020-06-23 11:25:20 +0800

    workaround click missing due to animation

--- e2e/tests/doors.test.ts
+++ e2e/tests/doors.test.ts
@@ -1,4 +1,5 @@
 import { RmfLauncher } from '../rmf-launcher';
+import { overwriteClick } from './utils';
 
 describe('door tests', () => {
   const doorName = 'main_door';
@@ -7,6 +8,7 @@ describe('door tests', () => {
   before(async () => await launcher.launch());
   after(async () => await launcher.kill());
 
+  before(() => overwriteClick());
   before(() => browser.url('/'));
 
   it('clicking door on the map focuses it on the panel', () => {
@@ -21,9 +23,18 @@ describe('door tests', () => {
 
     expect(doorItem.$('span=CLOSED')).toBeVisible();
 
-    const openButton = doorItem.$('button=Open');
-    openButton.click();
+    doorItem.$('button=Open').click();
 
     expect(doorItem.$('span=OPEN')).toBeVisible();
+  });
+
+  it('clicking on close button closes the door', () => {
+    const doorItem = $(`#DoorItem*=${doorName}`);
+
+    expect(doorItem.$('span=OPEN')).toBeVisible();
+
+    doorItem.$('button=Close').click();
+
+    expect(doorItem.$('span=CLOSED')).toBeVisible();
   });
 });
---
+++ e2e/tests/utils.ts
@@ -0,0 +1,23 @@
+import { Element } from '@wdio/sync';
+
+/**
+ * Overwrites the default click command to wait for animation to finish before attempting to click,
+ * this can fix flaky tests where the click is missed as the position changes as the animation is
+ * running.
+ */
+export function overwriteClick() {
+  browser.overwriteCommand(
+    'click',
+    function(this: Element, origClick) {
+      let prevLocation = this.getLocation();
+      this.waitUntil(() => {
+        const newLocation = this.getLocation();
+        const stablized = prevLocation.x === newLocation.x && prevLocation.y === newLocation.y;
+        prevLocation = newLocation;
+        return stablized;
+      });
+      return origClick();
+    },
+    true,
+  );
+}
