4643c3d2590e828253dcf603bc39955c5ee99d5f
vallq <vallq@outlook.com>
2020-07-09 15:13:15 +0800

    edit form test to remove places props

--- src/components/tests/loop-form.test.tsx
+++ src/components/tests/loop-form.test.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const mount = createMount();
 
-const buildWrapper = (listOfPlaces?: string[]) => {
+const buildWrapper = (fleetName: string) => {
   const onClick = (
     fleetName: string,
     numLoops: number,
@@ -15,19 +15,13 @@ const buildWrapper = (listOfPlaces?: string[]) => {
     console.log('test');
   };
 
-  const wrapper = mount(
-    <LoopForm
-      requestLoop={onClick}
-      fleets={['SuperFleet']}
-      listOfPlaces={!!listOfPlaces ? listOfPlaces : fakePlaces()['SuperFleet']}
-    />,
-  );
+  const wrapper = mount(<LoopForm requestLoop={onClick} fleets={[fleetName]} />);
   return wrapper;
 };
 
 describe('form Validation', () => {
   test('Initial Values', () => {
-    const wrapper = buildWrapper();
+    const wrapper = buildWrapper('SuperFleet');
     expect(wrapper.find("input[type='number']").props().value).toEqual('');
     expect(wrapper.findWhere(x => x.name() === 'input' && x.props().value === 'supplies'))
       .toBeTruthy;
@@ -46,27 +40,27 @@ describe('form Validation', () => {
   });
 
   test('Number of loops cannot be empty', async () => {
-    const wrapper = buildWrapper();
+    const wrapper = buildWrapper('SuperFleet');
     wrapper.find('form').simulate('submit');
     expect(wrapper.find('#numLoopsError').html()).toContain('Loops can only be &gt; 0');
     wrapper.unmount();
   });
 
   test('Location cannot be empty', async () => {
-    const wrapper = buildWrapper(['placeA']);
+    const wrapper = buildWrapper('FleetA');
     wrapper.find('form').simulate('submit');
     expect(wrapper.find('#startLocationError').html()).toContain('Location cannot be empty');
     wrapper.unmount();
   });
 
   test('Start Location cannot be equal to finish Location', async () => {
-    const wrapper = buildWrapper(['placeA', 'placeA', 'placeA']);
+    const wrapper = buildWrapper('FleetB');
     wrapper.find('form').simulate('submit');
     expect(wrapper.find('#startLocationError').html()).toContain(
-      'Start Location cannot be equal to finish Location',
+      'Start Location cannot be equal to Finish Location',
     );
     expect(wrapper.find('#finishLocationError').html()).toContain(
-      'Start Location cannot be equal to finish Location',
+      'Start Location cannot be equal to Finish Location',
     );
     wrapper.unmount();
   });
