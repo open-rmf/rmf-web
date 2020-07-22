2483b97cf7afee19961a0375090e89bbc950200d
ChawinTan <chawin15@gmail.com>
2020-07-22 14:08:05 +0800

    Initialize robot component

---
+++ src/stories/robot-component.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as RomiCore from '@osrf/romi-js-core-interfaces';
+
+import Robot from '../components/schedule-visualizer/robot';
+import ColorManager from '../components/schedule-visualizer/colors';
+
+export interface RobotComponentProps {
+    robot: RomiCore.RobotState;
+    footprint: number;
+    colorManager: ColorManager;
+    renderInfoPanel(): JSX.Element;
+}
+
+const styles = {
+    display: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 3fr'
+    }
+}
+
+export default function RobotComponent(props: RobotComponentProps): React.ReactElement {
+    
+    const { robot, footprint, colorManager, renderInfoPanel } = props;
+
+    return (
+        <div style={styles.display}>
+            {renderInfoPanel()}
+
+            <svg viewBox={'0 0 25.794363144785166 14.53525484725833'}>  
+                <Robot
+                    robot={robot}
+                    footprint={footprint}
+                    colorManager={colorManager}
+                />
+            </svg>
+        </div>
+    );
+}
---
+++ src/stories/robot.stories.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import * as RomiCore from '@osrf/romi-js-core-interfaces';
+import { Divider, Typography } from '@material-ui/core';
+
+import RobotComponent from './robot-component';
+import ColorManager from '../components/schedule-visualizer/colors';
+
+
+export default {
+    title: 'Robot',
+ };
+
+const styles = {
+    modeInfoPanel: {
+        padding: '2rem'
+    },
+    modeInfoItem: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '0.5rem'
+    },
+    modeInfoLink: {
+       marginTop: '0.5rem'
+   }
+};
+
+ const robotState = {
+    battery_percent: 100,
+    location: {
+        level_name: 'L1',
+        t: {sec: 0, nanosec: 0},
+        x: 8,
+        y: -4,
+        yaw: 0,
+     },
+     mode: { mode: RomiCore.RobotMode.MODE_IDLE },
+     model: "40_hours",
+     name: 'LingLing',
+     path: [],
+     task_id: 'taskA'
+ }
+
+ const colorManager = new ColorManager();
+
+const renderInfoPanel = (robotMode: string, footprint: number): JSX.Element => {
+    return (
+        <div style={styles.modeInfoPanel}>
+            <Typography align="center" variant="h5">Configurations</Typography>
+ 
+            <div style={styles.modeInfoItem}>
+                <Typography variant="body1">Robot Mode:</Typography>
+                <Typography variant="body1">{robotMode}</Typography>
+            </div>
+ 
+            <Divider />
+ 
+            <div style={styles.modeInfoItem}>
+                <Typography variant="body1">footprint:</Typography>
+                <Typography variant="body1">{footprint}</Typography>
+            </div>
+ 
+            <Divider />
+ 
+            <div style={styles.modeInfoLink}>
+                 <Typography variant="body1">Click
+                     <a href="https://osrf.github.io/romi-js-core-interfaces/classes/robotmode.html"> here </a> 
+                     for more details on Robot Modes and
+                     <a href="https://osrf.github.io/romi-js-core-interfaces/classes/robotstate.html"> here </a>
+                     for more details on Robot States
+                 </Typography>
+            </div>
+        </div>
+    );
+}
+  
+
+ export const Robot = () => (
+    <RobotComponent
+        robot={robotState}
+        footprint={1}
+        colorManager={colorManager}
+        renderInfoPanel={() => renderInfoPanel('Idle', 1)}
+    />
+ )
