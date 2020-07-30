d72a391f189941fe792e31c5fedea5db65a2288a
Matias Bavera <matiasbavera@gmail.com>
2020-07-30 17:30:20 -0400

    fix delivery tests

--- e2e/tests/deliveryRequest.test.ts
+++ e2e/tests/deliveryRequest.test.ts
@@ -12,28 +12,47 @@ describe('Delivery request', () => {
 
     it('rmf responds to delivery request', () => {
         $('[data-component=MainMenu] [data-item=Robots]').click();
-        const robotItem = $('[data-component=RobotItem]');
-        robotItem.click();
-        const position = robotItem.$('[data-role=position]').getText();
-        robotItem.$('button=Delivery').click();
-        // .clearValue it's not working on Autocomplete
-        robotItem.$('input[name=pickupPlace]').setValue(removeTextFromAutocomplete(10));
-        robotItem.$('input[name=pickupPlace]').setValue('pantry');
+
+        browser.addLocatorStrategy('findAllRobots', () => {
+            return document.querySelectorAll('[data-component=RobotItem]');
+        });
+
+        let getRobotLocations = () => {
+            const allRobotItems = browser.custom$$('findAllRobots', '[data-component=RobotItem]');
+            let robotLocations = allRobotItems.map(robot => {
+                robot.click();
+                return robot.$('[data-role=position]').getHTML();
+            });
+            return robotLocations;
+        };
+        const currentRobotLocations = getRobotLocations();
+
+        const backButton = $('[name="back-button"]');
+        backButton.click();
+        $('[data-component=MainMenu] [data-item=Commands]').click();
+        const deliveryForm = $('[data-component=DeliveryForm]');
+        deliveryForm.click();
+        $('input[name=pickupPlace]').setValue(removeTextFromAutocomplete(10))
+        $('input[name=pickupPlace]').setValue('pantry');
         $('.MuiAutocomplete-popper').click()
 
-        robotItem.$('input[name=pickupDispenser]').click();
+        $('input[name=pickupDispenser]').click();
         $('.MuiAutocomplete-popper').click()
 
-        robotItem.$('input[name=dropoffPlace]').setValue(removeTextFromAutocomplete(20));
-        robotItem.$('input[name=dropoffPlace]').setValue('hardware_2');
+        $('input[name=dropoffPlace]').setValue(removeTextFromAutocomplete(20));
+        $('input[name=dropoffPlace]').setValue('hardware_2');
         $('.MuiAutocomplete-popper').click()
 
-        robotItem.$('input[name=dropoffDispenser]').click();
+        $('input[name=dropoffDispenser]').click();
         $('.MuiAutocomplete-popper').click()
 
-        robotItem.$('button=Request').click();
-        robotItem.$('button=Info').click();
-        expect(robotItem.$('[data-role=position]')).not.toHaveText(position);
+        deliveryForm.$('button=Request').click();
+
+        backButton.click();
+        $('[data-component=MainMenu] [data-item=Robots]').click();
+        const newRobotLocations = getRobotLocations();
+
+        expect(newRobotLocations).not.toMatchObject(currentRobotLocations);
     });
 
     it('renders robot trajectory', () => {
