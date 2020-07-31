6ed316cf4e2de2786bd3d523971c06ac758f128f
Matias Bavera <matiasbavera@gmail.com>
2020-07-31 14:16:22 -0400

    Created a default robot icon component and position the icon robot correctly

--- src/components/schedule-visualizer/robot.tsx
+++ src/components/schedule-visualizer/robot.tsx
@@ -1,16 +1,13 @@
 import { makeStyles } from '@material-ui/core';
-import { useTheme } from '@material-ui/core/styles';
 import * as RomiCore from '@osrf/romi-js-core-interfaces';
 import React, { useMemo, useState, useEffect, useContext } from 'react';
 import ColorManager from './colors';
 import IconManager from '../../icons-manager';
 import { IconContext } from '../../app-contexts';
+import RobotDefaultIcon from './robot-default-icon';
+import { transformMiddleCoordsOfRectToSVGBeginPoint } from '../../util/angle-calculation';
 
 const useStyles = makeStyles(() => ({
-  robotMarker: {
-    cursor: 'pointer',
-    pointerEvents: 'auto',
-  },
   robotText: {
     dominantBaseline: 'central',
     textAnchor: 'middle',
@@ -40,29 +37,25 @@ const Robot = React.forwardRef(function(
 ): React.ReactElement {
   const iconContext = useContext(IconContext);
   const classes = useStyles();
-  const { robot, footprint, colorManager, onClick, inConflict } = props;
-  const [robotColor, setRobotColor] = React.useState<string | null>(() =>
-    colorManager.robotColorFromCache(robot.name, robot.model),
-  );
+  const { robot, footprint, colorManager, onClick } = props;
   const [iconError, setIconError] = useState(false);
   const [renderDefaultIcon, setRenderDefaultIcon] = useState(false);
-
-  const theme = useTheme();
-
-  React.useLayoutEffect(() => {
-    if (robotColor) {
-      return;
-    }
-    (async () => {
-      setRobotColor(await colorManager.robotColor(robot.name, robot.model));
-    })();
-  }, [robot, robotColor, colorManager]);
 
   // The only image formats SVG software support are JPEG, PNG, and other SVG files.
   const iconPath = useMemo(
     () => IconManager.getRobotIcon(iconContext, !!robot.model ? robot.model : robot.name),
     [iconContext, robot],
   );
+
+  const [imgIconWidth, imgIconHeigth] = useMemo(() => [footprint * 2, footprint * 2], [footprint]);
+
+  const { x: topVerticeX, y: topVerticeY } = transformMiddleCoordsOfRectToSVGBeginPoint(
+    robot.location.x,
+    robot.location.y,
+    imgIconWidth,
+    imgIconHeigth,
+  );
+
   useEffect(() => {
     setRenderDefaultIcon(!iconPath || !!iconError);
   }, [iconPath, iconError]);
@@ -78,40 +71,25 @@ const Robot = React.forwardRef(function(
         {!!iconPath && !iconError && (
           <g
             className={classes.robotImgContainer}
-            transform={`translate(${robot.location.x} ${-robot.location.y}) 
+            transform={`translate(${topVerticeX} ${-topVerticeY}) 
             rotate(${-(robot.location.yaw * 180) / Math.PI}, ${footprint}, ${footprint})`}
             onClick={e => onClick && onClick(e, robot)}
           >
             <image
               href={iconPath}
-              height={footprint * 2}
-              width={footprint * 2}
+              height={imgIconHeigth}
+              width={imgIconWidth}
               onError={() => setIconError(true)}
             />
           </g>
         )}
-        {renderDefaultIcon && !!robotColor && (
-          <g
-            transform={`translate(${robot.location.x} ${-robot.location.y})
-            rotate(${-(robot.location.yaw * 180) / Math.PI})`}
-          >
-            <filter id={`${robot.name}-shadow`} x="-20%" y="-20%" width="140%" height="140%">
-              <feDropShadow
-                dx="0"
-                dy="0"
-                stdDeviation={footprint * 0.15}
-                floodColor={inConflict ? theme.palette.error.main : theme.palette.common.black}
-              />
-            </filter>
-            <circle
-              className={classes.robotMarker}
-              onClick={e => onClick && onClick(e, robot)}
-              r={footprint}
-              fill={robotColor}
-              filter={`url(#${robot.name}-shadow)`}
-            />
-            <line x2={footprint} stroke={theme.palette.common.black} strokeWidth="0.05" />
-          </g>
+        {renderDefaultIcon && (
+          <RobotDefaultIcon
+            robot={robot}
+            footprint={footprint}
+            colorManager={colorManager}
+            onClick={onClick}
+          ></RobotDefaultIcon>
         )}
         <text
           id="robotName"
--- src/util/angle-calculation.ts
+++ src/util/angle-calculation.ts
@@ -2,3 +2,15 @@ export function radiansToDegrees(radians: number) {
     var pi = Math.PI;
     return radians * (180 / pi);
 }
+
+/**
+ * Transform coords on the middle of a SVG's Rect to top left coords.
+ */
+export const transformMiddleCoordsOfRectToSVGBeginPoint = (
+    x: number,
+    y: number,
+    width: number,
+    depth: number,
+) => {
+    return { x: x - width / 2, y: y + depth / 2 };
+};
