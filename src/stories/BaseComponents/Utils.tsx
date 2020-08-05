09a995e9df868e9a2d76e103d5a088b541b195f0
ChawinTan <chawin15@gmail.com>
2020-08-05 17:15:15 +0800

    refactor utils colorpalette and font stories

--- src/stories/BaseComponents/Utils.tsx
+++ src/stories/BaseComponents/Utils.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 export const viewBoxCoords: string = '0 0 25.794363144785166 14.53525484725833';
 
 export interface styleTyping {
-    [key: string]: React.CSSProperties;
+  [key: string]: React.CSSProperties;
 }
 
 export interface sampleStyleTyping {
-    colorSample: {
-        [key: string]: React.CSSProperties;
-    }
+  colorSample: {
+    [key: string]: React.CSSProperties;
+  }
 }
 
 export interface formProps {
-    errorState: boolean[];
-    errorMessage: string[];
-    labels: string[];
+  errorState: boolean[];
+  errorMessage: string[];
+  labels: string[];
 }
