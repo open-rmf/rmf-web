29ac20d8d4f4d1a921348c1c0092981be154ef9f
ChawinTan <chawin15@gmail.com>
2020-08-06 17:35:07 +0800

    Change imports

--- src/stories/colorpalette.stories.tsx
+++ src/stories/colorpalette.stories.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
-import { Divider,Typography } from '@material-ui/core';
+import { Divider, Typography } from '@material-ui/core';
 
-import { styleTyping, sampleStyleTyping } from './BaseComponents/Utils';
+import { StyleTyping, SampleStyleTyping } from './BaseComponents/Utils';
 
 export default {
   title: 'Color Palette',
 };
 
-const styles: styleTyping = {
+const styles: StyleTyping = {
   root: {
     margin: '0 auto',
     width: '40%',
   },
   heading: {
-    padding: '0.5rem'
+    padding: '0.5rem',
   },
   example: {
     display: 'flex',
     justifyContent: 'space-between',
-    margin: '1rem 0'
+    margin: '1rem 0',
   },
   aTag: {
     textDecoration: 'none',
-    color: 'rgb(20, 116, 243)'
+    color: 'rgb(20, 116, 243)',
   },
 };
 
-const sampleStyles: sampleStyleTyping = {
+const sampleStyles: SampleStyleTyping = {
   colorSample: {
     unknown: {
       backgroundColor: '#cccccc',
       padding: '0.5rem',
     },
-  }
-}
+  },
+};
 
 export const colors = () => (
   <div style={styles.root}>
     <div style={styles.heading}>
       <Typography variant="body1">
-        This section contains the palette of colors we use in our app 
-        for consistent design across components and state representation.
+        This section contains the palette of colors we use in our app for consistent design across
+        components and state representation.
       </Typography>
     </div>
     <Divider />
@@ -55,8 +55,12 @@ export const colors = () => (
     <div style={styles.example}>
       <Typography variant="body1">Material UI colors</Typography>
       <Typography variant="body1">
-        Refer to <a style={styles.aTag} href="https://material-ui.com/customization/palette/">here</a> for other colors we use.
+        Refer to{' '}
+        <a style={styles.aTag} href="https://material-ui.com/customization/palette/">
+          here
+        </a>{' '}
+        for other colors we use.
       </Typography>
     </div>
   </div>
-)
+);
--- src/stories/design-decision.stories.tsx
+++ src/stories/design-decision.stories.tsx
@@ -1,210 +1,215 @@
 import React from 'react';
-import { Divider,Typography } from '@material-ui/core';
+import { Divider, Typography } from '@material-ui/core';
 
 import ButtonColors from './BaseComponents/button-colors';
-import { styleTyping } from './BaseComponents/Utils'
+import { StyleTyping } from './BaseComponents/Utils';
 
 export default {
-    title: 'Design Decisions',
- };
+  title: 'Design Decisions',
+};
 
-const styles: styleTyping = {
-    root: {
-        margin: '0 auto',
-        width: '40%',
-    },
-    example: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        margin: '1rem 0'
-    },
-    ellipsis: {
-        border: '1px solid black',
-        padding: '0.5rem',
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-        whiteSpace: "nowrap",
-        maxWidth: "10rem",
-    },
-    explanation: {
-        margin: '1rem 0',
-    },
-    panelButton: {
-        border: '2px solid #CCCCCC',
-        padding: 5,
-        width: '4rem',
-        textAlign: 'center',
-        borderRadius: '4px'
-    }
-}
-
-const doorOpen: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #4caf50'
-}
-
-const doorMoving: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #ff9800'
-}
-
-const doorClosed: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #f44336'
-}
-
-const dispenserOnline: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #4caf50'
-}
-
-const dispenserIdle: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #ff9800'
-}
-
-const dispenserOffline: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #f44336'
-}
-
-const liftStop: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #2196f3'
-}
-const liftMoving: React.CSSProperties = {
-    ... styles.panelButton,
-    border: '2px solid #ff9800'
-}
-
-const robotStates: React.CSSProperties = {
+const styles: StyleTyping = {
+  root: {
+    margin: '0 auto',
+    width: '40%',
+  },
+  example: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '1rem 0',
+  },
+  ellipsis: {
+    border: '1px solid black',
+    padding: '0.5rem',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    maxWidth: '10rem',
+  },
+  explanation: {
+    margin: '1rem 0',
+  },
+  panelButton: {
+    border: '2px solid #CCCCCC',
     padding: 5,
-    minWidth: '4rem',
+    width: '4rem',
     textAlign: 'center',
     borderRadius: '4px',
-    border: '2px solid #2196f3'
-}
+  },
+};
+
+const doorOpen: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #4caf50',
+};
+
+const doorMoving: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #ff9800',
+};
+
+const doorClosed: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #f44336',
+};
+
+const dispenserOnline: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #4caf50',
+};
+
+const dispenserIdle: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #ff9800',
+};
+
+const dispenserOffline: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #f44336',
+};
+
+const liftStop: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #2196f3',
+};
+const liftMoving: React.CSSProperties = {
+  ...styles.panelButton,
+  border: '2px solid #ff9800',
+};
+
+const robotStates: React.CSSProperties = {
+  padding: 5,
+  minWidth: '4rem',
+  textAlign: 'center',
+  borderRadius: '4px',
+  border: '2px solid #2196f3',
+};
 
 const robotStateList = [
-    'CHARGING',
-    'DOCKING',
-    'EMERGENCY',
-    'GOING HOME',
-    'IDLE',
-    'MOVING',
-    'PAUSED',
-    'WAITING',
+  'CHARGING',
+  'DOCKING',
+  'EMERGENCY',
+  'GOING HOME',
+  'IDLE',
+  'MOVING',
+  'PAUSED',
+  'WAITING',
 ];
-const doorStateMap: {[key: string]: React.CSSProperties} = { OPEN: doorOpen, MOVING: doorMoving, CLOSED: doorClosed };
-const dispenserStateMap: {[key: string]: React.CSSProperties} = { ONLINE: dispenserOnline, IDLE: dispenserIdle, OFFLINE: dispenserOffline };
-const liftStateMap: {[key: string]: React.CSSProperties} = { STOP: liftStop, MOVING: liftMoving };
-  
+const doorStateMap: { [key: string]: React.CSSProperties } = {
+  OPEN: doorOpen,
+  MOVING: doorMoving,
+  CLOSED: doorClosed,
+};
+const dispenserStateMap: { [key: string]: React.CSSProperties } = {
+  ONLINE: dispenserOnline,
+  IDLE: dispenserIdle,
+  OFFLINE: dispenserOffline,
+};
+const liftStateMap: { [key: string]: React.CSSProperties } = { STOP: liftStop, MOVING: liftMoving };
+
 export const HandleLongName = () => (
-    <div style={styles.root}>
-        <Typography style={styles.explanation} variant="body1">
-            Since the names of the items have the potential to be longer than
-            the container they are in, we truncate it with an ellipsis if it exceeds
-            and also included a <b>Name</b> field in the detail panel.
-        </Typography>
+  <div style={styles.root}>
+    <Typography style={styles.explanation} variant="body1">
+      Since the names of the items have the potential to be longer than the container they are in,
+      we truncate it with an ellipsis if it exceeds and also included a <b>Name</b> field in the
+      detail panel.
+    </Typography>
 
-        <Divider />
+    <Divider />
 
-        <div style={styles.example}>
-            <Typography variant="body1">An example:</Typography>
-            <Typography style={styles.ellipsis} variant="body1">A very very long name</Typography>
-        </div>
+    <div style={styles.example}>
+      <Typography variant="body1">An example:</Typography>
+      <Typography style={styles.ellipsis} variant="body1">
+        A very very long name
+      </Typography>
     </div>
-)
+  </div>
+);
 
 export const HandleUnknown = () => (
-    <div style={styles.root}>
-        <Typography style={styles.explanation} variant="body1">
-            Sometimes, device states might be returned as <b>Unknown</b> for various reasons. As <b>Unknown </b> 
-            is too long for the width of the Panel button, we display it as <b>N/A </b> 
-            in the panel button with a greyed out border.
-        </Typography>
+  <div style={styles.root}>
+    <Typography style={styles.explanation} variant="body1">
+      Sometimes, device states might be returned as <b>Unknown</b> for various reasons. As{' '}
+      <b>Unknown </b>
+      is too long for the width of the Panel button, we display it as <b>N/A </b>
+      in the panel button with a greyed out border.
+    </Typography>
 
-        <Divider />
+    <Divider />
 
-        <div style={styles.example}>
-            <Typography variant="body1">An example:</Typography>
-            <Typography style={styles.panelButton} data-role="state" variant="button">N/A</Typography>
-        </div>
+    <div style={styles.example}>
+      <Typography variant="body1">An example:</Typography>
+      <Typography style={styles.panelButton} data-role="state" variant="button">
+        N/A
+      </Typography>
     </div>
-)
+  </div>
+);
 
 export const DoorButtonColors = () => (
-    <div style={styles.root}>
-        <div style={styles.example}>
-            <Typography variant="h6">Door State</Typography>
-            <Typography variant="h6">Button color and representation</Typography>
-        </div>
-        {
-            Object.keys(doorStateMap).map((state, index) => {
-                return ( 
-                    <React.Fragment key={index}>
-                        <Divider />
-                        <ButtonColors state={state} style={doorStateMap[state]} />
-                    </React.Fragment>
-                )
-            })
-        }
+  <div style={styles.root}>
+    <div style={styles.example}>
+      <Typography variant="h6">Door State</Typography>
+      <Typography variant="h6">Button color and representation</Typography>
     </div>
-)
+    {Object.keys(doorStateMap).map((state, index) => {
+      return (
+        <React.Fragment key={index}>
+          <Divider />
+          <ButtonColors state={state} style={doorStateMap[state]} />
+        </React.Fragment>
+      );
+    })}
+  </div>
+);
 
 export const DispenserButtonColors = () => (
-    <div style={styles.root}>
-        <div style={styles.example}>
-            <Typography variant="h6">Dispenser State</Typography>
-            <Typography variant="h6">Button color and representation</Typography>
-        </div>
-        {
-            Object.keys(dispenserStateMap).map((state, index) => {
-                return ( 
-                    <React.Fragment key={index}>
-                        <Divider />
-                        <ButtonColors state={state} style={dispenserStateMap[state]} />
-                    </React.Fragment>
-                )
-            })
-        }
+  <div style={styles.root}>
+    <div style={styles.example}>
+      <Typography variant="h6">Dispenser State</Typography>
+      <Typography variant="h6">Button color and representation</Typography>
     </div>
-)
+    {Object.keys(dispenserStateMap).map((state, index) => {
+      return (
+        <React.Fragment key={index}>
+          <Divider />
+          <ButtonColors state={state} style={dispenserStateMap[state]} />
+        </React.Fragment>
+      );
+    })}
+  </div>
+);
 
 export const LiftButtonColors = () => (
-    <div style={styles.root}>
-        <div style={styles.example}>
-            <Typography variant="h6">Lift State</Typography>
-            <Typography variant="h6">Button color and representation</Typography>
-        </div>
-        {
-            Object.keys(liftStateMap).map((state, index) => {
-                return ( 
-                    <React.Fragment key={index}>
-                        <Divider />
-                        <ButtonColors state={state} style={liftStateMap[state]} />
-                    </React.Fragment>
-                )
-            })
-        }
+  <div style={styles.root}>
+    <div style={styles.example}>
+      <Typography variant="h6">Lift State</Typography>
+      <Typography variant="h6">Button color and representation</Typography>
     </div>
-)
+    {Object.keys(liftStateMap).map((state, index) => {
+      return (
+        <React.Fragment key={index}>
+          <Divider />
+          <ButtonColors state={state} style={liftStateMap[state]} />
+        </React.Fragment>
+      );
+    })}
+  </div>
+);
 
 export const RobotButtonColors = () => (
-    <div style={styles.root}>
-        <div style={styles.example}>
-            <Typography variant="h6">Robot State</Typography>
-            <Typography variant="h6">Button color and representation</Typography>
-        </div>
-        {
-            robotStateList.map((state, index) => {
-                return ( 
-                    <React.Fragment key={index}>
-                        <Divider />
-                        <ButtonColors state={state} style={robotStates} />
-                    </React.Fragment>
-                )
-            })
-        }
+  <div style={styles.root}>
+    <div style={styles.example}>
+      <Typography variant="h6">Robot State</Typography>
+      <Typography variant="h6">Button color and representation</Typography>
     </div>
-)
+    {robotStateList.map((state, index) => {
+      return (
+        <React.Fragment key={index}>
+          <Divider />
+          <ButtonColors state={state} style={robotStates} />
+        </React.Fragment>
+      );
+    })}
+  </div>
+);
