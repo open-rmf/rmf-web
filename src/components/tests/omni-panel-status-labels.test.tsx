8b846d90e4f98834120c8cb7c5f44613ab46c98d
ChawinTan <chawin15@gmail.com>
2020-08-06 14:19:40 +0800

    add unit tests to omniPanelLabels

---
+++ src/components/tests/omni-panel-status-labels.test.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import toJson from 'enzyme-to-json';
+import { createMount } from '@material-ui/core/test-utils';
+
+import OmniPanelStatusLabels, { LabelProps } from '../omni-panel-status-labels';
+
+const mount = createMount();
+
+const getStatusLabelsComponent = (
+  Component: (props: LabelProps) => React.ReactElement,
+  modeText: string,
+  modalLabelClass: string,
+  name: string,
+  hideTextStyleOverride?: string,
+) => {
+  return (
+    <Component
+      modeText={modeText}
+      modalLabelClass={modalLabelClass}
+      name={name}
+      hideTextStyleOverride={hideTextStyleOverride}
+    />
+  );
+};
+
+const buildLabelsWrapper = (
+  Component: (props: LabelProps) => React.ReactElement,
+  modeText: string,
+  modalLabelClass: string,
+  name: string,
+  hideTextStyleOverride?: string,
+) => {
+  return mount(
+    getStatusLabelsComponent(Component, modeText, modalLabelClass, name, hideTextStyleOverride),
+  );
+};
+
+test('Component renders correctly', () => {
+  expect(
+    toJson(buildLabelsWrapper(OmniPanelStatusLabels, 'State', 'mock-style', 'Robot')),
+  ).toMatchSnapshot();
+
+  expect(
+    toJson(
+      buildLabelsWrapper(OmniPanelStatusLabels, 'State', 'mock-style', 'Robot', 'override-style'),
+    ),
+  ).toMatchSnapshot();
+});
+
+test('Component default style is being override when alternative style is provided', () => {
+  const overrideStyle: string = 'override-style';
+  const wrapper = buildLabelsWrapper(
+    OmniPanelStatusLabels,
+    'State',
+    'mock-style',
+    'Robot',
+    overrideStyle,
+  );
+
+  const isOverride: boolean = wrapper
+    .find(OmniPanelStatusLabels)
+    .at(0)
+    .find('h6')
+    .hasClass(overrideStyle);
+
+  expect(isOverride).toBe(true);
+});
