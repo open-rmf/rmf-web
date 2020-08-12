0e8f804ed13245f8cd50b5e18f379f46fbc837bc
Matias Bavera <matiasbavera@gmail.com>
2020-08-12 18:46:19 -0400

    Get resources with git sparse-checkout, Added option to update .resource.json

--- scripts/setup/get-icons.js
+++ scripts/setup/get-icons.js
@@ -1,7 +1,15 @@
 'use strict';
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 const fs = require('fs');
 const chalk = require('chalk');
+const path = require('path');
+
+const rootPath = path.resolve(__dirname);
+const fileExists = fs.existsSync('./.resources.json');
+if (!fileExists) {
+  console.log('File ./.resources.json not exists');
+  return;
+}
 
 const callback = (error, stdout, stderr) => {
   if (error) {
@@ -21,12 +29,37 @@ const callback = (error, stdout, stderr) => {
 let rawdata = fs.readFileSync('./.resources.json');
 let data = JSON.parse(rawdata);
 
-if (data.hasOwnProperty('url')) {
+if (data.hasOwnProperty('repoUrl')) {
   exec('[ -d "public/assets/icons/" ] && rm -rf public/assets/icons');
   exec('mkdir -p public/assets/icons');
-  exec(`svn checkout ${data.url} public/assets/icons/`, (error, stdout, stderr) =>
-    callback(error, stdout, stderr),
+  // TODO remove --branch or add support
+  execSync(
+    `git clone "${data.repoUrl}" --no-checkout  --depth=1 --single-branch --branch feat/createResourceFolder public/assets/icons/ -o repo`,
+    {
+      stdio: [0, 1, 2], // we need this so node will print the command output
+      cwd: path.resolve(__dirname, '../../'), // path to where you want to save the file
+    },
   );
+
+  execSync(
+    `git sparse-checkout init --cone &&
+      git config core.sparseCheckout 1 &&
+      git sparse-checkout set ${data.folder} &&
+      git checkout`,
+    {
+      stdio: [0, 1, 2],
+      cwd: path.resolve(__dirname, '../../public/assets/icons/'),
+    },
+  );
+
+  exec(`cp -r rmf_dashboard_resources/office/* . && rm -rf rmf_dashboard_resources/`, {
+    stdio: [0, 1, 2],
+    cwd: path.resolve(__dirname, '../../public/assets/icons/'),
+  });
+
+  // exec(`svn checkout ${data.url} public/assets/icons/`, (error, stdout, stderr) =>
+  //   callback(error, stdout, stderr),
+  // );
 } else if (data.hasOwnProperty('path')) {
   exec('[ -d "public/assets/icons/" ] && rm -rf public/assets/icons');
   exec('mkdir -p public/assets/icons');
--- scripts/setup/setup.js
+++ scripts/setup/setup.js
@@ -1,59 +1,109 @@
 const chalk = require('chalk');
 const inquirer = require('inquirer');
-var fs = require('fs');
+const { exec } = require('child_process');
+const fs = require('fs');
 
-if (fs.existsSync('./.resources.json')) {
+const fileExists = fs.existsSync('./.resources.json');
+
+const createConfigFile = () => {
   console.log(chalk`
-  {blue You already configured an icon source!} 👋`);
-  return;
-}
-
-console.log(chalk`
 Hello! 👋
 
 {bold.red IMPORTANT} 🚨 You are about to configure where you are going to bring the icons for the Romi Dashboard project `);
-console.log(chalk`
+  console.log(chalk`
 You have two options:
 {cyan →} 1. {blue get} the assets from a remote GH source.
 {cyan →} 2. {blue copy} assets from a specific folder.
 `);
 
-inquirer
-  .prompt([
-    {
-      name: 'GET_OR_COPY',
+  inquirer
+    .prompt([
+      {
+        name: 'GET_OR_COPY',
+        message: chalk`{blue What option are you going to choose?}`,
+        validate: input => /^[1-2-]$/.test(input) || 'Invalid option',
+      },
+      {
+        name: 'REPO',
+        // TODO: We should change the example once https://github.com/osrf/rmf_demos/pull/109 is merged.
+        message: `Set REPO. Example: https://github.com/matiasbavera/rmf_demos.git`,
+        when: keys => keys['GET_OR_COPY'] === '1',
+        validate: input =>
+          /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+            input,
+          ) || 'Not a valid URL',
+      },
+      {
+        name: 'FOLDER',
+        message: `Set FOLDER. Example: /rmf_dashboard_resources/office/`,
+        when: keys => !!keys['REPO'],
+        validate: input => /^(\/[^\/]+){0,20}\/?$/gm.test(input) || 'Not a valid PATH',
+      },
+      {
+        name: 'COPY',
+        message: 'Set absolute PATH',
+        when: keys => keys['GET_OR_COPY'] === '2',
+        validate: input => /^(\/[^\/]+){0,20}\/?$/gm.test(input) || 'Not a valid PATH',
+      },
+    ])
+    .then(keys => {
+      let information;
+      if (keys.GET_OR_COPY === '1') {
+        information = { repoUrl: keys.REPO, folder: keys.FOLDER };
+      }
+      if (keys.GET_OR_COPY === '2') {
+        information = { path: keys.COPY };
+      }
+      fs.writeFile('.resources.json', JSON.stringify(information), function(err) {
+        if (err) {
+          exec('mv .bak-resources.json .resources.json');
+          throw err;
+        }
+        console.log(chalk`{green File .resources.json was created successfully.}`);
+        try {
+          exec('rm .bak-resources.json');
+        } catch (error) {}
+      });
+    })
+    .catch(error => console.error(error));
+};
+
+if (fileExists) {
+  console.log(chalk`
+  {blue Hello! 👋 You already configured an icon source!
+  1. Continue and do nothing.
+  2. Do you want to modify it? (You can also update the values manually on .resources.json) }`);
+
+  inquirer
+    .prompt({
+      name: 'QUIT_OR_MODIFY',
       message: chalk`{blue What option are you going to choose?}`,
       validate: input => /^[1-2-]$/.test(input) || 'Invalid option',
-    },
-    {
-      name: 'URL',
-      // TODO: We should change the example once https://github.com/osrf/rmf_demos/pull/109 is merged.
-      message:
-        'Set URL. Example: https://github.com/matiasbavera/rmf_demos/branches/feat/createResourceFolder/rmf_dashboard_resources/office',
-      when: keys => keys['GET_OR_COPY'] === '1',
-      validate: input =>
-        /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
-          input,
-        ) || 'Not a valid URL',
-    },
-    {
-      name: 'COPY',
-      message: 'Set absolute PATH',
-      when: keys => keys['GET_OR_COPY'] === '2',
-      validate: input => /^(\/[^\/]+){0,20}\/?$/gm.test(input) || 'Not a valid PATH',
-    },
-  ])
-  .then(keys => {
-    let information;
-    if (keys.GET_OR_COPY === '1') {
-      information = { url: keys.URL };
-    }
-    if (keys.GET_OR_COPY === '2') {
-      information = { path: keys.COPY };
-    }
-    fs.writeFile('.resources.json', JSON.stringify(information), function(err) {
-      if (err) throw err;
-      console.log(chalk`{green File .resources.json was created successfully.}`);
-    });
-  })
-  .catch(error => console.error(error));
+      default: 1,
+    })
+    .then(keys => {
+      if (keys.QUIT_OR_MODIFY === '1') {
+        return;
+      }
+      if (keys.QUIT_OR_MODIFY === '2') {
+        // Rename and save a copy of the file
+        exec('mv .resources.json .bak-resources.json', (error, stdout, stderr) => {
+          if (error) {
+            console.error(chalk`{red exec error: ${error}}`);
+            return;
+          }
+
+          if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
+          createConfigFile();
+        });
+      }
+    })
+    .catch(error => console.error(error));
+}
+
+if (!fileExists) {
+  createConfigFile();
+}
