f5fa5a7993902f076d6fcd990ee4a96bd5b79b96
koonpeng <koonpeng@openrobotics.org>
2020-08-17 17:12:45 +0800

    generate certs and keys instead of committing them directly

--- e2e/prepare.js
+++ e2e/prepare.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
+const { execFileSync } = require('child_process');
 
 // generates soss config based on the current directory, we need to do this because soss
 // requires an absolute path for the cert and key.
 let sossConfig = fs.readFileSync(`${__dirname}/soss.yaml.in`, { encoding: 'utf8' });
 sossConfig = sossConfig.replace(/{{pwd}}/g, __dirname);
 fs.writeFileSync(`${__dirname}/soss.yaml`, sossConfig);
+
+execFileSync(`${__dirname}/generate-certs.sh`);
