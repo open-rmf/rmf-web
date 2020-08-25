af92b675194d719a1bb3589161a7aa83690a7839
koonpeng <koonpeng@openrobotics.org>
2020-08-25 10:21:41 +0800

    add some comments

--- e2e/auth-ready.ts
+++ e2e/auth-ready.ts
@@ -1,6 +1,9 @@
-import * as ChildProcess from 'child_process';
 import * as http from 'http';
 
+/**
+ * Waits for the authentication server to be ready.
+ * @param timeout Max amount of time (in milliseconds) to wait for
+ */
 async function authReady(timeout: number = 30000): Promise<boolean> {
   return new Promise(res => {
     let req: http.ClientRequest | undefined;
