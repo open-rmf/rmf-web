d31acc541d5b2ab3fb329d55f33883233e7cb1d1
koonpeng <koonpeng@openrobotics.org>
2020-08-25 19:00:57 +0800

    newer versions of ros2 launch no longer propagate signals, now we manage the subprocesses ourselves to prevent dangling  processes

--- e2e/rmf-launcher.ts
+++ e2e/rmf-launcher.ts
@@ -18,15 +18,23 @@ export class RmfLauncher {
     if (headless) {
       officeDemoArgs.push('headless:=true');
     }
-    this._officeDemo = ChildProcess.spawn('ros2', officeDemoArgs);
-    this._soss = ChildProcess.spawn('soss', [`${__dirname}/soss.yaml`]);
-    this._visualizerServer = ChildProcess.spawn('ros2', ['launch', 'visualizer', 'server.xml']);
+    this._officeDemo = new ManagedProcess('ros2', officeDemoArgs, { stdio: 'inherit' }, 'SIGINT');
 
-    process.once('beforeExit', async () => {
-      await this.kill();
-    });
+    this._soss = ChildProcess.spawn('soss', [`${__dirname}/soss.yaml`], { stdio: 'inherit' });
 
-    await rmfReady();
+    this._visualizerServer = new ManagedProcess(
+      'ros2',
+      ['launch', 'visualizer', 'server.xml'],
+      { stdio: 'inherit' },
+      'SIGINT',
+    );
+
+    const ready = await rmfReady();
+    if (!ready) {
+      throw new Error('unable to detect rmf');
+    }
+
+    this._launched = true;
   }
 
   async kill(): Promise<void> {
@@ -35,16 +43,16 @@ export class RmfLauncher {
     }
 
     await Promise.all([
-      this._officeDemo && this._killProcess(this._officeDemo, 'SIGINT'), // doesn't clean up properly with SIGTERM
+      this._officeDemo?.kill(),
       this._soss && this._killProcess(this._soss),
-      this._visualizerServer && this._killProcess(this._visualizerServer, 'SIGINT'), // doesn't clean up properly with SIGTERM
+      this._visualizerServer?.kill(),
     ]);
   }
 
   private _launched = false;
-  private _officeDemo?: ChildProcess.ChildProcessWithoutNullStreams;
-  private _soss?: ChildProcess.ChildProcessWithoutNullStreams;
-  private _visualizerServer?: ChildProcess.ChildProcessWithoutNullStreams;
+  private _officeDemo?: ManagedProcess;
+  private _soss?: ChildProcess.ChildProcess;
+  private _visualizerServer?: ManagedProcess;
 
   private async _killProcess(
     proc: ChildProcess.ChildProcess,
@@ -53,28 +61,84 @@ export class RmfLauncher {
     if (proc.killed) {
       return Promise.resolve();
     }
-    proc.kill(signal);
-    return new Promise(res => proc.once('exit', res));
+    return new Promise(res => {
+      proc.once('exit', res);
+      proc.kill(signal);
+    });
   }
 }
 
+/**
+ * A wrapper around soss process that helps manages spawned process and their subprocesses' life.
+ * It spawns processes in their own process groups. The `kill` method will kill the process and
+ * all its childrens.
+ *
+ * This is needed espacially for `ros2 launch` because newer versions of it no longer propagate
+ * signals to its children. So killing `ros2 launch` will leave zombie processes.
+ */
+class ManagedProcess {
+  get proc(): ChildProcess.ChildProcess {
+    return this._proc;
+  }
+
+  get alive(): boolean {
+    return this._procAlive;
+  }
+
+  constructor(
+    command: string,
+    args: string[],
+    options?: Omit<ChildProcess.SpawnOptions, 'detached'>,
+    killSignal?: NodeJS.Signals,
+  ) {
+    this._killSignal = killSignal;
+    this._proc = ChildProcess.spawn(command, args, {
+      ...options,
+      detached: true,
+    });
+    this._procAlive = !!this._proc;
+    if (this._procAlive) {
+      this._proc.once('exit', () => (this._procAlive = false));
+    }
+  }
+
+  /**
+   * Kill the child process and all their childrens.
+   */
+  async kill(): Promise<void> {
+    if (!this._procAlive) {
+      return;
+    }
+
+    return new Promise(res => {
+      this._proc.once('exit', res);
+      process.kill(-this._proc.pid, this._killSignal);
+    });
+  }
+
+  private _proc: ChildProcess.ChildProcess;
+  private _procAlive = false;
+  private _killSignal?: NodeJS.Signals;
+}
+
 async function rmfReady(timeout: number = 30000): Promise<boolean> {
-  const ros2Echo = ChildProcess.spawn('ros2', [
+  const ros2Echo = new ManagedProcess('ros2', [
     'topic',
     'echo',
     'fleet_states',
     'rmf_fleet_msgs/msg/FleetState',
   ]);
-  if (!ros2Echo) {
+  if (!ros2Echo.alive) {
     return false;
   }
-  process.once('beforeExit', () => ros2Echo && ros2Echo.kill());
+
   return new Promise(res => {
     const timer = setTimeout(() => {
       ros2Echo && ros2Echo.kill();
       res(false);
     }, timeout);
-    ros2Echo.once('data', () => {
+    ros2Echo.proc.stdout!.once('data', () => {
+      ros2Echo.kill();
       clearTimeout(timer);
       res(true);
     });
--- e2e/startBackend.ts
+++ e2e/startBackend.ts
@@ -1,4 +1,17 @@
 import { RmfLauncher } from './rmf-launcher';
 
 const launcher = new RmfLauncher();
-launcher.launch()
+launcher.launch();
+
+/**
+ * Make sure spawned processes are killed when the program exits.
+ */
+async function cleanUp(): Promise<never> {
+  await launcher.kill();
+  process.exit();
+}
+
+process.once('beforeExit', cleanUp);
+process.once('exit', cleanUp);
+process.once('SIGINT', cleanUp);
+process.once('SIGTERM', cleanUp);
