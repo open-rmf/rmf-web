e8401b7ad1f00323c3ac0e6b106b326ce32754ca
Matias Bavera <matiasbavera@gmail.com>
2020-09-04 16:10:43 -0400

    Fixed context after rebase

--- src/components/app-contexts.tsx
+++ src/components/app-contexts.tsx
@@ -1,12 +1,11 @@
 import * as RomiCore from '@osrf/romi-js-core-interfaces';
 import React from 'react';
-import ResourceManager from '../resource-manager';
+import ResourceManager, { ResourceConfigurationsType } from '../resource-manager';
 import { defaultSettings, Settings } from '../settings';
 import { NotificationBarContext, NotificationBarProps } from './notification-bar';
 
 /* Declares the ResourcesContext which contains the resources used on the app*/
 export const ResourcesContext = React.createContext<ResourceManager>({} as ResourceManager);
-// export const ResourcesContext = React.createContext<ResourceConfigurationsType>({});
 
 export const SettingsContext = React.createContext(defaultSettings());
 
@@ -16,15 +15,24 @@ export const DispenserStateContext = React.createContext<
 
 export interface AppContextProviderProps extends React.PropsWithChildren<{}> {
   settings: Settings;
+  resources?: Partial<ResourceConfigurationsType>;
   notificationDispatch: React.Dispatch<React.SetStateAction<NotificationBarProps | null>>;
 }
 
 export function AppContextProvider(props: AppContextProviderProps): React.ReactElement {
-  const { settings, notificationDispatch, children } = props;
+  const { settings, notificationDispatch, resources, children } = props;
   return (
     <SettingsContext.Provider value={settings}>
       <NotificationBarContext.Provider value={notificationDispatch}>
-        {children}
+        <ResourcesContext.Provider
+          value={
+            !!resources?.robots
+              ? new ResourceManager(resources as ResourceConfigurationsType)
+              : ({} as ResourceManager)
+          }
+        >
+          {children}
+        </ResourcesContext.Provider>
       </NotificationBarContext.Provider>
     </SettingsContext.Provider>
   );
