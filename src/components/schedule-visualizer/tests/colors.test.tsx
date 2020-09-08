611ff6129a6bfe316b1ff86542cddf8c6e725f24
ChawinTan <chawin15@gmail.com>
2020-09-08 10:29:45 +0800

    unit test for colors

--- src/components/schedule-visualizer/tests/colors.test.tsx
+++ src/components/schedule-visualizer/tests/colors.test.tsx
@@ -11,9 +11,23 @@ describe('Color manager', () => {
     colorManager = new ColorManager();
   });
 
-  it('returns a color when robotColor is called', async () => {
+  it('should store and return a color when robotColor is called', async () => {
     const colorPromise = await colorManager.robotColor(mockRobot.name, mockRobot.model);
-
     expect(colorPromise).toEqual(colorManager.robotColorFromCache(mockRobot.name));
+  });
+
+  it('should store and return a color when robotTrajectoryColor is called', async () => {
+    const colorPromise = await colorManager.robotTrajectoryColor(mockRobot.name, mockRobot.model);
+    expect(colorPromise).toEqual(colorManager.pathColorFromCache(mockRobot.name));
+  });
+
+  it('should return a promise when robotImageColor is called', done => {
+    const mockLink = 'link.com';
+
+    setImmediate(() => {
+      const colorPromise = colorManager.robotImageColor(mockLink, mockRobot.name);
+      expect(colorPromise).resolves.toEqual(colorManager.robotColorFromCache(mockRobot.name));
+      done();
+    });
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -10,6 +10,17 @@ class MockEncoder {
     return jest.fn();
   }
 }
+class MockImage {
+  src = '';
+  onload() {
+    return jest.fn();
+  }
+  onerror() {
+    return jest.fn();
+  }
+}
 const mockCrypto = { subtle: { digest: () => jest.fn() } };
+
 globalAny.window.TextEncoder = MockEncoder;
 globalAny.window.crypto = mockCrypto;
+globalAny.window.Image = MockImage;
