01117d346e8981466dbf0ae08d21d41f8a5655d0
ChawinTan <chawin15@gmail.com>
2020-09-09 08:47:23 +0800

    add trajectory

--- src/stories/baseComponents/trajectory-performance.tsx
+++ src/stories/baseComponents/trajectory-performance.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
-// import Trajectory from './trajectory';
+import Trajectory from './trajectory';
 import { createTrajectories } from '../baseComponents/utils-trajectory';
 import { TextField } from '@material-ui/core';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import { mapBound } from '../baseComponents/utils';
+import ColorManager from '../../components/schedule-visualizer/colors';
+import { defaultSettings } from '../../settings';
+import { defaultCipherList } from 'constants';
 
 export default function TrajectoryPerformance() {
   const [numberOfTraj, setNumberOfTraj] = useState(100);
   const [isConflict, setIsConflict] = useState(true);
   const [trajectories, setTrajectories] = useState(createTrajectories(isConflict, numberOfTraj));
+  const colorManager = new ColorManager();
 
   const handleNumberOfTrajChange = (e: any) => {
     setNumberOfTraj(e.target.value);
@@ -23,16 +28,29 @@ export default function TrajectoryPerformance() {
 
   return (
     <div>
-      <TextField
-        placeholder="Enter number of trajectories"
-        onChange={handleNumberOfTrajChange}
-        defaultValue={numberOfTraj}
-      />
-      <InputLabel id="conflict">Should the trajectory conflict</InputLabel>
-      <Select value={isConflict} labelId="conflict" onChange={handleIsConflictChange}>
-        <MenuItem value={'true'}>Yes</MenuItem>
-        <MenuItem value={'false'}>No</MenuItem>
-      </Select>
+      <div>
+        <TextField
+          placeholder="Enter number of trajectories"
+          onChange={handleNumberOfTrajChange}
+          defaultValue={numberOfTraj}
+        />
+        <InputLabel id="conflict">Should the trajectory conflict</InputLabel>
+        <Select value={isConflict} labelId="conflict" onChange={handleIsConflictChange}>
+          <MenuItem value={'true'}>Yes</MenuItem>
+          <MenuItem value={'false'}>No</MenuItem>
+        </Select>
+      </div>
+      <div>
+        <Trajectory
+          bounds={mapBound}
+          colorManager={colorManager}
+          conflicts={trajectories.conflicts}
+          conflictRobotNames={trajectories.conflictingRobotName}
+          trajs={trajectories.trajectories}
+          description={''}
+          currSettings={defaultSettings()}
+        />
+      </div>
     </div>
   );
 }
