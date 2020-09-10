aa6cfdd99bfbad9681b90703d79d43f177d22a8a
koonpeng <koonpeng@openrobotics.org>
2020-09-10 14:15:29 +0800

    source the e2e env for start:react and start:docker as well, the env script now uses the existing env values if already defined and uses the default if not

--- e2e/env.sh
+++ e2e/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TS_NODE_PROJECT=e2e/tsconfig.json
-export REACT_APP_SOSS_SERVER=wss://localhost:50001
-export REACT_APP_TRAJECTORY_SERVER=ws://localhost:8006
-export ROMI_DASHBOARD_PORT=5000
+export TS_NODE_PROJECT=${TS_NODE_PROJECT:-e2e/tsconfig.json}
+export REACT_APP_SOSS_SERVER=${REACT_APP_SOSS_SERVER:-wss://localhost:50001}
+export REACT_APP_TRAJECTORY_SERVER=${REACT_APP_TRAJECTORY_SERVER:-ws://localhost:8006}
+export ROMI_DASHBOARD_PORT=${ROMI_DASHBOARD_PORT:-5000}
