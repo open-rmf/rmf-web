44043a372f26dfea3f8562db87621b7d98054375
Matias Bavera <matiasbavera@gmail.com>
2020-09-10 17:33:13 -0400

    Added resources download to e2e test

--- scripts/test-e2e.js
+++ scripts/test-e2e.js
@@ -1,18 +1,34 @@
 const concurrently = require('concurrently');
 const { execSync } = require('child_process');
+const fs = require('fs');
 
-execSync('npm run build:e2e', { stdio: 'inherit' });
+const resources = `{
+  "repoUrl": "https://github.com/osrf/rmf_demos.git",
+  "folder": "rmf_dashboard_resources/office/",
+  "branch": "master"
+}
+`;
 
-// wrap in double quotes to support args with spaces
-const wdioArgs = process.argv
-  .slice(2)
-  .map(arg => `"${arg}"`)
-  .join(' ');
+fs.writeFile('.resources.json', resources, function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log(`File .resources.json was created successfully.`);
+  execSync('node ./scripts/setup/get-icons.js');
 
-concurrently(['npm:start:auth', 'npm:start:react:e2e', `npm:test:e2e:wdio -- ${wdioArgs}`], {
-  killOthers: ['success', 'failure'],
-  successCondition: 'first',
-}).catch(e => {
-  console.error(e);
-  process.exitCode = -1;
+  execSync('npm run build:e2e', { stdio: 'inherit' });
+
+  // wrap in double quotes to support args with spaces
+  const wdioArgs = process.argv
+    .slice(2)
+    .map(arg => `"${arg}"`)
+    .join(' ');
+
+  concurrently(['npm:start:auth', 'npm:start:react:e2e', `npm:test:e2e:wdio -- ${wdioArgs}`], {
+    killOthers: ['success', 'failure'],
+    successCondition: 'first',
+  }).catch(e => {
+    console.error(e);
+    process.exitCode = -1;
+  });
 });
