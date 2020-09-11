794109ffc17075a2dd48a4eaa7a5df1aae188f3e
ChawinTan <chawin15@gmail.com>
2020-09-11 10:55:47 +0800

    add remaining test for settings drawer onChange functions

--- src/components/tests/settings-drawer.test.tsx
+++ src/components/tests/settings-drawer.test.tsx
@@ -1,18 +1,103 @@
 import React from 'react';
 import { createMount } from '@material-ui/core/test-utils';
 import toJson from 'enzyme-to-json';
+import { Radio, RadioGroup } from '@material-ui/core';
 
 import SettingsDrawer from '../settings-drawer';
-import { loadSettings } from '../../settings';
+import {
+  loadSettings,
+  TrajectoryAnimation,
+  Settings,
+  TrajectoryDiameter,
+  TrajectoryColor,
+} from '../../settings';
+import { SettingsContext } from '../app-contexts';
 
 const mount = createMount();
 
 describe('Settings Drawer', () => {
   let settings = loadSettings();
+  let onSettingsChange: jest.Mock;
 
-  it('should not crash on render', () => {
-    const wrapper = mount(<SettingsDrawer settings={settings} onSettingsChange={jest.fn()} />);
+  beforeEach(() => {
+    onSettingsChange = jest.fn();
+  });
+
+  it('should render correctly', () => {
+    const wrapper = mount(
+      <SettingsContext.Provider value={settings}>
+        <SettingsDrawer settings={settings} onSettingsChange={onSettingsChange} open={true} />
+      </SettingsContext.Provider>,
+    );
     expect(toJson(wrapper)).toMatchSnapshot();
-    wrapper.unmount();
+  });
+
+  it('should call onSettingsChange function when handleTrajectoryAnimationChange is called', () => {
+    const newSettings: Settings = {
+      ...settings,
+      trajectoryAnimation: TrajectoryAnimation.Outline,
+    };
+    const mockEvent = { target: { value: TrajectoryAnimation.Outline } };
+    const wrapper = mount(
+      <SettingsContext.Provider value={newSettings}>
+        <SettingsDrawer
+          settings={settings}
+          onSettingsChange={newSettings => onSettingsChange(newSettings)}
+          open={true}
+        />
+      </SettingsContext.Provider>,
+    );
+
+    wrapper
+      .find('input')
+      .at(3)
+      .simulate('change', mockEvent);
+    expect(onSettingsChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onSettingsChange function when handleTrajectoryDiameterChange is called', () => {
+    const newSettings: Settings = {
+      ...settings,
+      trajectoryDiameter: TrajectoryDiameter.FixSize,
+    };
+    const mockEvent = { target: { value: TrajectoryDiameter.FixSize } };
+    const wrapper = mount(
+      <SettingsContext.Provider value={newSettings}>
+        <SettingsDrawer
+          settings={settings}
+          onSettingsChange={newSettings => onSettingsChange(newSettings)}
+          open={true}
+        />
+      </SettingsContext.Provider>,
+    );
+
+    wrapper
+      .find('input')
+      .at(4)
+      .simulate('change', mockEvent);
+    expect(onSettingsChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call onSettingsChange function when handleTrajectoryColorChange is called', () => {
+    const newSettings: Settings = {
+      ...settings,
+      trajectoryColor: TrajectoryColor.Theme,
+    };
+    const mockEvent = { target: { value: TrajectoryColor.Theme } };
+    const wrapper = mount(
+      <SettingsContext.Provider value={newSettings}>
+        <SettingsDrawer
+          settings={settings}
+          onSettingsChange={newSettings => onSettingsChange(newSettings)}
+          open={true}
+        />
+      </SettingsContext.Provider>,
+    );
+
+    wrapper
+      .find('input')
+      .at(6)
+      .simulate('change', mockEvent);
+    expect(onSettingsChange).toHaveBeenCalledTimes(1);
   });
 });
