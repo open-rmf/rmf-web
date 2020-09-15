ad4e9a1a76612181f39e7670253d652252408650
vallq <vallq@outlook.com>
2020-09-15 15:22:26 +0800

    update appbar snapshot and appbar tests

--- src/components/tests/appbar.test.tsx
+++ src/components/tests/appbar.test.tsx
@@ -9,22 +9,32 @@ const mount = createMount();
 describe('AppBar', () => {
   let toggleShowOmniPanel: jest.Mock;
   let showSettings: jest.Mock;
+  let showTour: jest.Mock;
 
   beforeEach(() => {
     toggleShowOmniPanel = jest.fn();
     showSettings = jest.fn();
+    showTour = jest.fn();
   });
 
   test('renders correctly', () => {
     const root = mount(
-      <AppBar toggleShowOmniPanel={toggleShowOmniPanel} showSettings={showSettings} />,
+      <AppBar
+        toggleShowOmniPanel={toggleShowOmniPanel}
+        showSettings={showSettings}
+        showTour={showTour}
+      />,
     );
     expect(root.html()).toMatchSnapshot();
   });
 
   test('toggles show omnipanel when dashboard button is clicked', () => {
     const root = mount(
-      <AppBar toggleShowOmniPanel={toggleShowOmniPanel} showSettings={showSettings} />,
+      <AppBar
+        toggleShowOmniPanel={toggleShowOmniPanel}
+        showSettings={showSettings}
+        showTour={showTour}
+      />,
     );
     root.find('button#toggle-omnipanel-btn').simulate('click');
     expect(toggleShowOmniPanel).toHaveBeenCalledTimes(1);
@@ -32,16 +42,36 @@ describe('AppBar', () => {
 
   test('show settings when settings button is clicked', () => {
     const root = mount(
-      <AppBar toggleShowOmniPanel={toggleShowOmniPanel} showSettings={showSettings} />,
+      <AppBar
+        toggleShowOmniPanel={toggleShowOmniPanel}
+        showSettings={showSettings}
+        showTour={showTour}
+      />,
     );
     root.find('button#show-settings-btn').simulate('click');
     expect(showSettings).toHaveBeenCalledTimes(1);
   });
 
+  test('show tour when tour button is clicked', () => {
+    const root = mount(
+      <AppBar
+        toggleShowOmniPanel={toggleShowOmniPanel}
+        showSettings={showSettings}
+        showTour={showTour}
+      />,
+    );
+    root.find('button#show-tour-btn').simulate('click');
+    expect(showTour).toHaveBeenCalledTimes(1);
+  });
+
   test('user button is shown when there is an authenticated user', () => {
     const root = mount(
       <UserContext.Provider value={{ username: 'test' }}>
-        <AppBar toggleShowOmniPanel={toggleShowOmniPanel} showSettings={showSettings} />
+        <AppBar
+          toggleShowOmniPanel={toggleShowOmniPanel}
+          showSettings={showSettings}
+          showTour={showTour}
+        />
       </UserContext.Provider>,
     );
     expect(root.find('#user-btn').length > 0).toBeTruthy();
@@ -54,7 +84,11 @@ describe('AppBar', () => {
     const root = mount(
       <AuthenticatorContext.Provider value={authenticator}>
         <UserContext.Provider value={{ username: 'test' }}>
-          <AppBar toggleShowOmniPanel={toggleShowOmniPanel} showSettings={showSettings} />
+          <AppBar
+            toggleShowOmniPanel={toggleShowOmniPanel}
+            showSettings={showSettings}
+            showTour={showTour}
+          />
         </UserContext.Provider>
       </AuthenticatorContext.Provider>,
     );
