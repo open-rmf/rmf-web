d74ec1ff7654171b9d2ef9e56b7f83fd17e4d0cb
Matias Bavera <matiasbavera@gmail.com>
2020-09-15 19:24:13 -0400

    Added env var for RMF_PACKAGES

--- scripts/setup/get-resources-location.js
+++ scripts/setup/get-resources-location.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
 
-const getDashboardResourcesLocation = () => {
-  return '/opt/rmf/share/rmf_demos_dashboard_resources/';
+if (!process.env.WORLD_NAME) {
+  console.error('You must assign a world name');
+  return;
+}
+
+const worldName = process.env.WORLD_NAME;
+
+const getPackageLocation = (packageName) => {
+  if (!packageName) {
+    throw new Error('You must provide a package name');
+  }
+  return `${process.env.RMF_PACKAGES}${packageName}/`;
 };
 
-const rmfDashboardResources = getDashboardResourcesLocation() + 'office/';
+const rmfDashboardResources = `${getPackageLocation('rmf_demos_dashboard_resources')}${worldName}/`;
 
 const resources = `{"path": "${rmfDashboardResources}"}`;
 
