9b54b4594b42fa3e6f0562cafa50b20a4877f753
vallq <vallq@outlook.com>
2020-09-15 15:19:54 +0800

    add tour button to appbar

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -10,16 +10,19 @@ import {
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { faMapSigns } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { AuthenticatorContext, UserContext } from './auth/contexts';
 
 export interface AppBarProps {
   toggleShowOmniPanel(): void;
   showSettings(show: boolean): void;
+  showTour(show: boolean): void;
 }
 
 export default function AppBar(props: AppBarProps): React.ReactElement {
-  const { toggleShowOmniPanel, showSettings } = props;
+  const { toggleShowOmniPanel, showSettings, showTour } = props;
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const classes = useStyles();
   const authenticator = React.useContext(AuthenticatorContext);
@@ -44,6 +47,9 @@ export default function AppBar(props: AppBarProps): React.ReactElement {
         </IconButton>
         <IconButton id="show-settings-btn" color="inherit" onClick={() => showSettings(true)}>
           <SettingsIcon />
+        </IconButton>
+        <IconButton id="show-tour-btn" color="inherit" onClick={() => showTour(true)}>
+          <FontAwesomeIcon icon={faMapSigns} />
         </IconButton>
         {user && (
           <>
