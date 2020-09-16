02e841dfa7c1bdc8c33e7ce6798f8699149b167a
Matias Bavera <matiasbavera@gmail.com>
2020-09-16 18:20:16 -0400

    simulate Ament functionality

--- scripts/setup/get-resources-location.js
+++ scripts/setup/get-resources-location.js
@@ -4,21 +4,46 @@ if (!process.env.WORLD_NAME) {
   console.error('You must assign a world name');
   return;
 }
-
 const worldName = process.env.WORLD_NAME;
 
-const getPackageLocation = (packageName) => {
-  if (!packageName) {
-    throw new Error('You must provide a package name');
+class Ament {
+  findPackage(packageName) {
+    this._isEmpty(packageName);
+    return this._getPackageLocation(packageName);
   }
-  return `${process.env.RMF_PACKAGES}${packageName}/`;
-};
 
-const rmfDashboardResources = `${getPackageLocation('rmf_demos_dashboard_resources')}${worldName}/`;
+  _isEmpty(packageName) {
+    if (!packageName) {
+      throw new Error('You must provide a package name');
+    }
+  }
 
-const resources = `{"path": "${rmfDashboardResources}"}`;
+  _getPackageLocation(packageName) {
+    const paths = this._getAmentPrefixPath().split(':');
+    // Is package inside?
+    for (let p in paths) {
+      const path = paths[p];
+      const location = `${path}/share/${packageName}/`;
+      const directoryExists = fs.existsSync(location);
+      if (directoryExists) {
+        return location;
+      }
+    }
+  }
 
-fs.writeFile('.resources.json', resources, function (err) {
+  _getAmentPrefixPath() {
+    const amentPath = process.env.AMENT_PREFIX_PATH;
+    if (!amentPath) {
+      throw new Error('Cannot found AMENT_PREFIX_PATH');
+    }
+    return process.env.AMENT_PREFIX_PATH;
+  }
+}
+
+const ament = new Ament();
+const rmfDashboardResources = `${ament.findPackage('rmf_demos_dashboard_resources')}${worldName}/`;
+
+fs.writeFile('.resources.json', `{"path": "${rmfDashboardResources}"}`, function (err) {
   if (err) {
     throw err;
   }
