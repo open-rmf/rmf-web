31f16dad20d5b537632a96b2ecf86487bb2e1a34
Matias Bavera <matiasbavera@gmail.com>
2020-09-17 18:33:46 -0400

    Removed left overs from app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -10,7 +10,6 @@ import PrivateRoute from './auth/private-route';
 import { User } from './auth/user';
 import Dashboard from './dashboard';
 import NotFoundPage from './error-pages/page-not-found';
-import { HotKeys } from 'react-hotkeys';
 
 export default function App(): React.ReactElement {
   const [authInitialized, setAuthInitialized] = React.useState(false);
@@ -26,36 +25,22 @@ export default function App(): React.ReactElement {
     })();
   }, [authenticator]);
 
-  const keyMap = {
-    SNAP_LEFT: '-',
-    OPEN_COMMANDS: ['shift+space'],
-    // SHOW_ALL_HOTKEYS: 'shift+?',
-  };
-
-  const handlers = {
-    SNAP_LEFT: (event: any) => console.log('Move up hotkey called!'),
-    OPEN_COMMANDS: (event: any) => console.log('Move up hotkey called!'),
-    MOVE_UP: (event: any) => console.log('Move up hotkey called!'),
-  };
-
   return authInitialized ? (
-    <HotKeys keyMap={keyMap} handlers={handlers}>
-      <AuthenticatorContext.Provider value={authenticator}>
-        <UserContext.Provider value={user}>
-          <BrowserRouter>
-            <Switch>
-              <Route exact={true} path={LOGIN_ROUTE}>
-                <Login />
-              </Route>
-              <PrivateRoute exact={true} path={'/'}>
-                <Dashboard />
-              </PrivateRoute>
-              <Route component={NotFoundPage} />
-            </Switch>
-          </BrowserRouter>
-        </UserContext.Provider>
-      </AuthenticatorContext.Provider>
-    </HotKeys>
+    <AuthenticatorContext.Provider value={authenticator}>
+      <UserContext.Provider value={user}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact={true} path={LOGIN_ROUTE}>
+              <Login />
+            </Route>
+            <PrivateRoute exact={true} path={'/'}>
+              <Dashboard />
+            </PrivateRoute>
+            <Route component={NotFoundPage} />
+          </Switch>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </AuthenticatorContext.Provider>
   ) : (
     <></>
   );
