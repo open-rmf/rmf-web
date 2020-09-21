5981531684c27b24d85d00f525eff09745c4faa7
ChawinTan <chawin15@gmail.com>
2020-09-21 15:20:01 +0800

    test for robotPrimaryColor error

--- src/components/schedule-visualizer/tests/robot-image-icon.test.tsx
+++ src/components/schedule-visualizer/tests/robot-image-icon.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { makeRobot } from '../../../mock/utils';
 import ColorManager from '../colors';
 import RobotImageIcon from '../robot-image-icon';
+import { act } from 'react-dom/test-utils';
 
 describe('RobotImageIcon', () => {
   const colorManager = new ColorManager();
@@ -41,5 +42,32 @@ describe('RobotImageIcon', () => {
       </svg>,
     );
     expect(root.find('circle#shadow[fill*="conflict"]')).toHaveLength(1);
+  });
+
+  test('should call dispatchIconError when robotPrimaryColor throws an error', async () => {
+    jest.spyOn(colorManager, 'robotPrimaryColor').mockRejectedValue(() => 'err');
+    const mockDispatchIconError = jest.fn();
+    const root = mount(
+      <svg>
+        <RobotImageIcon
+          robot={makeRobot()}
+          footprint={1}
+          colorManager={colorManager}
+          dispatchIconError={mockDispatchIconError}
+          iconPath={iconPath}
+          fleetName={fleetName}
+        />
+      </svg>,
+    );
+    await act(
+      () =>
+        new Promise<void>((resolve) => {
+          setImmediate(() => {
+            root.update();
+            resolve();
+          });
+        }),
+    );
+    expect(mockDispatchIconError).toBeCalledTimes(1);
   });
 });
