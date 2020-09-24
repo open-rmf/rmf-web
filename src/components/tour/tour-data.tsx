b833780043a2f8d09aff1d61b8a67c36c4a20518
vallq <vallq@outlook.com>
2020-09-24 15:36:31 +0800

    update to tour data

---
+++ src/components/tour/tour-data.tsx
@@ -0,0 +1,34 @@
+import { ReactourStep } from 'reactour';
+
+export const tourText: { [step: string]: string } = {
+  step1Welcome: 'Welcome to RoMi dashboard',
+  step2Zoom:
+    'Click on the zoom buttons to change the view of the floor plan. Alternatively, the scroll button on your mouse would work too!',
+  step3Floorplan:
+    'Use the floor plan button to switch between available levels and enabling / disabling the view of different components.',
+  step4Leaflet:
+    'Clicking individual components like doors, robots, lifts on the map will open up its corresponding information tab in the omnipanel.',
+  step5Omnipanel:
+    'The Omnipanel Button shows the different panel options available in the dashboard. Clicking each item would list different information about it!',
+  step6MainMenu:
+    'Each Panel contains a list of the available items and their corresponding states.',
+  step7DoorPanel: 'Let us take a look into the 🚪 Doors Panel.',
+  step8DoorTab: 'Here is an example of what you will see when a door tab is expanded',
+  step9CommandsPanel:
+    'The Commands Panel allows you to send different types of requests that will be handled by RoMi.',
+  step10LoopRequest:
+    'An example is the Loop Request which can be iterated multiple times. RoMi will assign the most suitable robot to perform the task at the point of request.',
+  step11Settings: 'The Settings Button opens up the drawer for different dashboard settings.',
+  step12TrajAnims:
+    'Finally, Trajectory configurations can be changed using the options available. Look out for new features ahead!',
+};
+
+export type stepStyling = {
+  backgroundColor: string;
+  color: string;
+  borderRadius: string;
+};
+
+export type stepDetails = {
+  [primaryKey: string]: ReactourStep;
+};
