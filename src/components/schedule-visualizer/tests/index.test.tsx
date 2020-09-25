de076a298f8eeef205bc98eabd57ad5f32f2846f
ChawinTan <chawin15@gmail.com>
2020-09-25 10:29:32 +0800

    refactor unit tests

--- src/components/schedule-visualizer/tests/index.test.tsx
+++ src/components/schedule-visualizer/tests/index.test.tsx
@@ -3,29 +3,31 @@ import ScheduleVisualizer from '../index';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 
-it('renders without crashing', async () => {
+const mockBuildingMap = {
+  name: 'name',
+  levels: [
+    {
+      name: 'name',
+      places: [],
+      elevation: 1,
+      images: [],
+      doors: [],
+      nav_graphs: [],
+      wall_graph: { name: '', vertices: [], edges: [], params: [] },
+    },
+  ],
+  lifts: [],
+};
+
+it('renders without crashing', () => {
   URL.createObjectURL = jest.fn();
 
-  const wrapper = await shallow(
+  const wrapper = shallow(
     <ScheduleVisualizer
       negotiationTrajStore={{}}
       fleets={[]}
       mapFloorLayerSorted={['name']}
-      buildingMap={{
-        name: 'name',
-        levels: [
-          {
-            name: 'name',
-            places: [],
-            elevation: 1,
-            images: [],
-            doors: [],
-            nav_graphs: [],
-            wall_graph: { name: '', vertices: [], edges: [], params: [] },
-          },
-        ],
-        lifts: [],
-      }}
+      buildingMap={mockBuildingMap}
     />,
   );
   expect(toJson(wrapper)).toMatchSnapshot();
