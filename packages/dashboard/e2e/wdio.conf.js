fa7a5e31425949b17f1f5b35848dc3f62917e4a2
koonpeng <koonpeng@openrobotics.org>
2020-10-06 09:22:54 +0800

    disable tour on e2e tests

--- packages/dashboard/e2e/wdio.conf.js
+++ packages/dashboard/e2e/wdio.conf.js
@@ -195,8 +195,10 @@ exports.config = {
    * @param {Array.<Object>} capabilities list of capabilities details
    * @param {Array.<String>} specs List of spec file paths that are to be run
    */
-  // before: function (capabilities, specs) {
-  // },
+  before: function (capabilities, specs) {
+    browser.url('/');
+    browser.execute(() => localStorage.setItem('tourComplete', 'true'));
+  },
   /**
    * Runs before a WebdriverIO command gets executed.
    * @param {String} commandName hook command name
