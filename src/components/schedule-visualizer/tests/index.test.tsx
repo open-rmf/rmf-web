e090c205fefc307b9366921d1f7e7835a8eba2a0
Chawin Tan <chawin15@gmail.com>
2020-10-08 13:14:45 +0800

    fix: remove zoom limit (#120)

---
+++ src/components/schedule-visualizer/tests/index.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ScheduleVisualizer from '../index';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
+
+const mockBuildingMap = {
+  name: 'name',
+  levels: [
+    {
+      name: 'name',
+      places: [],
+      elevation: 1,
+      images: [],
+      doors: [],
+      nav_graphs: [],
+      wall_graph: { name: '', vertices: [], edges: [], params: [] },
+    },
+  ],
+  lifts: [],
+};
+
+it('renders well with the appropriate data', () => {
+  URL.createObjectURL = jest.fn();
+
+  const wrapper = shallow(
+    <ScheduleVisualizer
+      negotiationTrajStore={{}}
+      fleets={[]}
+      mapFloorLayerSorted={['name']}
+      buildingMap={mockBuildingMap}
+    />,
+  );
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
