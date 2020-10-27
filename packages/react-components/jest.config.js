4734be75fe75451178be892377daeafae9ecde9c
koonpeng <koonpeng@openrobotics.org>
2020-10-27 16:55:34 +0800

    ignore color-manager from coverage

--- packages/react-components/jest.config.js
+++ packages/react-components/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['lib/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: [
+    'lib/color-manager.ts', // uses many apis not available it jsdom
+  ],
 };
