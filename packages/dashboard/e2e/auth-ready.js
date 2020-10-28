c1ce62ee20ce37b938138b5e9638781d8dc9b573
ChawinTan <chawin15@gmail.com>
2020-10-28 14:51:47 +0800

    test behaviour in ci

--- packages/dashboard/e2e/auth-ready.js
+++ packages/dashboard/e2e/auth-ready.js
@@ -4,6 +4,10 @@ const { execSync } = require('child_process');
  * Waits for the authentication server to be ready.
  * @param timeout Max amount of time (in milliseconds) to wait for
  */
+execSync('echo Hello_world__________________________________________', {
+  stdio: 'inherit',
+});
+console.log('can i see what is logged out ?????????????');
 async function authReady(timeout = 30000) {
   return new Promise((res) => {
     let req;
