b187e3ae3f3066f9e9dbd7e189fc83dcdb404495
Matías Bavera <matiasbavera@gmail.com>
2020-10-27 23:01:08 -0300

    refactor: moved requestLoop to e2e utils function. (#153)

--- packages/dashboard/e2e/tests/utils.ts
+++ packages/dashboard/e2e/tests/utils.ts
@@ -53,3 +53,49 @@ export function login(): void {
   $('#password').setValue('admin');
   $('#kc-login').click();
 }
+
+/**
+ * Open loop form
+ * Obs: Make sure that the OmniPanel is visible and in the root to run this.
+ */
+export const openRequestForm = () => {
+  $('[data-component=MainMenu] [data-item=Commands]').click();
+
+  const loopForm = $('[data-component=LoopForm]');
+  loopForm.click();
+};
+
+interface E2eRequestLoopProps {
+  pointA: string;
+  pointB: string;
+  loopNumber?: number;
+  charsToRemoveFromPointA?: number;
+  charsToRemoveFromPointB?: number;
+}
+
+/**
+ * Request a loop task to the robots.
+ * Obs: Make sure that the OmniPanel is visible and it's placed on the loop form.
+ */
+export const requestLoop = (props: E2eRequestLoopProps): void => {
+  const { pointA, pointB, loopNumber, charsToRemoveFromPointA, charsToRemoveFromPointB } = props;
+  const loopForm = $('[data-component=LoopForm]');
+
+  $('input[name=startLocation]').waitForClickable();
+  $('input[name=startLocation]').setValue(
+    removeTextFromAutocomplete(charsToRemoveFromPointA || 10),
+  );
+  $('input[name=startLocation]').setValue(pointA);
+  $('.MuiAutocomplete-popper').click();
+
+  $('input[name=finishLocation]').waitForClickable();
+  $('input[name=finishLocation]').setValue(
+    removeTextFromAutocomplete(charsToRemoveFromPointB || 20),
+  );
+  $('input[name=finishLocation]').setValue(pointB);
+  $('.MuiAutocomplete-popper').click();
+
+  $('input[name=numLoops]').waitForClickable();
+  $('input[name=numLoops]').setValue(loopNumber || 1);
+  loopForm.$('button=Request').click();
+};
