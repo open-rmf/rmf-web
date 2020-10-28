b83b825b2612c28a190af2ebc5aa856939352353
ChawinTan <chawin15@gmail.com>
2020-10-28 11:28:10 +0800

    try executing when auth is ready

--- packages/dashboard/e2e/auth-ready.js
+++ packages/dashboard/e2e/auth-ready.js
@@ -1,5 +1,6 @@
 const http = require('http');
-
+const { execSync } = require('child_process');
+const { executionAsyncId } = require('async_hooks');
 /**
  * Waits for the authentication server to be ready.
  * @param timeout Max amount of time (in milliseconds) to wait for
@@ -18,6 +19,12 @@ async function authReady(timeout = 30000) {
       req = http.request('http://localhost:8080/auth/', () => {
         clearTimeout(timer);
         clearTimeout(retryTimer);
+        execSync('echo $IMAGE', { stdio: 'inherit' });
+        execSync(
+          "export NETWORK=docker inspect $IMAGE --format='{{range $k,$v := .NetworkSettings.Networks }} {{$k}} {{end}}'",
+          { stdio: 'inherit' },
+        );
+        execSync('echo $NETWORK', { stdio: 'inherit' });
         res(true);
       });
       req.once('error', () => {
