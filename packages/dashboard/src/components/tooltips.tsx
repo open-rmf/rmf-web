3c55e0796e78b7500fe1f78d5e61c0a7621ecc57
vallq <vallq@outlook.com>
2020-11-02 12:10:20 +0800

    add custom tooltips component

---
+++ packages/dashboard/src/components/tooltips.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles, Tooltip } from '@material-ui/core';
+
+export interface TooltipProps {
+  title: string;
+  id: string;
+  children: React.ReactElement;
+  enabled: boolean;
+}
+
+export default function DashboardTooltip(props: TooltipProps): React.ReactElement {
+  const { title, id, enabled } = props;
+  const classes = useStyles();
+
+  return (
+    <div>
+      {enabled && (
+        <Tooltip title={title} arrow id={id} className={classes.tooltipWidth}>
+          {props.children}
+        </Tooltip>
+      )}
+      {!enabled && props.children}
+    </div>
+  );
+}
+const useStyles = makeStyles((_theme) => ({
+  tooltipWidth: {
+    maxWidth: 200,
+  },
+}));
