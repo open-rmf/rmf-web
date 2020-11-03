35363d3136ff0a9beb19979a203349d2d863ad0c
vallq <vallq@outlook.com>
2020-11-03 16:18:25 +0800

    add snapshot and false test

--- packages/dashboard/src/components/tests/tooltip.test.tsx
+++ packages/dashboard/src/components/tests/tooltip.test.tsx
@@ -3,18 +3,34 @@ import ReactDOM from 'react-dom';
 import DashboardTooltip from '../tooltip';
 import { IconButton } from '@material-ui/core';
 import { Settings as SettingsIcon } from '@material-ui/icons';
+import { createMount, createShallow } from '@material-ui/core/test-utils';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <DashboardTooltip title="test" id="test-tooltip" enabled={true}>
-      <IconButton>
-        <SettingsIcon />
-      </IconButton>
-    </DashboardTooltip>,
-    div,
-  );
-  ReactDOM.unmountComponentAtNode(div);
+describe('Dashboard Tooltip', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <DashboardTooltip title="test" id="test-tooltip" enabled={true}>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+      </DashboardTooltip>,
+      div,
+    );
+    expect(div).toMatchSnapshot();
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('does not render tooltips when disabled', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <DashboardTooltip title="test" id="test-tooltip" enabled={false}>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+      </DashboardTooltip>,
+      div,
+    );
+    expect(div).toMatchSnapshot();
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
-
-//add snapshot and enabled={false} test
