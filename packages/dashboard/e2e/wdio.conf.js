8d841c0dceb28af978b2e359e705daadadeb5a88
ChawinTan <chawin15@gmail.com>
2020-11-03 13:31:14 +0800

    add whitelist and verbose options to wdio

--- packages/dashboard/e2e/wdio.conf.js
+++ packages/dashboard/e2e/wdio.conf.js
@@ -73,7 +73,7 @@ exports.config = {
         binary: process.env.CHROME_BIN || undefined,
         // to run chrome headless the following flags are required
         // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-        args: [...chromeArgs, '--window-size=1366,768'],
+        args: [...chromeArgs, '--window-size=1366,768', '--verbose', '--whitelisted-ips=""'],
       },
     },
   ],
