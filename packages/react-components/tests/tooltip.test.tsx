d35d2e8e6349fe8080004815a1dabc80521013df
vallq <vallq@outlook.com>
2020-11-06 16:50:54 +0800

    modify test for tooltip

--- packages/react-components/tests/tooltip.test.tsx
+++ packages/react-components/tests/tooltip.test.tsx
@@ -4,15 +4,15 @@ import { Typography } from '@material-ui/core';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 describe('Dashboard Tooltip', () => {
-  it('does not render the tooltips when disabled', () => {
+  it('renders the tooltip', () => {
     render(
-      <DashboardTooltip title="test" id="test-tooltip" enabled={false}>
+      <DashboardTooltip title="test" id="test-tooltip">
         <Typography variant="h5">Tooltip is disabled</Typography>
       </DashboardTooltip>,
     );
 
     fireEvent.mouseEnter(screen.getByRole('heading'));
 
-    expect(screen.queryByTestId('tooltip')).toBeNull();
+    expect(screen.getByTestId('tooltip')).toBeTruthy();
   });
 });
