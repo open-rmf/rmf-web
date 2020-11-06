cc7993e3c07e2a9aec915ddde846fcc0abf6da19
koonpeng <koonpeng@openrobotics.org>
2020-11-06 17:27:40 +0800

    add trajectory marker

--- packages/react-components/lib/robots/index.ts
+++ packages/react-components/lib/robots/index.ts
@@ -1,2 +1,4 @@
 export * from './robot-accordion';
 export * from './robot-marker';
+export * from './trajectory';
+export * from './trajectory-marker';
--- packages/react-components/tests/robots/test-utils.ts
+++ packages/react-components/tests/robots/test-utils.ts
@@ -1,4 +1,5 @@
 import * as RomiCore from '@osrf/romi-js-core-interfaces';
+import { Trajectory } from '../../lib';
 
 export function makeRobot(robotState?: Partial<RomiCore.RobotState>): RomiCore.RobotState {
   return {
@@ -10,5 +11,29 @@ export function makeRobot(robotState?: Partial<RomiCore.RobotState>): RomiCore.R
     task_id: 'test_task_id',
     path: [],
     ...robotState,
+  };
+}
+
+export function makeTrajectory(traj?: Partial<Trajectory>): Trajectory {
+  return {
+    id: 0,
+    dimensions: 1,
+    fleet_name: 'test_fleet',
+    robot_name: 'test_robot',
+    map_name: 'test_map',
+    segments: [
+      {
+        t: 0,
+        v: [0, 0, 0.0],
+        x: [0, 0, 0],
+      },
+      {
+        t: 1000,
+        v: [10, 0, 0],
+        x: [10, 0, 0],
+      },
+    ],
+    shape: 'circle',
+    ...traj,
   };
 }
