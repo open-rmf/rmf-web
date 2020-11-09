efef04fb034da2c17c7b3993a0ac04f8585bde0d
ChawinTan <chawin15@gmail.com>
2020-11-09 15:12:56 +0800

    add options to custom network

--- packages/dashboard/e2e/auth-ready.js
+++ packages/dashboard/e2e/auth-ready.js
@@ -26,7 +26,7 @@ async function authReady(timeout = 80000) {
         process.env.CONTAINER = container;
 
         let isConnected = execSync(
-          'docker ps -q --filter network=$NETWORK --filter ancestor=romi-dashboard/auth',
+          'docker ps -q --filter network=test-net --filter ancestor=romi-dashboard/auth',
         ).toString();
 
         authIpAddress = execSync(
@@ -38,13 +38,35 @@ async function authReady(timeout = 80000) {
 
         if (!isConnected) {
           console.log('I am inside isConnected!!! >>>>> ' + isConnected);
-          execSync('docker network connect $NETWORK $CONTAINER', {
+          execSync(
+            `docker network create 
+          --opt com.docker.network.bridge.default_bridge=true 
+          --opt com.docker.network.bridge.enable_icc=true 
+          --opt com.docker.network.bridge.enable_ip_masquerade=true 
+          --opt com.docker.network.bridge.host_binding_ipv4=0.0.0.0  
+          --opt com.docker.network.bridge.name=docker0
+          --opt com.docker.network.driver.mtu=1500 
+          test-net`,
+            {
+              stdio: 'inherit',
+            },
+          );
+          execSync('docker network connect test-net $CONTAINER', {
             stdio: 'inherit',
           });
-
+          execSync('docker network connect test-net $OTHERCONTAINER', {
+            stdio: 'inherit',
+          });
           execSync('docker network disconnect romidashboarde2e_default $CONTAINER', {
             stdio: 'inherit',
           });
+          execSync('docker network disconnect $NETWORK $OTHERCONTAINER', {
+            stdio: 'inherit',
+          });
+          let githubIp = execSync(
+            "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $OTHERCONTAINER",
+          ).toString();
+          process.env.GITHUB_IP = githubIp;
         }
 
         console.log('=========================== END =============================');
@@ -53,20 +75,17 @@ async function authReady(timeout = 80000) {
         console.log('=========================== END =============================');
       }
 
-      req = http.request(
-        `http://${process.env.AUTH_IP ? process.env.AUTH_IP : 'localhost'}:8080/auth/`,
-        () => {
-          console.log(
-            '-------------------------------- connecting success ------------------------------' +
-              process.env.AUTH_IP,
-          );
-          execSync('docker network inspect $NETWORK', { stdio: 'inherit' });
-          execSync('docker network inspect test-net', { stdio: 'inherit' });
-          clearTimeout(timer);
-          clearTimeout(retryTimer);
-          res(true);
-        },
-      );
+      req = http.request(`http://${'localhost'}:8080/auth/`, () => {
+        console.log(
+          '-------------------------------- connecting success ------------------------------' +
+            process.env.AUTH_IP,
+        );
+        execSync('docker network inspect $NETWORK', { stdio: 'inherit' });
+        execSync('docker network inspect test-net', { stdio: 'inherit' });
+        clearTimeout(timer);
+        clearTimeout(retryTimer);
+        res(true);
+      });
       req.once('error', (err) => {
         console.log(err);
         retryTimer = setTimeout(waitAuthReady, 20000);
--- packages/dashboard/e2e/wdio.conf.js
+++ packages/dashboard/e2e/wdio.conf.js
@@ -107,7 +107,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: `http://localhost:` + port,
+  baseUrl: `http://${process.env.GITHUB_IP ? GITHUB_IP : 'localhost'}:` + port,
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
