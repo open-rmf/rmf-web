df39e3aabe1539a6c58cf7b4df3f28a456a23a75
vallq <vallq@outlook.com>
2020-11-09 14:12:23 +0800

    add test for disabled tooltip

--- packages/react-components/tests/tooltip.test.tsx
+++ packages/react-components/tests/tooltip.test.tsx
@@ -6,13 +6,24 @@ import { render, screen, fireEvent } from '@testing-library/react';
 describe('Dashboard Tooltip', () => {
   it('renders the tooltip', () => {
     render(
-      <DashboardTooltip title="test" id="test-tooltip">
+      <DashboardTooltip title="test" id="test" enabled={true}>
+        <Typography variant="h5">Tooltip is enabled</Typography>
+      </DashboardTooltip>,
+    );
+
+    fireEvent.mouseEnter(screen.getByRole('heading'));
+
+    expect(screen.getByTestId('test-tooltip')).toBeTruthy();
+  });
+  it('does not render the tooltip when disabled', () => {
+    render(
+      <DashboardTooltip title="test" id="test" enabled={false}>
         <Typography variant="h5">Tooltip is disabled</Typography>
       </DashboardTooltip>,
     );
 
     fireEvent.mouseEnter(screen.getByRole('heading'));
 
-    expect(screen.getByTestId('tooltip')).toBeTruthy();
+    expect(screen.queryByTestId('test-tooltip')).toBeNull();
   });
 });
