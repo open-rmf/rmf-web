09f6a06c4c7a351d7a4588361e7a6b438a396475
Matias Bavera <matiasbavera@gmail.com>
2020-11-10 15:45:36 -0300

    Added tests to tree button group

--- packages/react-components/tests/tree-button-group.test.tsx
+++ packages/react-components/tests/tree-button-group.test.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { TreeButtonGroup } from '../lib';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 test('It disabled buttons', () => {
   const root = render(
     <TreeButtonGroup disableClear={true} disableReset={true} disableRestore={true} />,
   );
-  expect(root.container.querySelector('button#clear-button')).toEqual(true);
-  expect(root.container.querySelector('button#reset-button').props().disabled).toEqual(true);
-  expect(root.container.querySelector('button#restore-button').props().disabled).toEqual(true);
+  expect(root.container.querySelector('button#clear-button').hasAttribute('disabled')).toBeTruthy();
+  expect(root.container.querySelector('button#reset-button').hasAttribute('disabled')).toBeTruthy();
+  expect(
+    root.container.querySelector('button#restore-button').hasAttribute('disabled'),
+  ).toBeTruthy();
 });
 
-// test('It executes callbacks correctly', () => {
-//   const handleResetClick = jest.fn();
-//   const handleClearClick = jest.fn();
-//   const handleRestoreClick = jest.fn();
-//   const root = mount(
-//     <TreeButtonGroup
-//       handleResetClick={handleRes\etClick}
-//       handleClearClick={handleClearClick}
-//       handleRestoreClick={handleRestoreClick}
-//     />,
-//   );
-//   root.find('button#clear-button').simulate('click');
-//   root.find('button#reset-button').simulate('click');
-//   root.find('button#restore-button').simulate('click');
+test('It executes callbacks correctly', () => {
+  const handleResetClick = jest.fn();
+  const handleClearClick = jest.fn();
+  const handleRestoreClick = jest.fn();
+  const root = render(
+    <TreeButtonGroup
+      handleResetClick={handleResetClick}
+      handleClearClick={handleClearClick}
+      handleRestoreClick={handleRestoreClick}
+    />,
+  );
+  fireEvent.click(root.container.querySelector('button#clear-button'));
+  fireEvent.click(root.container.querySelector('button#reset-button'));
+  fireEvent.click(root.container.querySelector('button#restore-button'));
 
-//   expect(handleResetClick).toHaveBeenCalledTimes(1);
-//   expect(handleClearClick).toHaveBeenCalledTimes(1);
-//   expect(handleRestoreClick).toHaveBeenCalledTimes(1);
-// });
+  expect(handleResetClick).toHaveBeenCalledTimes(1);
+  expect(handleClearClick).toHaveBeenCalledTimes(1);
+  expect(handleRestoreClick).toHaveBeenCalledTimes(1);
+});
