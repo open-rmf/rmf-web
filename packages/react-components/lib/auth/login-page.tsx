9b565829b346b1e468cc9c84e8f2c8a493f51f9c
koonpeng <koonpeng@openrobotics.org>
2020-11-13 12:52:54 +0800

    login page defaults to theme primary color

--- packages/react-components/lib/auth/login-page.tsx
+++ packages/react-components/lib/auth/login-page.tsx
@@ -2,19 +2,19 @@ import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import { LoginCard, LoginCardProps } from './login-card';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   container: {
     width: '100vw',
     height: '100vh',
     position: 'absolute',
     left: 0,
     top: 0,
-    backgroundColor: '#44497a',
+    backgroundColor: theme.palette.primary.main,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}));
 
 export const LoginPage = (props: LoginCardProps): JSX.Element => {
   const classes = useStyles();
