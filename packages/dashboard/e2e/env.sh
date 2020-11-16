1b0af80aa8425284d49cc03dd1336d5dade5acb6
ChawinTan <chawin15@gmail.com>
2020-11-16 16:55:14 +0800

    use CI instead in env.sh

--- packages/dashboard/e2e/env.sh
+++ packages/dashboard/e2e/env.sh
@@ -6,7 +6,7 @@ export REACT_APP_API_SERVER=${REACT_APP_API_SERVER:-ws://localhost:50002}
 export ROMI_DASHBOARD_PORT=${ROMI_DASHBOARD_PORT:-5000}
 export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-romidashboarde2e}
 
-if [ -z "${GITHUB}" ]; then
+if [ -z "${CI}" ]; then
   echo "In local environment"
 else
   export REACT_APP_AUTH_CONFIG='{"realm":"master", "clientId":"romi-dashboard", "url":"http://172.16.0.2:8080/auth"}'
