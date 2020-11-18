2207665f6e9a4a07d5b263d87fc4083357b2af7b
ChawinTan <chawin15@gmail.com>
2020-11-18 15:04:27 +0800

    try method1

--- packages/dashboard/e2e/auth-ready.js
+++ packages/dashboard/e2e/auth-ready.js
@@ -17,9 +17,9 @@ async function authReady(timeout = 80000) {
     const waitAuthReady = () => {
       // initialize network and container related variables.
       let authIpAddress;
-      const commonNetwork = 'auth_dashboard_network';
-      const commonNetworkSubnet = '172.16.0.0/16';
-      const authContainerIp = '172.16.0.2';
+      // const commonNetwork = 'auth_dashboard_network';
+      // const commonNetworkSubnet = '172.16.0.0/16';
+      // const authContainerIp = '172.16.0.2';
 
       // check if we are in github CI environment
       if (process.env.CI) {
@@ -28,8 +28,9 @@ async function authReady(timeout = 80000) {
           'docker ps -q --filter ancestor=romi-dashboard/auth',
         ).toString();
         if (authContainer) {
+          execSync('docker network inspect auth_network', { stdio: 'inherit' });
           process.env.AUTH_CONTAINER = authContainer;
-
+          execSync('docker inspect $AUTH_CONTAINER', { stdio: 'inherit' });
           // check if auth container is already connected to custom network
           // to prevent running the same operations if it has already been connected to custom network
           const isConnected = execSync(
@@ -38,43 +39,48 @@ async function authReady(timeout = 80000) {
 
           if (!isConnected) {
             // find and set default auth network id
-            const defaultAuthNetwork = execSync(
-              "docker inspect $AUTH_CONTAINER -f '{{range.NetworkSettings.Networks }}{{.NetworkID}}{{end}}'",
-            ).toString();
-            process.env.DEFAULT_AUTH_NETWORK = defaultAuthNetwork;
+            // const defaultAuthNetwork = execSync(
+            //   "docker inspect $AUTH_CONTAINER -f '{{range.NetworkSettings.Networks }}{{.NetworkID}}{{end}}'",
+            // ).toString();
+            // process.env.DEFAULT_AUTH_NETWORK = defaultAuthNetwork;
 
             // create common network with 172.16.0.0/16 subnet and connect the auth and dashboard container
             // We need to specify a subnet to assign an ip address later
             // use an ip in the 172.16.0.0/16 range as it is private, like localhost but is not blocked by github's environment
             // and eliminates the need for ssl
-            execSync(`docker network create --subnet=${commonNetworkSubnet} ${commonNetwork}`, {
-              stdio: 'inherit',
-            });
-            execSync(
-              `docker network connect --ip=${authContainerIp} ${commonNetwork} $AUTH_CONTAINER`,
-              {
-                stdio: 'inherit',
-              },
-            );
-            execSync(`docker network connect ${commonNetwork} $ROMIDASHBOARD_CONTAINER`, {
+            // execSync(`docker network create --subnet=${commonNetworkSubnet} ${commonNetwork}`, {
+            //   stdio: 'inherit',
+            // });
+            // execSync(
+            //   `docker network connect --ip=${authContainerIp} ${commonNetwork} $AUTH_CONTAINER`,
+            //   {
+            //     stdio: 'inherit',
+            //   },
+            // );
+            execSync(`docker network connect auth_network $ROMIDASHBOARD_CONTAINER`, {
               stdio: 'inherit',
             });
 
             // disconnect from old network to prevent concatenation of 2 different network ip later on
-            execSync(`docker network disconnect $DEFAULT_AUTH_NETWORK $AUTH_CONTAINER`, {
-              stdio: 'inherit',
-            });
+            // execSync(`docker network disconnect $DEFAULT_AUTH_NETWORK $AUTH_CONTAINER`, {
+            //   stdio: 'inherit',
+            // });
             execSync('docker network disconnect $GITHUB_NETWORK $ROMIDASHBOARD_CONTAINER', {
               stdio: 'inherit',
             });
 
             // assign of auth container ip
-            authIpAddress = authContainerIp;
+            // authIpAddress = authContainerIp;
           }
         }
       }
-
+      authIpAddress = process.env.AUTH_GATEWAY_IP;
       req = http.request(`http://${authIpAddress ? authIpAddress : 'localhost'}:8080/auth/`, () => {
+        console.log(
+          '--------------------------- connecting success ---------------------------: ' +
+            process.env.AUTH_GATEWAY_IP,
+        );
+        console.log(process.env.REACT_APP_AUTH_CONFIG);
         clearTimeout(timer);
         clearTimeout(retryTimer);
         res(true);
--- packages/dashboard/e2e/scripts/test-e2e.js
+++ packages/dashboard/e2e/scripts/test-e2e.js
@@ -1,10 +1,17 @@
 const concurrently = require('concurrently');
 const { execSync } = require('child_process');
 
+execSync('docker network create auth_network', { stdio: 'inherit' });
+const defaultAuthGatewayIp = execSync(
+  "docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' auth_network",
+).toString();
+process.env.AUTH_GATEWAY_IP = defaultAuthGatewayIp;
+// process.env.REACT_APP_AUTH_CONFIG=`{"realm":"master", "clientId":"romi-dashboard", "url":"http://${defaultAuthGatewayIp}:8080/auth"}`
+console.log('gateway ip address: ' + process.env.AUTH_GATEWAY_IP);
+
 execSync('cd .. && node ./scripts/setup/get-icons.js', { stdio: 'inherit' });
-
+console.log(process.env.REACT_APP_AUTH_CONFIG);
 execSync('npm run build', { stdio: 'inherit' });
-
 // wrap in double quotes to support args with spaces
 const wdioArgs = process.argv
   .slice(2)
