804bb420b7872953ea5f1f961c7e37a0250417cd
ChawinTan <chawin15@gmail.com>
2020-11-18 19:09:44 +0800

    try without switching network

--- packages/dashboard/e2e/auth-ready.js
+++ packages/dashboard/e2e/auth-ready.js
@@ -19,29 +19,29 @@ async function authReady(timeout = 80000) {
       const authIpAddress = process.env.AUTH_GATEWAY_IP;
 
       // check if we are in github CI environment
-      if (process.env.CI) {
-        // check if auth container has already been initialized
-        const authContainer = execSync(
-          'docker ps -q --filter ancestor=romi-dashboard/auth',
-        ).toString();
-        if (authContainer) {
-          process.env.AUTH_CONTAINER = authContainer;
-          // check if auth container is already connected to custom network
-          // to prevent running the same operations if it has already been connected to custom network
-          const isConnected = execSync(
-            `docker ps -q --filter network=auth_network --filter ancestor=romi-dashboard/auth`,
-          ).toString();
+      // if (process.env.CI) {
+      //   // check if auth container has already been initialized
+      //   const authContainer = execSync(
+      //     'docker ps -q --filter ancestor=romi-dashboard/auth',
+      //   ).toString();
+      //   if (authContainer) {
+      //     process.env.AUTH_CONTAINER = authContainer;
+      //     // check if auth container is already connected to custom network
+      //     // to prevent running the same operations if it has already been connected to custom network
+      //     const isConnected = execSync(
+      //       `docker ps -q --filter network=auth_network --filter ancestor=romi-dashboard/auth`,
+      //     ).toString();
 
-          if (!isConnected) {
-            execSync(`docker network connect auth_network $ROMIDASHBOARD_CONTAINER`, {
-              stdio: 'inherit',
-            });
-            execSync('docker network disconnect $GITHUB_NETWORK $ROMIDASHBOARD_CONTAINER', {
-              stdio: 'inherit',
-            });
-          }
-        }
-      }
+      //     if (!isConnected) {
+      //       execSync(`docker network connect auth_network $ROMIDASHBOARD_CONTAINER`, {
+      //         stdio: 'inherit',
+      //       });
+      //       execSync('docker network disconnect $GITHUB_NETWORK $ROMIDASHBOARD_CONTAINER', {
+      //         stdio: 'inherit',
+      //       });
+      //     }
+      //   }
+      // }
       req = http.request(`http://${authIpAddress ? authIpAddress : 'localhost'}:8080/auth/`, () => {
         clearTimeout(timer);
         clearTimeout(retryTimer);
