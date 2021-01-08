43c598391ad8dec4044a7e7500db5321068e822c
koonpeng <koonpeng@openrobotics.org>
2021-01-08 10:38:30 +0800

    make BASE_PATH always starts with "/"

--- packages/dashboard/src/util/url.ts
+++ packages/dashboard/src/util/url.ts
@@ -1,8 +1,14 @@
-export const BASE_PATH =
-  !process.env.REACT_APP_BASE_PATH || process.env.REACT_APP_BASE_PATH === '/'
-    ? ''
-    : process.env.REACT_APP_BASE_PATH;
-
-export const DASHBOARD_ROUTE = `${BASE_PATH}/`;
+/**
+ * Always starts with a '/'.
+ */
+export const BASE_PATH = (() => {
+  if (!process.env.REACT_APP_BASE_PATH || process.env.REACT_APP_BASE_PATH === '') {
+    return '/';
+  }
+  if (!process.env.REACT_APP_BASE_PATH.startsWith('/')) {
+    return `/${process.env.REACT_APP_BASE_PATH}`;
+  }
+  return process.env.REACT_APP_BASE_PATH;
+})();
 
 export const LOGIN_ROUTE = `${BASE_PATH}/login`;
