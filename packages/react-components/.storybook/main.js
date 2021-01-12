29de304e555d9aefda9c3df366151a8547801cd8
koonpeng <koonpeng@openrobotics.org>
2021-01-12 12:23:57 +0800

    prefer ts in storybook

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.extensions = ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.cjs'];
+    return config;
+  },
 };
