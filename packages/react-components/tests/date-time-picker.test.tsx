dd89e5fa4804001a21fdc57ba7df899f4a33dcc5
Matias Bavera <matiasbavera@gmail.com>
2021-01-20 23:03:38 -0300

    add test files

---
+++ packages/react-components/tests/date-time-picker.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+describe('date time picker', () => {
+  it('shows error message', () => {});
+
+  it('shows the correct Label', () => {});
+
+  it('shows the correct date', () => {});
+
+  it('shows the current date if `date` parameter is undefined', () => {});
+});
