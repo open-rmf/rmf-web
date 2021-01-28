0c41f7862a4a3f0c6d1527146428a26fb03c35dc
koonpeng <koonpeng@openrobotics.org>
2021-01-28 10:38:09 +0800

    fix generate script

--- packages/api-server/generate_mixins.sh
+++ packages/api-server/generate_mixins.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd $(dirname $0)
-python3 -m ros_tortoise.main -o api_server/models/mixins \
+python3 -m ros_tortoise -o api_server/models/mixins \
   rmf_charger_msgs \
   rmf_dispenser_msgs \
   rmf_door_msgs \
