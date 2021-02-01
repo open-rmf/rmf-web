c083270fa76c10008e39de76c9e0076ee68e392c
koonpeng <koonpeng@openrobotics.org>
2021-02-01 14:53:22 +0800

    fix tests

--- packages/api-server/api_server/rmf_io/test_rmf_io.py
+++ packages/api-server/api_server/rmf_io/test_rmf_io.py
@@ -20,7 +20,18 @@ def make_door_state(name: str):
 
 
 class TestRmfIO(unittest.IsolatedAsyncioTestCase):
+    async def make_client(self, topic: str):
+        client = client = socketio.AsyncClient()
+        self.clients.append(client)
+        await client.connect('http://localhost:8080')
+        fut = asyncio.Future()
+        client.on('connect', lambda: fut.set_result(None))
+        await fut
+        await client.emit('subscribe', topic)
+        return client
+
     async def asyncSetUp(self):
+        self.clients = []
         self.sio = socketio.AsyncServer(async_mode='aiohttp')
         self.rmf_io = RmfIO(self.sio)
         self.app = aiohttp.web.Application()
@@ -30,49 +41,34 @@ class TestRmfIO(unittest.IsolatedAsyncioTestCase):
         self.site = aiohttp.web.TCPSite(self.runner, 'localhost', 8080)
         await self.site.start()
 
-        self.client = client = socketio.AsyncClient()
-        await client.connect('http://localhost:8080')
-        fut = asyncio.Future()
-        self.client.on('connect', lambda: fut.set_result(None))
-        await fut
-
     async def asyncTearDown(self):
-        await self.client.disconnect()
+        asyncio.gather(*[client.disconnect() for client in self.clients])
+        self.clients.clear()
         await self.runner.cleanup()
 
     async def test_door_states(self):
         '''
         test receiving door states events
         '''
-        door_state_fut = asyncio.Future()
-        self.client.on(topics.door_states, door_state_fut.set_result)
-
-        await self.client.emit('subscribe', topics.door_states)
-        sub_result = asyncio.Future()
-        self.client.on('subscribe', sub_result.set_result)
-        await sub_result
-
-        self.rmf_io.on_door_state(make_door_state('test_door'))
-        await door_state_fut
-        door_state = door_state_fut.result()
-        self.assertEqual(door_state['door_name'], 'test_door')
-
-    async def test_door_states_late_sub(self):
-        '''
-        clients should receive all current door states on subscribe
-        '''
         done = asyncio.Future()
         test_names = ['test_door', 'test_door_2']
         test_states = [make_door_state(name) for name in test_names]
-        all_states = {}
+        received_states = {}
+        client = await self.make_client(topics.door_states)
 
-        @self.client.on(topics.door_states)
         def on_door_states(door_state):
-            all_states[door_state['door_name']] = door_state
-            if len(all_states) == len(test_states) and \
-                    all([x in test_names for x in all_states.keys()]):
+            received_states[door_state['door_name']] = door_state
+            if len(received_states) == len(test_states) and \
+                    all([x in test_names for x in received_states.keys()]):
                 done.set_result(True)
+        client.on(topics.door_states, on_door_states)
 
         [self.rmf_io.on_door_state(x) for x in test_states]
-        await self.client.emit('subscribe', topics.door_states)
+        await asyncio.wait_for(done, 1)
+        received_states = {}
+
+        # test that a new client receives all the current states on subscribe
+        done = asyncio.Future()
+        client = await self.make_client(topics.door_states)
+        client.on(topics.door_states, on_door_states)
         await asyncio.wait_for(done, 1)
