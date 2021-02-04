3fb96bbfd6b95af2ce6b5813ce9ba5edc211ccd3
vallq <vallq@outlook.com>
2021-02-04 19:04:23 +0800

    update drawer-header tests

--- packages/dashboard/src/components/drawers/tests/drawer-header.test.tsx
+++ packages/dashboard/src/components/drawers/tests/drawer-header.test.tsx
@@ -1,49 +1,44 @@
 import React from 'react';
-import { createShallow } from '@material-ui/core/test-utils';
+import { render } from '@testing-library/react';
 import DrawerHeader from '../drawer-header';
-import CloseIcon from '@material-ui/icons/Close';
-import { KeyboardBackspace as BackIcon } from '@material-ui/icons';
-import { Typography } from '@material-ui/core';
-
-const mount = createShallow();
 
 describe('Drawer header', () => {
   test('Renders correctly', () => {
-    const root = mount(
+    const root = render(
       <DrawerHeader
         handleCloseButton={() => jest.fn()}
         handleBackButton={() => jest.fn()}
         title={'Test'}
       />,
     );
-    expect(root.find(Typography).html()).toContain('Test');
-    expect(root.find(CloseIcon).exists()).toBeTruthy();
-    expect(root.find(BackIcon).exists()).toBeTruthy();
-    expect(root).toMatchSnapshot();
+    expect(root.getByText('Test')).toBeDefined();
+    expect(root.getByTestId('backDrawerButton')).toBeDefined();
+    expect(root.getByTestId('closeDrawerButton')).toBeDefined();
+    //expect(root).toMatchSnapshot();
   });
 
   test('Renders without back button if there is no handler', () => {
-    const root = mount(<DrawerHeader handleCloseButton={() => jest.fn()} title={'Test'} />);
-    expect(root.find(BackIcon).exists()).toBeFalsy();
+    const root = render(<DrawerHeader handleCloseButton={() => jest.fn()} title={'Test'} />);
+    expect(root.queryByTestId('backDrawerButton')).toBeFalsy();
   });
 
   test('Click on close button fires an event correctly', () => {
     let clicked = false;
-    const root = mount(<DrawerHeader handleCloseButton={() => (clicked = true)} title={'Test'} />);
-    root.find('#closeDrawerButton').simulate('click');
+    const root = render(<DrawerHeader handleCloseButton={() => (clicked = true)} title={'Test'} />);
+    root.getByTestId('closeDrawerButton').click();
     expect(clicked).toBe(true);
   });
 
   test('Click on back button fires an event correctly', () => {
     let clicked = false;
-    const root = mount(
+    const root = render(
       <DrawerHeader
         handleCloseButton={() => jest.fn()}
         handleBackButton={() => (clicked = true)}
         title={'Test'}
       />,
     );
-    root.find('#backDrawerButton').simulate('click');
+    root.getByTestId('backDrawerButton').click();
     expect(clicked).toBe(true);
   });
 });
