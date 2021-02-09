c12e97989cf3e6c7c7138bebc58f8eb1656d6432
vallq <vallq@outlook.com>
2021-02-09 11:28:13 +0800

    update login tests

--- packages/dashboard/src/components/auth/tests/login.test.tsx
+++ packages/dashboard/src/components/auth/tests/login.test.tsx
@@ -1,5 +1,6 @@
 import { createMount } from '@material-ui/core/test-utils';
 import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { Redirect } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import FakeAuthenticator from '../../../mock/fake-authenticator';
@@ -18,6 +19,17 @@ describe('Login page', () => {
     expect(component).toMatchSnapshot();
   });
 
+  test('renders without crashing', () => {
+    const root = render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>,
+    );
+    root.unmount();
+  });
+
+  /*NOTE: Removing this test causes the subsequent 2 tests to fail
+  but running the tests individually (test.only) allows the tests to pass*/
   test('redirects to dashboard when user is authenticated', () => {
     window.history.replaceState(window.history.state, '');
     const wrapper = mount(
@@ -34,20 +46,38 @@ describe('Login page', () => {
     expect(wrapper.find(Redirect)).toBeTruthy();
   });
 
+  test('redirects from login when user is authenticated', () => {
+    const root = render(
+      <BrowserRouter>
+        <UserContext.Provider
+          value={{
+            username: 'test',
+          }}
+        >
+          <Login />
+        </UserContext.Provider>
+      </BrowserRouter>,
+    );
+    ``;
+    //screen.debug();
+    expect(root.queryByText('Login with RMF')).toBeFalsy();
+    root.unmount();
+  });
+
   test('performs login when login button is clicked', () => {
     const authenticator = new FakeAuthenticator();
     const spy = jest.spyOn(authenticator, 'login').mockImplementation(() => undefined as any);
 
-    const root = mount(
+    const root = render(
       <BrowserRouter>
         <AuthenticatorContext.Provider value={authenticator}>
           <Login />
         </AuthenticatorContext.Provider>
       </BrowserRouter>,
     );
-    const loginButton = root.find('button#login-button').first();
-    expect(loginButton).toBeTruthy();
-    loginButton.simulate('click');
+    //screen.debug();
+    const loginButton = root.getByRole('button', { name: /Login with RMF/i });
+    fireEvent.click(loginButton);
     expect(spy).toHaveBeenCalledTimes(1);
   });
 });
