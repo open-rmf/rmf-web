43f869694c1e98998913f2232bbd944a0c5ba0af
ChawinTan <chawin15@gmail.com>
2021-02-10 13:51:21 +0800

    test banner isError props

--- packages/react-components/tests/systems-summary/systems-summary-banner.test.tsx
+++ packages/react-components/tests/systems-summary/systems-summary-banner.test.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { SystemSummaryBanner } from '../../lib';
 import { render } from '@testing-library/react';
 
-test('smoke test', () => {
-  render(<SystemSummaryBanner isError={false} />);
+test('should contain noError class if isError is false', () => {
+  const root = render(<SystemSummaryBanner isError={false} />);
+  expect(root.container.querySelector('h6')?.classList[2].includes('makeStyles-noError')).toEqual(
+    true,
+  );
+});
+
+test('should contain error class if isError is true', () => {
+  const root = render(<SystemSummaryBanner isError={true} />);
+  expect(root.container.querySelector('h6')?.classList[2].includes('makeStyles-error')).toEqual(
+    true,
+  );
 });
