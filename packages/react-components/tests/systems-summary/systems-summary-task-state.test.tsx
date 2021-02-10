b93768217d8c9e9f2c5f617d6184352760ad66b5
ChawinTan <chawin15@gmail.com>
2021-02-10 12:03:59 +0800

    system summary task test

--- packages/react-components/tests/systems-summary/systems-summary-task-state.test.tsx
+++ packages/react-components/tests/systems-summary/systems-summary-task-state.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { SystemSummaryTaskState } from '../../lib';
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import { tasks } from './test.utils';
 
-test('smoke test', () => {
-  const { debug } = render(<SystemSummaryTaskState tasks={tasks} onClick={jest.fn()} />);
-  debug();
+test('expect all different task states to be calculated and rendered', () => {
+  render(<SystemSummaryTaskState tasks={tasks} onClick={jest.fn()} />);
+  // 1 task for each state, total 4 states
+  expect(screen.queryAllByText('1').length).toEqual(4);
 });
