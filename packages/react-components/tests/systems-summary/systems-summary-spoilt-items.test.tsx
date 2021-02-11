315105b5de38038c2aad929530a07b12844a6b25
ChawinTan <chawin15@gmail.com>
2021-02-11 10:46:55 +0800

    add test for onClick spoilt items

--- packages/react-components/tests/systems-summary/systems-summary-spoilt-items.test.tsx
+++ packages/react-components/tests/systems-summary/systems-summary-spoilt-items.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SystemSummarySpoiltItems } from '../../lib';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { door, lift, fleet } from './test.utils';
 
 test('should render the list of spoilt items', () => {
   const spoiltItems = [
@@ -23,4 +24,47 @@ test('should render the list of spoilt items', () => {
 
   expect(screen.getAllByText('item - state').length).toEqual(spoiltItems.length);
   expect(screen.getAllByText('Error - error').length).toEqual(errorMessageCount);
+});
+
+test('it should trigger the spoilt item callback function when spoit item is clicked', () => {
+  const spoiltItems = [
+    { type: 'door', name: 'door', itemNameAndState: 'door - state' },
+    { type: 'lift', name: 'lift', itemNameAndState: 'lift - state' },
+    { type: 'dispenser', name: 'dispenser', itemNameAndState: 'dispenser - state' },
+    {
+      type: 'robot',
+      name: 'robot',
+      fleet: 'fleet',
+      itemNameAndState: 'robot - state',
+      errorMessage: 'error',
+    },
+  ];
+  const doorClick = jest.fn();
+  const liftClick = jest.fn();
+  const dispenserClick = jest.fn();
+  const robotClick = jest.fn();
+
+  const { debug } = render(
+    <SystemSummarySpoiltItems
+      doors={[door]}
+      lifts={[lift]}
+      dispensers={['dispenser']}
+      robots={{ fleet: fleet }}
+      spoiltItems={spoiltItems}
+      spoiltDoorClick={doorClick}
+      spoiltLiftClick={liftClick}
+      spoiltDispenserClick={dispenserClick}
+      spoiltRobotClick={robotClick}
+    />,
+  );
+  debug();
+  fireEvent.click(screen.getByText('door - state'));
+  fireEvent.click(screen.getByText('lift - state'));
+  fireEvent.click(screen.getByText('dispenser - state'));
+  fireEvent.click(screen.getByText('robot - state'));
+
+  expect(doorClick).toBeCalled();
+  expect(liftClick).toBeCalled();
+  expect(dispenserClick).toBeCalled();
+  expect(robotClick).toBeCalled();
 });
--- packages/react-components/tests/systems-summary/test.utils.ts
+++ packages/react-components/tests/systems-summary/test.utils.ts
@@ -45,3 +45,52 @@ export const tasks: RomiCore.TaskSummary[] = [
     end_time: { sec: 0, nanosec: 0 },
   },
 ];
+
+export const door: RomiCore.Door = {
+  name: 'door',
+  v1_x: 8.2,
+  v1_y: -5.5,
+  v2_x: 7.85,
+  v2_y: -6.2,
+  door_type: 2,
+  motion_range: -1.571,
+  motion_direction: 1,
+};
+
+export const lift: RomiCore.Lift = {
+  name: 'lift',
+  doors: [door],
+  levels: ['L1', 'L2', 'L3'],
+  ref_x: 7.1,
+  ref_y: -2.8,
+  ref_yaw: -0.5,
+  width: 2.5,
+  depth: 2.5,
+  wall_graph: {
+    name: 'wallgraph',
+    vertices: [],
+    edges: [],
+    params: [],
+  },
+};
+
+export const fleet: RomiCore.FleetState = {
+  name: 'fleet',
+  robots: [
+    {
+      name: 'robot',
+      model: 'Model1',
+      mode: { mode: RomiCore.RobotMode.MODE_EMERGENCY },
+      location: {
+        level_name: 'L1',
+        x: 4,
+        y: -12,
+        yaw: 0,
+        t: { sec: 0, nanosec: 0 },
+      },
+      battery_percent: 100,
+      path: [],
+      task_id: 'task1',
+    },
+  ],
+};
