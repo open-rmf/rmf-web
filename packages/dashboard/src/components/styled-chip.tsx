c38260bce2408394c7d69f4b4ee723794cb75f01
vallq <vallq@outlook.com>
2021-02-16 19:32:12 +0800

    add styled chip

---
+++ packages/dashboard/src/components/styled-chip.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import { red, blue, teal, cyan } from '@material-ui/core/colors';
+
+interface StyledChipProps {
+  state: string;
+}
+
+const useStyles = (bgCol: string) =>
+  makeStyles({
+    root: {
+      background: bgCol,
+      color: 'white',
+      height: 24,
+      verticalAlign: 'middle',
+    },
+  })();
+
+const returnChipColor = (state: string): string => {
+  switch (state) {
+    case 'Code Blue':
+      return blue[500];
+    case 'Code Red':
+      return red[400];
+    case 'NA':
+      return teal[500];
+  }
+  return cyan[500];
+};
+
+const StyledChip = (props: StyledChipProps): ReactElement => {
+  const { state } = props;
+  const classes = useStyles(returnChipColor(state));
+
+  return <Chip classes={{ root: classes.root }} label={state} />;
+};
+
+export default StyledChip;
