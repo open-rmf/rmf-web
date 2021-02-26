7e7b5fe526883268f97c719bfb0d5485dc9acb4f
Teo Koon Peng <koonpeng@openrobotics.org>
2021-02-26 15:28:35 +0800

    add smoke tests for door marker, fail gracefully when rendering door with unknown type

--- packages/react-components/tests/doors/door-marker.spec.tsx
+++ packages/react-components/tests/doors/door-marker.spec.tsx
@@ -1,8 +1,61 @@
-import { render } from '@testing-library/react';
+import * as RomiCore from '@osrf/romi-js-core-interfaces';
+import { cleanup, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { DoorMarker } from '../../lib';
 import { makeDoor } from './test-utils';
+
+it('smoke test with different door modes', () => {
+  ([
+    { value: RomiCore.DoorMode.MODE_CLOSED },
+    { value: RomiCore.DoorMode.MODE_MOVING },
+    { value: RomiCore.DoorMode.MODE_OPEN },
+    { value: -1 },
+  ] as RomiCore.DoorMode[]).forEach((mode) => {
+    render(
+      <svg>
+        <DoorMarker door={makeDoor()} doorMode={mode} />
+      </svg>,
+    );
+    cleanup();
+  });
+});
+
+it('smoke test with different door types', () => {
+  [
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_DOUBLE_SLIDING,
+    }),
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_DOUBLE_SWING,
+    }),
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_DOUBLE_TELESCOPE,
+    }),
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_SINGLE_SLIDING,
+    }),
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_SINGLE_SWING,
+    }),
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_SINGLE_TELESCOPE,
+    }),
+    makeDoor({
+      door_type: RomiCore.Door.DOOR_TYPE_UNDEFINED,
+    }),
+    makeDoor({
+      door_type: -1,
+    }),
+  ].forEach((door) => {
+    render(
+      <svg>
+        <DoorMarker door={door} />
+      </svg>,
+    );
+    cleanup();
+  });
+});
 
 it('triggers onClick callback when button is clicked', () => {
   const mockOnClick = jasmine.createSpy();
