2c4d2c1967f26ee74a224a30673a06b61a8bc067
Teo Koon Peng <koonpeng@openrobotics.org>
2021-02-27 01:15:51 +0800

    react-components: refactor omnipanel (#230)
    * receive a stack of view as props, this should make it harder to mess up the state of the views
    
    * fix minor border rendering issues; better support flexible layouts
    
    * fix ros2-bridge ci path

--- packages/react-components/tests/stack-navigator.spec.ts
+++ packages/react-components/tests/stack-navigator.spec.ts
@@ -1,33 +1,55 @@
-import { StackNavigator } from '../lib';
+import { act, renderHook, RenderResult } from '@testing-library/react-hooks/dom';
+import { StackNavigatorDispatch, useStackNavigator } from '../lib';
 
-describe('Stack navigator', () => {
-  let stack: StackNavigator<number>;
+let hookResult: RenderResult<[number[], StackNavigatorDispatch<number>]>;
+let stackDispatch: StackNavigatorDispatch<number>;
 
-  beforeEach(() => {
-    stack = new StackNavigator(0);
+beforeEach(() => {
+  hookResult = renderHook(() => useStackNavigator([0], 0)).result;
+  stackDispatch = hookResult.current[1];
+});
+
+it('push', () => {
+  act(() => stackDispatch.push(10));
+  const stack = hookResult.current[0];
+  expect(stack).toHaveSize(2);
+  expect(stack[0]).toBe(0);
+  expect(stack[1]).toBe(10);
+});
+
+it('pop does not remove last item', () => {
+  act(() => {
+    stackDispatch.push(2);
+    stackDispatch.pop();
+    stackDispatch.pop();
   });
+  const stack = hookResult.current[0];
+  expect(stack).toHaveSize(1);
+  expect(stack[0]).toBe(0);
+});
 
-  it('push', () => {
-    stack.push(10);
-    expect(stack.stack).toHaveSize(2);
-    expect(stack.stack[0]).toBe(0);
-    expect(stack.stack[1]).toBe(10);
+it('reset returns the stack to the initial state', () => {
+  act(() => {
+    stackDispatch.push(2);
+    stackDispatch.push(3);
   });
+  expect(hookResult.current[0]).toHaveSize(3);
+  act(() => {
+    stackDispatch.reset();
+  });
+  expect(hookResult.current[0]).toHaveSize(1);
+  expect(hookResult.current[0][0]).toBe(0);
+});
 
-  it('pop does not remove last item', () => {
-    stack.push(2);
-    stack.pop();
-    stack.pop();
-    expect(stack.stack).toHaveSize(1);
-    expect(stack.stack[0]).toBe(0);
+it('home pushes the home view onto the stack', () => {
+  act(() => {
+    stackDispatch.push(2);
+    stackDispatch.push(3);
   });
-
-  it('reset leaves only the home item in the stack', () => {
-    stack.push(2);
-    stack.push(3);
-    expect(stack.stack).toHaveSize(3);
-    stack.reset();
-    expect(stack.stack).toHaveSize(1);
-    expect(stack.stack[0]).toBe(0);
+  expect(hookResult.current[0]).toHaveSize(3);
+  act(() => {
+    stackDispatch.home();
   });
+  expect(hookResult.current[0]).toHaveSize(4);
+  expect(hookResult.current[0][3]).toBe(0);
 });
