8826d76a0bed828f4e86a088619a5369c3bcbee3
Teo Koon Peng <koonpeng@openrobotics.org>
2021-02-26 15:42:27 +0800

    fix false positive, it appears headless chrome and chrome may give a different result

--- packages/react-components/tests/color-manager.spec.ts
+++ packages/react-components/tests/color-manager.spec.ts
@@ -13,7 +13,11 @@ it('snapshot - get color based on image', async () => {
     'test_model',
     '/base/tests/assets/tiny-robot.png',
   );
-  expect(color).toBe('rgb(4, 52, 156)');
+  const values = color.slice(color.indexOf('(') + 1, color.lastIndexOf(')'));
+  const rgb = values.split(',').map((v) => parseInt(v));
+  expect(Math.abs(rgb[0] - 4)).toBeLessThanOrEqual(2);
+  expect(Math.abs(rgb[1] - 52)).toBeLessThanOrEqual(2);
+  expect(Math.abs(rgb[2] - 156)).toBeLessThanOrEqual(2);
 });
 
 it('able to get color from cache after the color is loaded previously', async () => {
