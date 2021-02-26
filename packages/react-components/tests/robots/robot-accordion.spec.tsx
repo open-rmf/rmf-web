bce5bf6069c735da4ae38010ef4ba8508e170bda
Teo Koon Peng <koonpeng@openrobotics.org>
2021-02-26 16:50:24 +0800

    add test for basic robot information

--- packages/react-components/tests/robots/robot-accordion.spec.tsx
+++ packages/react-components/tests/robots/robot-accordion.spec.tsx
@@ -1,3 +1,4 @@
+import * as RomiCore from '@osrf/romi-js-core-interfaces';
 import { cleanup, render } from '@testing-library/react';
 import React from 'react';
 import { RobotAccordion } from '../../lib';
@@ -14,4 +15,24 @@ it('smoke test with different robot modes', () => {
       render(<RobotAccordion fleetName="test_fleet" robot={robot} />);
       cleanup();
     });
+});
+
+it('renders basic robot information', () => {
+  const robot = makeRobot({
+    name: 'test_robot',
+    battery_percent: 1,
+    location: { level_name: 'test_level', x: 0, y: 0, yaw: 0, t: { sec: 0, nanosec: 0 } },
+    mode: { mode: RomiCore.RobotMode.MODE_PAUSED },
+    model: 'test_model',
+    task_id: 'test_task_id',
+    path: [],
+  });
+  const accordion = render(<RobotAccordion fleetName="test_fleet" robot={robot} />);
+  expect(accordion.queryAllByText('test_robot')).toHaveSize(2); // summary and details
+  expect(accordion.queryByText('test_model')).toBeTruthy();
+  expect(accordion.queryByText('test_fleet')).toBeTruthy();
+  expect(accordion.queryByText('test_level (0.000, 0.000)')).toBeTruthy();
+  expect(accordion.queryByText('0.000')).toBeTruthy(); // yaw
+  expect(accordion.queryByText('test_task_id')).toBeTruthy();
+  expect(accordion.queryByText('1')).toBeTruthy(); // battery
 });
