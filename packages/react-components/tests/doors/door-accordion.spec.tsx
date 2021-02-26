70db8703bb325eb582307124bc1d64096d27bf4d
Teo Koon Peng <koonpeng@openrobotics.org>
2021-02-26 15:14:05 +0800

    add smoke tests for different doors and states

--- packages/react-components/tests/doors/door-accordion.spec.tsx
+++ packages/react-components/tests/doors/door-accordion.spec.tsx
@@ -1,19 +1,84 @@
 import * as RomiCore from '@osrf/romi-js-core-interfaces';
-import { render, RenderResult } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { DoorAccordion } from '../../lib';
-import { makeDoor } from './test-utils';
+import { makeDoor, makeDoorState } from './test-utils';
 
-describe('Door-accordion', () => {
-  let fakeOnClick: ReturnType<typeof jasmine.createSpy>;
-  let root: RenderResult;
-  beforeEach(() => {
-    fakeOnClick = jasmine.createSpy();
-    root = render(<DoorAccordion door={makeDoor()} onDoorControlClick={fakeOnClick} />);
+describe('Door accordion', () => {
+  it('smoke test with different door types', () => {
+    [
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_DOUBLE_SLIDING,
+      }),
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_DOUBLE_SWING,
+      }),
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_DOUBLE_TELESCOPE,
+      }),
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_SINGLE_SLIDING,
+      }),
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_SINGLE_SWING,
+      }),
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_SINGLE_TELESCOPE,
+      }),
+      makeDoor({
+        door_type: RomiCore.Door.DOOR_TYPE_UNDEFINED,
+      }),
+      makeDoor({
+        door_type: -1,
+      }),
+    ].forEach((door) => {
+      render(<DoorAccordion door={door} />);
+      cleanup();
+    });
+  });
+
+  it('smoke test with different door modes', () => {
+    [
+      makeDoorState({
+        current_mode: { value: RomiCore.DoorMode.MODE_CLOSED },
+      }),
+      makeDoorState({
+        current_mode: { value: RomiCore.DoorMode.MODE_OPEN },
+      }),
+      makeDoorState({
+        current_mode: { value: RomiCore.DoorMode.MODE_MOVING },
+      }),
+      makeDoorState({
+        current_mode: { value: -1 },
+      }),
+    ].forEach((state) => {
+      render(<DoorAccordion door={makeDoor()} doorState={state} />);
+      cleanup();
+    });
+  });
+
+  it('smoke test with different motion direction', () => {
+    [
+      makeDoor({
+        motion_direction: 1,
+      }),
+      makeDoor({
+        motion_direction: -1,
+      }),
+      makeDoor({
+        motion_direction: 100,
+      }),
+    ].forEach((door) => {
+      render(<DoorAccordion door={door} />);
+      cleanup();
+    });
   });
 
   it('triggers door control dispatch when open door button is clicked', () => {
+    const fakeOnClick = jasmine.createSpy();
+    const root = render(<DoorAccordion door={makeDoor()} onDoorControlClick={fakeOnClick} />);
+
     userEvent.click(root.getByText('Open'));
     expect(fakeOnClick).toHaveBeenCalledWith(
       jasmine.anything(),
@@ -23,6 +88,9 @@ describe('Door-accordion', () => {
   });
 
   it('triggers door control dispatch when close door button is clicked', () => {
+    const fakeOnClick = jasmine.createSpy();
+    const root = render(<DoorAccordion door={makeDoor()} onDoorControlClick={fakeOnClick} />);
+
     userEvent.click(root.getByText('Close'));
     expect(fakeOnClick).toHaveBeenCalledWith(
       jasmine.anything(),
