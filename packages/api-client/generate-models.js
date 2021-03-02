ac43219f35a945f6906ada6972ae46c3dc4e7097
Teo Koon Peng <koonpeng@openrobotics.org>
2021-03-02 13:13:54 +0800

    generate all models

--- packages/api-client/generate-models.js
+++ packages/api-client/generate-models.js
@@ -1,19 +1,30 @@
 const jsonSchemaToTs = require('json-schema-to-typescript');
 const path = require('path');
-const fsp = require('fs').promises;
+const fs = require('fs');
+const { execSync } = require('child_process');
 
-const files = process.argv.slice(2);
-if (!files.length) {
-  console.log(`usage: ${path.basename(process.argv[1])} <files>...`);
-}
+console.log('generate schemas:');
+execSync(`python3 -m pipenv run python ${__dirname}/generate-schemas.py`, { stdio: 'inherit' });
 
+const schemas = [
+  'build/schema/door-health.json',
+  'build/schema/door-state.json',
+  'build/schema/door.json',
+];
+
+const modelsDir = path.join(__dirname, 'lib', 'models');
+fs.rmdirSync(modelsDir, { recursive: true, force: true });
+fs.mkdirSync(modelsDir, { recursive: true });
+
+console.log('generate models:');
 (async () => {
-  for (const f of files) {
+  for (const f of schemas) {
     const ts = await jsonSchemaToTs.compileFromFile(f);
     const baseName = path.basename(f);
     const tsFileName = `${baseName.slice(0, baseName.lastIndexOf('.'))}.ts`;
     const tsFilePath = path.join(__dirname, 'lib', 'models', tsFileName);
-    fsp.writeFile(tsFilePath, ts);
+    fs.writeFileSync(tsFilePath, ts);
     console.log(tsFilePath);
   }
+  fs.writeFileSync(path.join(modelsDir, 'GENERATED'), 'THIS DIRECTORY IS GENERATED, DO NOT EDIT!!');
 })();
