3f8208a32a0594569b8f84a85cffb34b81824b63
Teo Koon Peng <koonpeng@openrobotics.org>
2021-03-15 10:31:54 +0800

    increase timeout even more
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard/e2e/tests/negotiations.test.ts
+++ packages/dashboard/e2e/tests/negotiations.test.ts
@@ -32,7 +32,7 @@ describe('Loop request for negotiations', () => {
     const subTreeItem = treeItem.$('[data-component=TreeItem]');
     expect(subTreeItem).toBeVisible();
     subTreeItem.click();
-  }).timeout(60000);
+  }).timeout(600000);
 
   // Disabled till we find out a way to locate that floor icon
   // Also figure out a way for a non-rejected negotiation status click
