46bab7ac13fe82d3c669baf2fd04444bb37d9d73
Morgan Quigley <morgan@osrfoundation.org>
2021-03-15 09:58:00 +0800

    remove tiny css file and replace with css-in-js for layout-manager
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- packages/dashboard/src/components/layout-manager/layout-manager.tsx
+++ packages/dashboard/src/components/layout-manager/layout-manager.tsx
@@ -1,11 +1,20 @@
 import { Mosaic, MosaicWindow, MosaicBranch } from 'react-mosaic-component';
 import 'react-mosaic-component/react-mosaic-component.css';
-import './layout-manager.css';
+import { makeStyles } from '@material-ui/core';
 import Debug from 'debug';
 import React from 'react';
 import BuildingMapWindow from './building-map-window';
 
 const debug = Debug('LayoutManager');
+
+const useStyles = makeStyles({
+  layoutManager: {
+    width: '100%',
+    height: '100%',
+    margin: '0px',
+    padding: '0px',
+  },
+});
 
 export default function LayoutManager(_props: {}): React.ReactElement {
   debug('render');
@@ -26,8 +35,9 @@ export default function LayoutManager(_props: {}): React.ReactElement {
     }
   }
 
+  const classes = useStyles();
   return (
-    <div id="layout_manager">
+    <div className={classes.layoutManager}>
       <Mosaic<string>
         renderTile={(id, path) => create_tile(id, path)}
         resize={{ minimumPaneSizePercentage: 5 }}
