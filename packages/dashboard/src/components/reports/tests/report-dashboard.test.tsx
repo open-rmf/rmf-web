255a861be096ccd36c23882000a47b5729fdab5a
Matias Bavera <matiasbavera@gmail.com>
2021-03-16 17:40:23 -0300

    removed leftover
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- packages/dashboard/src/components/reports/tests/report-dashboard.test.tsx
+++ packages/dashboard/src/components/reports/tests/report-dashboard.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, cleanup, RenderResult } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { ReportDashboard } from '../report-dashboard';
