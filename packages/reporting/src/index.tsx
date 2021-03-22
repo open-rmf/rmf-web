3c8904cdaee02372e69c8902b5e03bbccb234823
Matias Bavera <matiasbavera@gmail.com>
2021-03-22 14:48:18 -0300

    Removed report vital web
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- packages/reporting/src/index.tsx
+++ packages/reporting/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -15,8 +14,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
