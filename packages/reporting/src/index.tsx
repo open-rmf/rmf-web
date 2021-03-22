0da06259aa0bdf8a3e5f0c623cd362ffc7153e37
Matias Bavera <matiasbavera@gmail.com>
2021-03-22 14:11:58 -0300

    Removed cache comment
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- packages/reporting/src/index.tsx
+++ packages/reporting/src/index.tsx
@@ -11,9 +11,6 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// NOTE: There is no point to have the app work offline as all the data must come from rmf/romi and
-// cannot be cached.
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
