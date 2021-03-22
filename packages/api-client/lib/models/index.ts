7d4824a54e88d1e524c41811f8f6c8ea6a5e73fe
Teo Koon Peng <koonpeng@openrobotics.org>
2021-03-22 12:37:32 +0800

    fix typings
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/api-client/lib/models/index.ts
+++ packages/api-client/lib/models/index.ts
@@ -3,33 +3,17 @@ import type {
   BuildingMap as BaseBuildingMap,
   Level as BaseLevel,
 } from '@osrf/romi-js-core-interfaces';
-import { DispenserState as DispenserStateModel } from './generated/dispenser-state';
-import { DoorState as DoorStateModel } from './generated/door-state';
-import { FleetState as FleetStateModel } from './generated/fleet-state';
-import { IngestorState as IngestorStateModel } from './generated/ingestor-state';
-import { LiftState as LiftStateModel } from './generated/lift-state';
-import { DispenserState as RmfDispenserState } from './generated/ros/rmf_dispenser_msgs/msg/DispenserState';
-import { DoorState as RmfDoorState } from './generated/ros/rmf_door_msgs/msg/DoorState';
-import { FleetState as RmfFleetState } from './generated/ros/rmf_fleet_msgs/msg/FleetState';
-import { IngestorState as RmfIngestorState } from './generated/ros/rmf_ingestor_msgs/msg/IngestorState';
-import { LiftState as RmfLiftState } from './generated/ros/rmf_lift_msgs/msg/LiftState';
-import { TaskSummary as RmfTaskSummary } from './generated/ros/rmf_task_msgs/msg/TaskSummary';
-import { TaskSummary as TaskSummaryModel } from './generated/task-summary';
-
 export { DispenserHealth } from './generated/dispenser-health';
 export { DoorHealth } from './generated/door-health';
 export { IngestorHealth } from './generated/ingestor-health';
 export { LiftHealth } from './generated/lift-health';
 export { RobotHealth } from './generated/robot-health';
-
-type RmfJsonWrapperT<ModelT, RmfT> = Omit<ModelT, 'data'> & { data: RmfT };
-
-export type DoorState = RmfJsonWrapperT<DoorStateModel, RmfDoorState>;
-export type LiftState = RmfJsonWrapperT<LiftStateModel, RmfLiftState>;
-export type DispenserState = RmfJsonWrapperT<DispenserStateModel, RmfDispenserState>;
-export type IngestorState = RmfJsonWrapperT<IngestorStateModel, RmfIngestorState>;
-export type FleetState = RmfJsonWrapperT<FleetStateModel, RmfFleetState>;
-export type TaskSummary = RmfJsonWrapperT<TaskSummaryModel, RmfTaskSummary>;
+export { DispenserState } from './generated/ros/rmf_dispenser_msgs/msg/DispenserState';
+export { DoorState } from './generated/ros/rmf_door_msgs/msg/DoorState';
+export { FleetState } from './generated/ros/rmf_fleet_msgs/msg/FleetState';
+export { IngestorState } from './generated/ros/rmf_ingestor_msgs/msg/IngestorState';
+export { LiftState } from './generated/ros/rmf_lift_msgs/msg/LiftState';
+export { TaskSummary } from './generated/ros/rmf_task_msgs/msg/TaskSummary';
 
 export interface AffineImage extends Omit<BaseAffineImage, 'data'> {
   data: string;
