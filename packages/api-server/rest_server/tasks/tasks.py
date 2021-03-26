37854f398989e8ca3b944be684ce908c8fef490b
ChawinTan <chawin15@gmail.com>
2021-03-26 10:04:25 +0800

    get task list
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/api-server/rest_server/tasks/tasks.py
+++ packages/api-server/rest_server/tasks/tasks.py
@@ -1,25 +1,9 @@
-import argparse
-import json
-import logging
-import math
-import os
-import sys
-import time
-from threading import Thread
-
 import rclpy
-import yaml
 from fastapi import APIRouter, Request
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
 from rclpy.node import Node
-from rclpy.parameter import Parameter
-from rclpy.qos import QoSProfile, qos_profile_system_default
-from rclpy.time import Time
-from rmf_fleet_msgs.msg import FleetState
 from rmf_task_msgs.msg import Delivery, Loop, TaskType
-from rmf_task_msgs.srv import SubmitTask
+from rmf_task_msgs.srv import GetTaskList, SubmitTask
 
 # from typing import Optional
 
@@ -30,18 +14,13 @@ class DispatcherClient(Node):
     def __init__(self):
         super().__init__("dispatcher_client")
         self.submit_task_srv = self.create_client(SubmitTask, "/submit_task")
-        counter = 10
+        self.get_tasks_srv = self.create_client(GetTaskList, "/get_tasks")
 
-        print("starting up....... the dispatcher")
-        print(self.submit_task_srv.wait_for_service(timeout_sec=1.0))
+        self.active_tasks_cache = []
+        self.terminated_tasks_cache = []
 
-        while (
-            not self.submit_task_srv.wait_for_service(timeout_sec=1.0) and counter > 0
-        ):
+        while not self.submit_task_srv.wait_for_service(timeout_sec=1.0):
             self.get_logger().warn("Dispatcher node is not avail, waiting...")
-            counter -= 1
-
-        print(self.submit_task_srv.wait_for_service(timeout_sec=1.0))
 
     def submit_task_request(self, req_msg) -> str:
         """
@@ -123,9 +102,117 @@ class DispatcherClient(Node):
             return None, str(ex)
         return req_msg, ""
 
+    def get_task_status(self):
+        """
+        Get all task status - This fn will trigger a ros srv call to acquire
+        all submitted tasks to dispatcher node. Fn returns an object of tasks
+        """
+        req = GetTaskList.Request()
+        try:
+            future = self.get_tasks_srv.call_async(req)
+            rclpy.spin_until_future_complete(self, future, timeout_sec=0.5)
+            response = future.result()
+            if response is None:
+                return self.active_tasks_cache + self.terminated_tasks_cache
+            else:
+                active_tasks = self.__convert_task_status_msg(
+                    response.active_tasks, False
+                )
+                terminated_tasks = self.__convert_task_status_msg(
+                    response.terminated_tasks, True
+                )
+                self.active_tasks_cache = active_tasks
+                self.terminated_tasks_cache = terminated_tasks
+                return active_tasks + terminated_tasks
+        except Exception as e:
+            self.get_logger().error("Error! GetTasks Srv failed %r" % (e,))
+        return []
+
+    def __convert_task_status_msg(self, task_summaries, is_done=True):
+        """
+        convert task summary msg and return a jsonify-able task status obj
+        """
+        states_enum = {
+            0: "Queued",
+            1: "Active/Executing",
+            2: "Completed",
+            3: "Failed",
+            4: "Cancelled",
+            5: "Pending",
+        }
+        type_enum = {
+            0: "Station",
+            1: "Loop",
+            2: "Delivery",
+            3: "Charging",
+            4: "Clean",
+            5: "Patrol",
+        }
+
+        status_list = []
+        rclpy.spin_once(self, timeout_sec=0.0)
+        now = self.get_clock().now().to_msg().sec  # only use sec
+        for task in task_summaries:
+            desc = task.task_profile.description
+            status = {}
+            status["task_id"] = task.task_id
+            status["state"] = states_enum[task.state]
+            status["done"] = is_done
+            status["fleet_name"] = task.fleet_name
+            status["robot_name"] = task.robot_name
+            status["task_type"] = type_enum[desc.task_type.type]
+            status["priority"] = desc.priority.value
+            status["submited_start_time"] = desc.start_time.sec
+            status["start_time"] = task.start_time.sec  # only use sec
+            status["end_time"] = task.end_time.sec  # only use sec
+
+            if status["task_type"] == "Clean":
+                status["description"] = desc.clean.start_waypoint
+            elif status["task_type"] == "Loop":
+                status["description"] = (
+                    desc.loop.start_name
+                    + " --> "
+                    + desc.loop.finish_name
+                    + " x"
+                    + str(desc.loop.num_loops)
+                )
+            elif status["task_type"] == "Delivery":
+                status["description"] = (
+                    desc.delivery.pickup_place_name
+                    + " --> "
+                    + desc.delivery.dropoff_place_name
+                )
+            elif status["task_type"] == "Charging":
+                status["description"] = "Back to Charging Station"
+
+            # Current hack to generate a progress percentage
+            duration = abs(task.end_time.sec - task.start_time.sec)
+            # check if is completed
+            if is_done or task.state == 3:
+                status["progress"] = "100%"
+            # check if it state is queued/cancelled
+            elif duration == 0 or (task.state in [0, 4]):
+                status["progress"] = "0%"
+            else:
+                percent = int(100 * (now - task.start_time.sec) / float(duration))
+                if percent < 0:
+                    status["progress"] = "0%"
+                elif percent > 100:
+                    status["progress"] = "Delayed"
+                else:
+                    status["progress"] = f"{percent}%"
+            status_list.insert(0, status)  # insert front
+        return status_list
+
 
 rclpy.init(args=None)
 dispatcher_client = DispatcherClient()
+
+
+@router.get("/task_list")
+async def get_tasks():
+    tasks = dispatcher_client.get_task_status()
+    return JSONResponse(content=tasks)
 
 
 @router.post("/submit_task")
