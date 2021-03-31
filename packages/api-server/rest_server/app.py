42d82b2f2f73e848b86504de91c27393e0e8a1dd
Chawin Tan <chawin15@gmail.com>
2021-03-31 15:55:58 +0800

    feat(api-server): task management (#291)
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/api-server/rest_server/app.py
+++ packages/api-server/rest_server/app.py
@@ -1,13 +1,10 @@
-import asyncio
 import logging
 import os
 import sys
 
-import socketio
 from fastapi import FastAPI
 
-from .app_config import app_config
-from .building_map import building_map_router
+from .tasks import tasks
 
 logger = logging.getLogger("rest_app")
 handler = logging.StreamHandler(sys.stdout)
@@ -19,33 +16,6 @@ else:
     logger.setLevel(logging.INFO)
 
 app = FastAPI()
-sio = socketio.AsyncClient()
 
-
-@app.on_event("startup")
-async def on_startup():
-    await sio.connect(app_config.api_server_url)
-    connected = asyncio.Future()
-    sio.on("connect", lambda: connected.set_result(True))
-    await connected
-    del sio.handlers["/"]["connect"]
-
-    app.include_router(
-        building_map_router(sio, logger.getChild("building_map")),
-        prefix="/building_map",
-    )
-
-    logger.info("started app")
-
-
-# There is a bug with uvicorn and socketio that causes
-# `RuntimeError: Event loop stopped before Future completed.`
-# when getting SIGINT, the reason is because both uvicorn and socketio installs signal
-# handlers, socketio's handler overwrites uvicorn's and in its handler to calls `asyncio.get_event_loop().stop()`.
-# This forces the event loop to stop before uvicorn completes its work, hence the error appears.
-# The side effect is that the shutdown handler is never called because the loop has
-# been stopped.
-@app.on_event("shutdown")
-async def on_shutdown():
-    await sio.disconnect()
-    logger.info("shutdown app")
+app.include_router(tasks.router)
+logger.info("started app")
