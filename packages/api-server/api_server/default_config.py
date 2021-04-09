ed9430ddf42623fb9f36a50d283efd30288cc2d0
Teo Koon Peng <koonpeng@openrobotics.org>
2021-04-09 18:25:45 +0800

    fix default public url
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/api-server/api_server/default_config.py
+++ packages/api-server/api_server/default_config.py
@@ -6,7 +6,7 @@ config = {
     # url that rmf-server is being served on.
     # When being a proxy, this must be the url that rmf-server is mounted on.
     # E.g. https://example.com/rmf/api/v1
-    "public_url": "",
+    "public_url": "http://localhost:8000",
     "static_directory": "static",  # The directory where static files should be stored.
     "log_level": "WARNING",  # https://docs.python.org/3.8/library/logging.html#levels
     # path to a PEM encoded RSA public key which is used to verify JWT tokens, if the path is relative, it is based on the working dir.
