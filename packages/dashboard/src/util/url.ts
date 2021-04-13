8182f650c5563bdac603a9c01326e85c38f70e2e
vallq <vallq@outlook.com>
2021-04-13 09:59:24 +0800

    add navigation bar to appintrinsics and update routes
    Signed-off-by: vallq <val@openrobotics.org>

--- packages/dashboard/src/util/url.ts
+++ packages/dashboard/src/util/url.ts
@@ -3,3 +3,7 @@ import { normalizePath } from 'rmf-auth';
 export const BASE_PATH = normalizePath(process.env.PUBLIC_URL || '/');
 export const DASHBOARD_ROUTE = BASE_PATH;
 export const LOGIN_ROUTE = normalizePath(`${BASE_PATH}/login`);
+export const BUILDING_ROUTE = normalizePath(`${BASE_PATH}/building`);
+export const ROBOTS_ROUTE = normalizePath(`${BASE_PATH}/robots`);
+export const TASKS_ROUTE = normalizePath(`${BASE_PATH}/tasks`);
+export const LOGS_ROUTE = normalizePath(`${BASE_PATH}/logs`);
