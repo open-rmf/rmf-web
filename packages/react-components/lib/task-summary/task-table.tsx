8e4e9f3041ffe00dfbfaabb2d7ec624801973386
Teo Koon Peng <koonpeng@openrobotics.org>
2021-04-13 12:55:30 +0800

    add button to create task
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/react-components/lib/task-summary/task-table.tsx
+++ packages/react-components/lib/task-summary/task-table.tsx
@@ -1,4 +1,5 @@
 import {
+  IconButton,
   makeStyles,
   Paper,
   Table,
@@ -7,17 +8,40 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Toolbar,
+  Typography,
 } from '@material-ui/core';
 import React from 'react';
 import * as RmfModels from 'rmf-models';
 import { taskStateToStr } from './utils';
 import { TaskPhases } from './task-phases';
+import AddOutlinedIcon from '@material-ui/icons/AddCircle';
 
 const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 650,
+  },
+  title: {
+    flex: '1 1 100%',
+  },
   phasesCell: {
-    padding: `0 ${theme.spacing(1)}px 0 ${theme.spacing(1)}px`,
+    padding: `0 ${theme.spacing(1)}px`,
   },
 }));
+
+function TableToolbar() {
+  const classes = useStyles();
+  return (
+    <Toolbar>
+      <Typography className={classes.title} variant="h6">
+        Tasks
+      </Typography>
+      <IconButton>
+        <AddOutlinedIcon />
+      </IconButton>
+    </Toolbar>
+  );
+}
 
 export interface TaskTableProps {
   tasks: RmfModels.TaskSummary[];
@@ -27,36 +51,39 @@ export function TaskTable({ tasks }: TaskTableProps): JSX.Element {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table stickyHeader={true} size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Task Id</TableCell>
-            <TableCell>Assignee</TableCell>
-            <TableCell>Start Time</TableCell>
-            <TableCell>End Time</TableCell>
-            <TableCell>State</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tasks.map((task) => (
-            <React.Fragment key={task.task_id}>
-              <TableRow key={task.task_id}>
-                <TableCell>{task.task_id}</TableCell>
-                <TableCell>{task.robot_name}</TableCell>
-                <TableCell>{task.start_time.sec}</TableCell>
-                <TableCell>{task.end_time.sec}</TableCell>
-                <TableCell>{taskStateToStr(task.state)}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className={classes.phasesCell} colSpan={5}>
-                  <TaskPhases taskSummary={task}></TaskPhases>
-                </TableCell>
-              </TableRow>
-            </React.Fragment>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Paper>
+      <TableToolbar />
+      <TableContainer>
+        <Table className={classes.table} stickyHeader={true} size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Task Id</TableCell>
+              <TableCell>Assignee</TableCell>
+              <TableCell>Start Time</TableCell>
+              <TableCell>End Time</TableCell>
+              <TableCell>State</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tasks.map((task) => (
+              <React.Fragment key={task.task_id}>
+                <TableRow key={task.task_id}>
+                  <TableCell>{task.task_id}</TableCell>
+                  <TableCell>{task.robot_name}</TableCell>
+                  <TableCell>{task.start_time.sec}</TableCell>
+                  <TableCell>{task.end_time.sec}</TableCell>
+                  <TableCell>{taskStateToStr(task.state)}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className={classes.phasesCell} colSpan={5}>
+                    <TaskPhases taskSummary={task}></TaskPhases>
+                  </TableCell>
+                </TableRow>
+              </React.Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
