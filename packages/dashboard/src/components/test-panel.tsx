9e3b5b22e1a03174b2cefbf17209c2e48fb791a3
vallq <vallq@outlook.com>
2021-04-13 10:01:41 +0800

    add temporary placeholder panels
    Signed-off-by: vallq <val@openrobotics.org>

---
+++ packages/dashboard/src/components/test-panel.tsx
@@ -0,0 +1,20 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+//To be removed
+
+export const TestBuildingPanel = (): React.ReactElement => {
+  return <Typography variant="h5">This is a new Tab Content for BUILDING MANAGEMENT</Typography>;
+};
+
+export const TestRobotsPanel = (): React.ReactElement => {
+  return <Typography variant="h5">This is a new Tab Content for ROBOTS</Typography>;
+};
+
+export const TestTasksPanel = (): React.ReactElement => {
+  return <Typography variant="h5">This is a new Tab Content for TASKS</Typography>;
+};
+
+export const TestLogsPanel = (): React.ReactElement => {
+  return <Typography variant="h5">This is a new Tab Content for 7-DAY LOGS</Typography>;
+};
