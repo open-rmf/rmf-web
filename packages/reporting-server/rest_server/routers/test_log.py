d909238e25a67e131a95cd4902bd63d807b11d3e
Matias Bavera <matiasbavera@gmail.com>
2021-04-14 14:43:21 -0400

    Added test for log router
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

---
+++ packages/reporting-server/rest_server/routers/test_log.py
@@ -0,0 +1,34 @@
+import unittest
+
+from fastapi.testclient import TestClient
+from tortoise import Tortoise
+
+from ..app import app
+
+
+class TestLogRoute(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        await Tortoise.init(
+            db_url="sqlite://:memory:",
+            modules={"models": ["models.raw_log"]},
+        )
+        await Tortoise.generate_schemas()
+        self.client = TestClient(app)
+
+    async def asyncTearDown(self):
+        await Tortoise.close_connections()
+
+    def test_log_raw_creation(self):
+        response = self.client.post(
+            "/log/raw/",
+            json=[{"log": "test"}],
+        )
+        assert response.status_code == 201
+
+    def test_error_on_bad_body(self):
+        self.client = TestClient(app)
+        response = self.client.post(
+            "/log/raw/",
+            json=[{"log343": "test"}],
+        )
+        assert response.status_code == 503
