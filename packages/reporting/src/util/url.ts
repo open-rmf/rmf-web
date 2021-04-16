9990905c1a8ac351bb2e12028f25dd733efa7679
Matias Bavera <matiasbavera@gmail.com>
2021-04-16 11:32:17 -0400

    Added new dashboard route to monitoring
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- packages/reporting/src/util/url.ts
+++ packages/reporting/src/util/url.ts
@@ -1,4 +1,4 @@
 import { normalizePath } from 'rmf-auth';
-
-export const DASHBOARD_ROUTE = ``;
+export const BASE_PATH = normalizePath(process.env.PUBLIC_URL || '/');
+export const DASHBOARD_ROUTE = BASE_PATH;
 export const LOGIN_ROUTE = normalizePath(`/login`);
