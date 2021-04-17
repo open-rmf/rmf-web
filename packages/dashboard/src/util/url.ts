e257398bd7abad28bb7f1b0812d00375ed66ae1a
vallq <vallq@outlook.com>
2021-04-17 17:40:08 +0800

    add border to divide
    Signed-off-by: vallq <val@openrobotics.org>

--- packages/dashboard/src/util/url.ts
+++ packages/dashboard/src/util/url.ts
@@ -3,9 +3,5 @@ import { normalizePath } from 'rmf-auth';
 export const BASE_PATH = normalizePath(process.env.PUBLIC_URL || '/');
 export const DASHBOARD_ROUTE = BASE_PATH;
 export const LOGIN_ROUTE = normalizePath(`${BASE_PATH}/login`);
-export const BUILDING_ROUTE = normalizePath(`${BASE_PATH}/building`);
-export const ROBOTS_ROUTE = normalizePath(`${BASE_PATH}/robots`);
-export const TASKS_ROUTE = normalizePath(`${BASE_PATH}/tasks`);
-export const LOGS_ROUTE = normalizePath(`${BASE_PATH}/logs`);
 
-export const TAB_NAMES = ['Overview'];
+export const TAB_NAMES = ['Building'];
