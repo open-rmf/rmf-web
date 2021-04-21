3d354169d2314c4b304b909d8f5b7e1dabc540e9
Teo Koon Peng <koonpeng@openrobotics.org>
2021-04-21 13:56:50 +0800

    add package.json
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/ros_translator/ros_translator/typescript/test/test.sh
+++ packages/ros_translator/ros_translator/typescript/test/test.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)
 rm -rf out
 pipenv run python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
 echo 'test build'
-node ./node_modules/.bin/tsc --noEmit
+node ../../../node_modules/.bin/tsc --noEmit
 echo 'ok'
-node ./node_modules/.bin/ts-node -T ./node_modules/.bin/jasmine "$@"
+node ../../../node_modules/.bin/ts-node -T ../../../node_modules/.bin/jasmine "$@"
