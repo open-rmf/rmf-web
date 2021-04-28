736eaedb583638c52a5e657e2878d8c192384302
vallq <val@openrobotics.org>
2021-04-28 11:53:36 +0800

    add tabs panel component
    Signed-off-by: vallq <val@openrobotics.org>

---
+++ packages/react-components/lib/tab-panel.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+export interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+export function TabPanel(props: TabPanelProps): JSX.Element {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
