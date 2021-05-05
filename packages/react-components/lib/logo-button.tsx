9c45fddacefc6a93b3e550903660b4aa5ece5d4a
vallq <val@openrobotics.org>
2021-05-05 18:25:25 +0800

    add logo button component
    Signed-off-by: vallq <val@openrobotics.org>

---
+++ packages/react-components/lib/logo-button.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, createStyles, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      '&$disabled': {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.main,
+      },
+    },
+    disabled: {},
+    logo: {
+      maxWidth: 120,
+      opacity: 1,
+    },
+  }),
+);
+
+export interface LogoButtonProps {
+  logoPath?: string;
+  onClick?: () => void;
+}
+
+export const LogoButton = (props: LogoButtonProps): JSX.Element => {
+  const { logoPath, onClick } = props;
+  const classes = useStyles();
+
+  if (onClick) {
+    return (
+      <Button disableElevation color="primary" variant="contained" onClick={onClick}>
+        <img src={logoPath} style={{ width: '120px' }} alt="logo" />
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        disableElevation
+        color="primary"
+        variant="contained"
+        classes={{
+          root: classes.root,
+          disabled: classes.disabled,
+        }}
+        disabled
+      >
+        <img src={logoPath} style={{ width: '120px' }} alt="logo" />
+      </Button>
+    );
+  }
+};
