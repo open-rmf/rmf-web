b8bad1254622a53a4ee65418bbc5b7488f044148
Teo Koon Peng <koonpeng@openrobotics.org>
2021-05-11 17:07:34 +0800

    downgrade to webpack4, storybook does not support webpack5
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/react-components/karma.conf.js
+++ packages/react-components/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = (config) => {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'webpack'],
+    frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
