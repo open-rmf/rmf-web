20b29d373c84beedf5bcdb4556245f9019dffc36
Teo Koon Peng <koonpeng@openrobotics.org>
2021-05-14 10:19:27 +0800

    update config
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard/e2e/rmf_server_config.py
+++ packages/dashboard/e2e/rmf_server_config.py
@@ -1,15 +1,16 @@
 import os
-from copy import deepcopy
 
-from api_server.default_config import config as default_config
+from api_server.default_config import config
 
-config = deepcopy(default_config)
-config["jwt_public_key"] = "certs/keycloak.pub"
-config[
-    "oidc_url"
-] = "http://localhost:8088/auth/realms/master/.well-known/openid-configuration"
-config["aud"] = "rmf-dashboard"
 if "CI" in os.environ:
-    config["iss"] = "http://rmf-web_keycloak_1:8080/auth/realms/master"
+    iss = "http://rmf-web_keycloak_1:8080/auth/realms/master"
 else:
-    config["iss"] = "http://localhost:8088/auth/realms/master"
+    iss = "http://localhost:8088/auth/realms/master"
+config.update(
+    {
+        "jwt_public_key": "certs/keycloak.pub",
+        "oidc_url": "http://localhost:8088/auth/realms/master/.well-known/openid-configuration",
+        "aud": "rmf-dashboard",
+        "iss": iss,
+    }
+)
