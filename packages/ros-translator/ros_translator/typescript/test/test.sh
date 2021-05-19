2c30e7f28c895c2712aa20d37e4be49d82b5eef9
Teo Koon Peng <koonpeng@openrobotics.org>
2021-05-19 17:55:47 +0800

    try setting pipenv max depth
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/ros-translator/ros_translator/typescript/test/test.sh
+++ packages/ros-translator/ros_translator/typescript/test/test.sh
@@ -3,7 +3,7 @@ set -e
 
 cd $(dirname $0)
 rm -rf out
-pipenv run python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
+PIPENV_MAX_DEPTH=6 pipenv run python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
 echo 'test build'
 npx tsc --noEmit
 echo 'ok'
--- packages/ros-translator/test.sh
+++ packages/ros-translator/test.sh
@@ -8,4 +8,4 @@ colcon build
 source install/setup.bash
 popd
 
-PIPENV_MAX_DEPTH=6 ros_translator/typescript/test/test.sh
+ros_translator/typescript/test/test.sh
