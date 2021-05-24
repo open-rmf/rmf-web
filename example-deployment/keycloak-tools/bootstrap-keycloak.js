2869b78724caed57a409f7b1bc78b0e5d09768c7
Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2021-05-24 21:28:48 +0800

    Modify bootstrap file to automatically add dashboard client scope
    Signed-off-by: Boon Han <charayaphan.nakorn.boon.han@gmail.com>

--- example-deployment/keycloak-tools/bootstrap-keycloak.js
+++ example-deployment/keycloak-tools/bootstrap-keycloak.js
@@ -31,9 +31,9 @@ function authHeaders(token) {
       },
       {
         clientId: 'dashboard',
-        rootUrl: 'https://example.com',
-        redirectUris: ['https://example.com/*'],
-        webOrigins: ['https://example.com'],
+        rootUrl: 'https://openrobotics.demo.open-rmf.org',
+        redirectUris: ['https://openrobotics.demo.open-rmf.org/*'],
+        webOrigins: ['https://openrobotics.demo.open-rmf.org'],
         publicClient: true,
       },
     );
@@ -46,9 +46,9 @@ function authHeaders(token) {
       },
       {
         clientId: 'reporting',
-        rootUrl: 'https://example.com',
-        redirectUris: ['https://example.com/*'],
-        webOrigins: ['https://example.com'],
+        rootUrl: 'https://openrobotics.demo.open-rmf.org',
+        redirectUris: ['https://openrobotics.demo.open-rmf.org/*'],
+        webOrigins: ['https://openrobotics.demo.open-rmf.org'],
         publicClient: true,
       },
     );
@@ -97,7 +97,69 @@ function authHeaders(token) {
         eventsListeners: ['jsonlog_event_listener'],
       },
     );
+
+    // create audience client scope
+    await request(
+      `${baseUrl}/admin/realms/rmf-web/client-scopes`,
+      {
+        method: 'POST',
+        headers: authHeaders(token),
+      },
+      {
+        name: 'dashboard',
+	protocol: 'openid-connect',
+	description: 'dashboard scope',
+	protocolMappers: [
+	  {
+	    config: {
+	      "access.token.claim": "true",
+	      "id.token.claim": "false",
+	      "included.client.audience": "dashboard"
+	    },
+	    name: 'rmf-audience',
+ 	    protocol: 'openid-connect',
+  	    protocolMapper: 'oidc-audience-mapper'
+	  }
+	]
+      },
+    );
+
+    // get dashboard id ( not clientid )
+    resp = await request(`${baseUrl}/admin/realms/rmf-web/clients`, {
+      method: 'GET',
+      headers: authHeaders(token),
+    });
+
+    const client_array = JSON.parse(resp.body);
+    const dashboard_id = client_array.filter(function(item){
+	    return item.clientId == "dashboard";
+    })[0].id
+
+    // get client scope id 
+    resp = await request(`${baseUrl}/admin/realms/rmf-web/client-scopes`, {
+      method: 'GET',
+      headers: authHeaders(token),
+    });
+
+    const client_scopes_array = JSON.parse(resp.body);
+    const client_scope_id = client_scopes_array.filter(function(item){
+	    return item.name == "dashboard";
+    })[0].id
+    console.log(client_scope_id)
+
+    // set client with client scope
+    await request(
+      `${baseUrl}/admin/realms/rmf-web/clients/${dashboard_id}/default-client-scopes/${client_scope_id}`,
+      {
+        method: 'PUT',
+        headers: authHeaders(token),
+      }
+    );
+   
+
   } catch (e) {
     process.exitCode = 1;
   }
 })();
+
+
