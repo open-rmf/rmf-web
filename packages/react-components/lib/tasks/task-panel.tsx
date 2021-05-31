61e804094aaf6059c0d83dbbbca6d66dbaf452df
Teo Koon Peng <koonpeng@openrobotics.org>
2021-05-31 13:35:44 +0800

    more tests for create task form
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/react-components/lib/tasks/task-panel.tsx
+++ packages/react-components/lib/tasks/task-panel.tsx
@@ -78,6 +78,7 @@ export function TaskPanel({
     })();
   }, [fetchTasks, page]);
 
+  /* istanbul ignore next */
   const tasksFromFile = (): Promise<SubmitTask[]> => {
     return new Promise((res) => {
       const fileInputEl = uploadFileInputRef.current;
--- packages/react-components/tests/tasks/create-task.spec.tsx
+++ packages/react-components/tests/tasks/create-task.spec.tsx
@@ -1,6 +1,11 @@
 import { render, RenderResult, waitForElementToBeRemoved, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { CleanTaskDescription, LoopTaskDescription } from 'api-client';
+import type {
+  CleanTaskDescription,
+  DeliveryTaskDescription,
+  LoopTaskDescription,
+  SubmitTask,
+} from 'api-client';
 import React from 'react';
 import * as RmfModels from 'rmf-models';
 import { CreateTaskForm } from '../../lib';
@@ -9,33 +14,62 @@ import { makeSubmitTask } from './utils';
 const getTaskTypeEl = (root: RenderResult) => root.getByLabelText('Task Type');
 
 describe('CreateTaskForm', () => {
-  it('check fields are present', async () => {
-    const root = render(<CreateTaskForm open />);
-    root.getByLabelText('Start Time');
-    root.getByLabelText('Priority');
+  describe('submit tasks', () => {
+    it('is called with correct clean task data when form is submitted', async () => {
+      const spy = jasmine.createSpy().and.resolveTo(undefined);
+      const root = render(<CreateTaskForm open submitTasks={spy} />);
+      userEvent.click(getTaskTypeEl(root));
+      userEvent.click(root.getByRole('option', { name: 'Clean' }));
+      await waitForElementToBeRemoved(() => root.getByRole('option', { name: 'Clean' }));
+      userEvent.type(root.getByLabelText('Cleaning Zone'), 'test_zone');
+      userEvent.click(root.getByLabelText('Submit'));
 
-    // check fields are present in clean form
-    userEvent.click(getTaskTypeEl(root));
-    userEvent.click(root.getByRole('option', { name: 'Clean' }));
-    await waitForElementToBeRemoved(() => root.getByRole('option', { name: 'Clean' }));
-    root.getByLabelText('Cleaning Zone');
+      expect(spy).toHaveBeenCalledTimes(1);
+      const task = spy.calls.argsFor(0)[0][0] as SubmitTask;
+      expect((task.description as CleanTaskDescription).cleaning_zone).toBe('test_zone');
+    });
 
-    // check fields are present in loop form
-    userEvent.click(root.getByLabelText('Task Type'));
-    userEvent.click(root.getByRole('option', { name: 'Loop' }));
-    await waitForElementToBeRemoved(() => root.getByRole('option', { name: 'Clean' }));
-    root.getByLabelText('Start Location');
-    root.getByLabelText('Finish Location');
-    root.getByLabelText('Loops');
+    it('is called with correct loop task data when form is submitted', async () => {
+      const spy = jasmine.createSpy().and.resolveTo(undefined);
+      const root = render(<CreateTaskForm open submitTasks={spy} />);
+      userEvent.click(getTaskTypeEl(root));
+      userEvent.click(root.getByRole('option', { name: 'Loop' }));
+      await waitForElementToBeRemoved(() => root.getByRole('option', { name: 'Clean' }));
+      userEvent.type(root.getByLabelText('Start Location'), 'start');
+      userEvent.type(root.getByLabelText('Finish Location'), 'finish');
+      const loopsInput = root.getByLabelText('Loops');
+      userEvent.clear(loopsInput);
+      userEvent.type(loopsInput, '2');
+      userEvent.click(root.getByLabelText('Submit'));
 
-    // check fields are present in delivery form
-    userEvent.click(root.getByLabelText('Task Type'));
-    userEvent.click(root.getByRole('option', { name: 'Delivery' }));
-    await waitForElementToBeRemoved(() => root.getByRole('option', { name: 'Delivery' }));
-    root.getByLabelText('Pickup Location');
-    root.getByLabelText('Dispenser');
-    root.getByLabelText('Dropoff Location');
-    root.getByLabelText('Ingestor');
+      expect(spy).toHaveBeenCalledTimes(1);
+      const task = spy.calls.argsFor(0)[0][0] as SubmitTask;
+      const desc = task.description as LoopTaskDescription;
+      expect(desc.start_name).toBe('start');
+      expect(desc.finish_name).toBe('finish');
+      expect(desc.num_loops).toBe(2);
+    });
+
+    it('is called with correct delivery task data when form is submitted', async () => {
+      const spy = jasmine.createSpy().and.resolveTo(undefined);
+      const root = render(<CreateTaskForm open submitTasks={spy} />);
+      userEvent.click(getTaskTypeEl(root));
+      userEvent.click(root.getByRole('option', { name: 'Delivery' }));
+      await waitForElementToBeRemoved(() => root.getByRole('option', { name: 'Delivery' }));
+      userEvent.type(root.getByLabelText('Pickup Location'), 'pickup_location');
+      userEvent.type(root.getByLabelText('Dispenser'), 'pickup_dispenser');
+      userEvent.type(root.getByLabelText('Dropoff Location'), 'dropoff_location');
+      userEvent.type(root.getByLabelText('Ingestor'), 'dropoff_ingestor');
+      userEvent.click(root.getByLabelText('Submit'));
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      const task = spy.calls.argsFor(0)[0][0] as SubmitTask;
+      const desc = task.description as DeliveryTaskDescription;
+      expect(desc.pickup_place_name).toBe('pickup_location');
+      expect(desc.pickup_dispenser).toBe('pickup_dispenser');
+      expect(desc.dropoff_place_name).toBe('dropoff_location');
+      expect(desc.dropoff_ingestor).toBe('dropoff_ingestor');
+    });
   });
 
   it('onCancelClick is called when cancel button is clicked', () => {
@@ -45,14 +79,7 @@ describe('CreateTaskForm', () => {
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  it('submitTask is called when form is submitted', () => {
-    const spy = jasmine.createSpy().and.resolveTo(undefined);
-    const root = render(<CreateTaskForm open submitTasks={spy} />);
-    userEvent.click(root.getByLabelText('Submit'));
-    expect(spy).toHaveBeenCalledTimes(1);
-  });
-
-  it('onFail is called when submitTask fails', async () => {
+  it('onFail is called when submitTasks fails', async () => {
     const submitSpy = jasmine.createSpy().and.rejectWith(new Error('error!!'));
     const failSpy = jasmine.createSpy();
     const root = render(<CreateTaskForm open submitTasks={submitSpy} onFail={failSpy} />);
