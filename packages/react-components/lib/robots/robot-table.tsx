79446968605fdcfcc807f2a8e53bd5022564039d
vallq <val@openrobotics.org>
2021-06-02 19:51:07 +0800

    add box shadow and adjust padding for table components
    Signed-off-by: vallq <val@openrobotics.org>
    (cherry picked from commit 900d6b1e08e0647ca29186a1053d4143a0c18ff8)
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/react-components/lib/robots/robot-table.tsx
+++ packages/react-components/lib/robots/robot-table.tsx
@@ -31,12 +31,10 @@ const useStyles = makeStyles((theme) => ({
     background: theme.palette.action.hover,
   },
   infoRow: {
+    boxShadow: `${theme.shadows[1]}`,
     '& > *': {
       borderBottom: 'unset',
     },
-  },
-  phasesCell: {
-    padding: `0 ${theme.spacing(1)}px`,
   },
 }));
 
