1b844d192cc2bf48a6dc9657513082bc4d80129b
vallq <val@openrobotics.org>
2021-06-02 21:41:25 +0800

    cherry pick, exclude remove phase colors, include change font size
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/react-components/lib/tasks/task-table.tsx
+++ packages/react-components/lib/tasks/task-table.tsx
@@ -1,4 +1,12 @@
-import { makeStyles, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import {
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core';
 import clsx from 'clsx';
 import { formatDistanceToNow } from 'date-fns';
 import React from 'react';
@@ -47,11 +55,21 @@ function TaskRow({ task, onClick }: TaskRowProps) {
         onMouseOver={() => setHover(true)}
         onMouseOut={() => setHover(false)}
       >
-        <TableCell>{task.task_id}</TableCell>
-        <TableCell>{task.robot_name}</TableCell>
-        <TableCell>{toRelativeDate(task.start_time)}</TableCell>
-        <TableCell>{toRelativeDate(task.end_time)}</TableCell>
-        <TableCell>{taskStateToStr(task.state)}</TableCell>
+        <TableCell>
+          <Typography variant="body1">{task.task_id}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">{task.robot_name}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">{toRelativeDate(task.start_time)}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">{toRelativeDate(task.end_time)}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography variant="body1">{taskStateToStr(task.state)}</Typography>
+        </TableCell>
       </TableRow>
       <TableRow
         className={clsx(hover && classes.taskRowHover)}
