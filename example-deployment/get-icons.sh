55c4b7e7128653dcec5cbd5f7bc0792f0f722348
Matias Bavera <matiasbavera@gmail.com>
2021-06-02 22:27:51 -0400

    Added HOME_RMF_WEB and ICONS_FOLDER as env vars
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- example-deployment/get-icons.sh
+++
@@ -1,5 +0,0 @@
-
-cd ws/rmf-web/packages/dashboard
-echo -e '{"repoUrl":"https://github.com/open-rmf/rmf_demos.git","folder":"rmf_demos_dashboard_resources/office/","branch":"main"}' > .resources.json
-node ./scripts/setup/get-icons.js
-cd -
