642396ef3569be197bcad9a6f231352820a85462
Matias Bavera <matiasbavera@gmail.com>
2021-06-14 17:35:49 -0400

    dinamic postgres url
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- packages/reporting-server/test_migration/app.py
+++ packages/reporting-server/test_migration/app.py
@@ -1,5 +1,14 @@
+import os
+
+host = os.environ.get("POSTGRES_HOST", "localhost")
+port = os.environ.get("POSTGRES_PORT", "5432")
+user = os.environ.get("POSTGRES_USER", "postgres")
+password = os.environ.get("POSTGRES_PASSWORD", "postgres")
+database = os.environ.get("POSTGRES_DATABASE", "reporting")
+
+url = "postgres://" + user + ":" + password + "@" + host + ":" + port + "/" + database
 TORTOISE_ORM = {
-    "connections": {"default": "postgres://postgres:postgres@localhost:5432/reporting"},
+    "connections": {"default": url},
     "apps": {
         "models": {
             "models": ["models", "aerich.models"],
