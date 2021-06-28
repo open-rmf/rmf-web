3d3061baec450620a27c6b25420f3caf5159e495
Teo Koon Peng <koonpeng@openrobotics.org>
2021-06-28 16:46:13 +0800

    maybe fix e2e tests
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard-e2e/rmf_server_config.py
+++ packages/dashboard-e2e/rmf_server_config.py
@@ -10,6 +10,7 @@ config.update(
     {
         "jwt_public_key": "certs/keycloak.pub",
         "oidc_url": "http://localhost:8088/auth/realms/master/.well-known/openid-configuration",
+        "client_id": "rmf-dashboard",
         "aud": "rmf-dashboard",
         "iss": iss,
     }
