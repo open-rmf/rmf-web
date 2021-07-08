b0009819b37025c40966212c2396195392dfc3bb
Teo Koon Peng <koonpeng@openrobotics.org>
2021-07-08 10:08:00 +0800

    PositiveIntField overrides values and onchange to filter change events that does not led to new values
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/react-components/lib/form-inputs.tsx
+++ packages/react-components/lib/form-inputs.tsx
@@ -1,19 +1,37 @@
 import { TextField, TextFieldProps } from '@material-ui/core';
 import React from 'react';
 
-export type PositiveIntField = Omit<TextFieldProps, 'type' | 'inputProps'>;
+export interface PositiveIntField
+  extends Omit<TextFieldProps, 'type' | 'value' | 'inputProps' | 'onChange'> {
+  value: number;
+  onChange?(ev: React.ChangeEvent, int: number): void;
+}
 
 export function PositiveIntField(props: PositiveIntField): JSX.Element {
+  const [value, setValue] = React.useState(props.value.toString());
+
+  React.useEffect(() => {
+    setValue(props.value.toString());
+  }, [props.value]);
+
   return (
     <TextField
       {...props}
       type="number"
+      value={value}
       inputProps={{ min: 0 }}
       onKeyDown={(ev) => {
         if ('-+.'.indexOf(ev.key) >= 0) {
           ev.preventDefault();
         }
         props.onKeyDown && props.onKeyDown(ev);
+      }}
+      onChange={(ev) => {
+        const int = parseInt(ev.target.value);
+        if (int > 0) {
+          props.onChange && props.onChange(ev, int);
+        }
+        setValue(ev.target.value);
       }}
     />
   );
