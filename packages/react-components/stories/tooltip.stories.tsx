b26b4bc760065326254e9a9a122ec4eb1d6ae470
Teo Koon Peng <koonpeng@openrobotics.org>
2021-07-09 14:15:06 +0800

    fix storybook
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/react-components/stories/tooltip.stories.tsx
+++ packages/react-components/stories/tooltip.stories.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
 import { Typography } from '@material-ui/core';
-import { Story, Meta } from '@storybook/react';
-import { DashboardTooltip } from '../lib';
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { Tooltip } from '../lib';
 
 export default {
   title: 'Tooltip',
-  component: DashboardTooltip,
+  component: Tooltip,
 } as Meta;
 
 export const SimpleTooltip: Story = (args) => {
   return (
-    <DashboardTooltip title="This is a tooltip" id="test" enabled={true} {...args}>
+    <Tooltip title="This is a tooltip" id="test" enabled={true} {...args}>
       <Typography variant="h5">Hover over me</Typography>
-    </DashboardTooltip>
+    </Tooltip>
   );
 };
