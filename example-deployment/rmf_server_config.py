a3ac9ffb72854bcca06ee3f701ef05464be71e64
Teo Koon Peng <koonpeng@openrobotics.org>
2021-07-19 12:40:23 +0800

    make example user an admin
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- example-deployment/rmf_server_config.py
+++ example-deployment/rmf_server_config.py
@@ -1,16 +1,16 @@
-from copy import deepcopy
-
 from api_server.default_config import config as default_config
 
-config = deepcopy(default_config)
-config["host"] = "0.0.0.0"
-config["port"] = 8000
-config["db_url"] = "postgres://rmf-server:rmf-server@rmf-server-db/rmf-server"
-config["public_url"] = "https://example.com/rmf/api/v1"
-config["log_level"] = "INFO"
-config["jwt_public_key"] = "/jwt-configmap/jwt-pub-key.pub"
-config[
-    "oidc_url"
-] = "https://example.com/auth/realms/rmf-web/.well-known/openid-configuration"
-config["aud"] = "dashboard"
-config["iss"] = "https://example.com/auth/realms/rmf-web"
+config = default_config.update(
+    {
+        "host": "0.0.0.0",
+        "port": 8000,
+        "db_url": "postgres://rmf-server:rmf-server@rmf-server-db/rmf-server",
+        "public_url": "https://example.com/rmf/api/v1",
+        "log_level": "INFO",
+        "jwt_public_key": "/jwt-configmap/jwt-pub-key.pub",
+        "oidc_url": "https://example.com/auth/realms/rmf-web/.well-known/openid-configuration",
+        "aud": "dashboard",
+        "iss": "https://example.com/auth/realms/rmf-web",
+        "builtin_admin": "example",
+    }
+)
