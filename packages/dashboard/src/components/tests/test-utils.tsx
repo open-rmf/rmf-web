39350871762ca1ac368b82d85ca7d9c8e0d3291e
Teo Koon Peng <koonpeng@openrobotics.org>
2021-07-21 09:59:44 +0800

    remove renderAct
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard/src/components/tests/test-utils.tsx
+++ packages/dashboard/src/components/tests/test-utils.tsx
@@ -1,17 +1,6 @@
-import { act, render, RenderResult } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { User, UserContext } from '../auth/contexts';
-
-/**
- * Wraps a `render` in a `act`.
- */
-export async function renderAct(ui: React.ReactElement): Promise<RenderResult> {
-  let root: RenderResult;
-  await act(async () => {
-    root = render(ui);
-  });
-  return root!;
-}
 
 export const superUser: User = {
   profile: {
