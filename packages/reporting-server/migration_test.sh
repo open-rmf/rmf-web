fa3697d7cf7fe5e3be923177829b1faa3af9c13f
Matias Bavera <matiasbavera@gmail.com>
2021-07-21 16:23:47 -0400

    Added a good way to determine if postgres is up and running on the docker container
    Signed-off-by: Matias Bavera <matiasbavera@gmail.com>

--- packages/reporting-server/migration_test.sh
+++ packages/reporting-server/migration_test.sh
@@ -1,26 +1,30 @@
 
+DOCKER_CONTAINER_NAME="reporting-server-migration-test"
 # First, we need to build the docker image, this will copy all the migrations to the container.
 
 docker build . -t rmf-web/reporting-server-migration-test -f migration-test.dockerfile
 
-if [ ! "$(docker ps -q -f name=reporting-server-migration-test)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=reporting-server-migration-test)" ]; then
+if [ ! "$(docker ps -q -f name=$DOCKER_CONTAINER_NAME)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=$DOCKER_CONTAINER_NAME)" ]; then
         # cleanup
         docker rm reporting-server-migration-test
     fi
     echo "running the container"
-    docker run -d --name reporting-server-migration-test rmf-web/reporting-server-migration-test:latest
+    docker run -d --name $DOCKER_CONTAINER_NAME rmf-web/reporting-server-migration-test:latest
 fi
 
-sleep 5
 
-if [ "$(docker ps -q -f name=reporting-server-migration-test)" ]; then
+if [ "$(docker ps -q -f name=$DOCKER_CONTAINER_NAME)" ]; then
+  
+  echo "waiting for postgres to be up and runnning"
+  timeout 10s bash -c "until docker exec $DOCKER_CONTAINER_NAME pg_isready ; do sleep 5 ; done"
+  
   echo "running migrations"
-  docker exec -it reporting-server-migration-test ./migrate.sh
+  docker exec -it $DOCKER_CONTAINER_NAME ./migrate.sh
 fi
 
 echo "stopping"
-docker stop reporting-server-migration-test
+docker stop $DOCKER_CONTAINER_NAME
 
 echo "removing"
-docker rm reporting-server-migration-test
+docker rm $DOCKER_CONTAINER_NAME
