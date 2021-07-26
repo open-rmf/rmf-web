8cfecf74736473e2025d0024254026ddaf5e57b2
Teo Koon Peng <koonpeng@openrobotics.org>
2021-07-26 16:35:20 +0800

    add make admin action in user profile card
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard/src/components/admin/user-profile.tsx
+++ packages/dashboard/src/components/admin/user-profile.tsx
@@ -1,7 +1,17 @@
-import { Card, CardHeader } from '@material-ui/core';
+import {
+  Card,
+  CardHeader,
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  Menu,
+  MenuItem,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AccountIcon from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import React from 'react';
+import { useAsync } from 'react-components';
 import { UserProfile } from '../auth';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,10 +23,15 @@ const useStyles = makeStyles((theme) => ({
 
 export interface UserProfileCardProps {
   profile: UserProfile;
+  makeAdmin?: (admin: boolean) => Promise<void> | void;
 }
 
-export function UserProfileCard({ profile }: UserProfileCardProps): JSX.Element {
+export function UserProfileCard({ profile, makeAdmin }: UserProfileCardProps): JSX.Element {
   const classes = useStyles();
+  const safeAsync = useAsync();
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const [disableAdminCheckbox, setDisableAdminCheckbox] = React.useState(false);
+
   return (
     <Card variant="outlined">
       <CardHeader
@@ -24,7 +39,26 @@ export function UserProfileCard({ profile }: UserProfileCardProps): JSX.Element 
         titleTypographyProps={{ variant: 'h5' }}
         subheader={profile.is_admin ? 'Admin' : 'User'}
         avatar={<AccountIcon className={classes.avatar} />}
+        action={
+          <IconButton onClick={(ev) => setAnchorEl(ev.currentTarget)} aria-label="more actions">
+            <MoreIcon />
+          </IconButton>
+        }
       />
+      <Menu open={!!anchorEl} anchorEl={anchorEl} onClose={() => setAnchorEl(null)}>
+        <MenuItem
+          onClick={async () => {
+            setDisableAdminCheckbox(true);
+            makeAdmin && (await safeAsync(makeAdmin(!profile.is_admin)));
+            setDisableAdminCheckbox(false);
+          }}
+        >
+          <FormControlLabel
+            control={<Checkbox checked={profile.is_admin} disabled={disableAdminCheckbox} />}
+            label="Admin"
+          />
+        </MenuItem>
+      </Menu>
     </Card>
   );
 }
