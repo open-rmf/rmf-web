f6ba770a919e96f3323f6fdccfcc47ebe548e391
vallq <val@openrobotics.org>
2021-07-28 10:29:58 +0800

    remove user following main
    Signed-off-by: vallq <val@openrobotics.org>

--- packages/minimal/src/components/app.tsx
+++ packages/minimal/src/components/app.tsx
@@ -6,11 +6,11 @@ import ResourceManager from '../managers/resource-manager';
 import { AppBase } from './app-base';
 import { AppConfigContext, ResourcesContext, TrajectorySocketContext } from './app-contexts';
 import './app.css';
-import { AuthenticatorContext, UserContext } from './auth/contexts';
+import { AuthenticatorContext } from './auth/contexts';
 import { RmfApp } from './rmf-app';
 import { Route, Redirect, useLocation, Switch } from 'react-router';
 import { BrowserRouter, Link } from 'react-router-dom';
-import { getUrl, LoginHOC, PrivateRouteHOC, User } from 'rmf-auth';
+import { getUrl, LoginHOC, PrivateRouteHOC } from 'rmf-auth';
 import { DASHBOARD_ROUTE, LOGIN_ROUTE } from '../util/url';
 import Dashboard from './dashboard/dashboard';
 
@@ -34,7 +34,7 @@ const theme = createMuiTheme({
 export default function App(): JSX.Element | null {
   const [authInitialized, setAuthInitialized] = React.useState(!!appConfig.authenticator.user);
   const [authenticator, setAuthenticator] = React.useState(appConfig.authenticator);
-  const [user, setUser] = React.useState<User | null>(appConfig.authenticator.user || null);
+  const [user, setUser] = React.useState<string | null>(appConfig.authenticator.user || null);
   const [ws, setWs] = React.useState<WebSocket>(new WebSocket(appConfig.trajServerUrl));
   const appRoutes = [DASHBOARD_ROUTE];
 
@@ -46,20 +46,22 @@ export default function App(): JSX.Element | null {
   }, [setWs]);
 
   React.useEffect(() => {
-    if (user) {
-      return;
-    }
-    const onUserChanged = (newUser: User | null) => setUser(newUser);
+    let cancel = false;
+    const onUserChanged = (newUser: string | null) => setUser(newUser);
     authenticator.on('userChanged', onUserChanged);
     (async () => {
       await authenticator.init();
+      if (cancel) {
+        return;
+      }
       setUser(authenticator.user || null);
       setAuthInitialized(true);
     })();
     return () => {
+      cancel = true;
       authenticator.off('userChanged', onUserChanged);
     };
-  }, [authenticator, user]);
+  }, [authenticator]);
 
   const resourceManager = React.useRef<ResourceManager | undefined>(undefined);
   const [appReady, setAppReady] = React.useState(false);
@@ -79,54 +81,50 @@ export default function App(): JSX.Element | null {
     })();
   }, []);
 
-  React.useEffect(() => {});
-
   return authInitialized && appReady ? (
     <AppConfigContext.Provider value={appConfig}>
       <ResourcesContext.Provider value={resourceManager.current}>
         <AuthenticatorContext.Provider value={authenticator}>
-          <UserContext.Provider value={user}>
-            <TrajectorySocketContext.Provider value={ws}>
-              <ThemeProvider theme={theme}>
-                <BrowserRouter>
-                  <Switch>
-                    <Route exact path={LOGIN_ROUTE}>
-                      <Login
-                        user={user}
-                        title={'Dashboard'}
-                        authenticator={authenticator}
-                        successRedirectUri={getUrl(DASHBOARD_ROUTE)}
+          <TrajectorySocketContext.Provider value={ws}>
+            <ThemeProvider theme={theme}>
+              <BrowserRouter>
+                <Switch>
+                  <Route exact path={LOGIN_ROUTE}>
+                    <Login
+                      user={user}
+                      title={'Dashboard'}
+                      authenticator={authenticator}
+                      successRedirectUri={getUrl(DASHBOARD_ROUTE)}
+                    />
+                  </Route>
+                  {user ? (
+                    <PrivateRoute exact path={appRoutes} redirectPath={LOGIN_ROUTE} user={user}>
+                      <RmfApp>
+                        <AppBase>
+                          <Switch>
+                            <PrivateRoute
+                              exact
+                              path={DASHBOARD_ROUTE}
+                              redirectPath={LOGIN_ROUTE}
+                              user={user}
+                            >
+                              <Dashboard />
+                            </PrivateRoute>
+                          </Switch>
+                        </AppBase>
+                      </RmfApp>
+                    </PrivateRoute>
+                  ) : (
+                    <Route>
+                      <NotFoundPage
+                        routeLinkComponent={<Link to={LOGIN_ROUTE}>Go to Login</Link>}
                       />
                     </Route>
-                    {user ? (
-                      <PrivateRoute exact path={appRoutes} redirectPath={LOGIN_ROUTE} user={user}>
-                        <RmfApp>
-                          <AppBase>
-                            <Switch>
-                              <PrivateRoute
-                                exact
-                                path={DASHBOARD_ROUTE}
-                                redirectPath={LOGIN_ROUTE}
-                                user={user}
-                              >
-                                <Dashboard />
-                              </PrivateRoute>
-                            </Switch>
-                          </AppBase>
-                        </RmfApp>
-                      </PrivateRoute>
-                    ) : (
-                      <Route>
-                        <NotFoundPage
-                          routeLinkComponent={<Link to={LOGIN_ROUTE}>Go to Login</Link>}
-                        />
-                      </Route>
-                    )}
-                  </Switch>
-                </BrowserRouter>
-              </ThemeProvider>
-            </TrajectorySocketContext.Provider>
-          </UserContext.Provider>
+                  )}
+                </Switch>
+              </BrowserRouter>
+            </ThemeProvider>
+          </TrajectorySocketContext.Provider>
         </AuthenticatorContext.Provider>
       </ResourcesContext.Provider>
     </AppConfigContext.Provider>
