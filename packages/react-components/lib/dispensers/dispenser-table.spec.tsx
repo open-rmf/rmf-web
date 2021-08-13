af1dc50abfed1bb11a04b88f20665b056e6dcc11
ChawinTan <chawin15@gmail.com>
2021-08-13 16:03:00 +0800

    add mode color and change some test
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/react-components/lib/dispensers/dispenser-table.spec.tsx
+++ packages/react-components/lib/dispensers/dispenser-table.spec.tsx
@@ -1,16 +1,23 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-
+import * as RmfModels from 'rmf-models';
 import { DispenserTable } from './dispenser-table';
 import { makeDispenser, makeDispenserState } from './test-utils.spec';
 
 describe('Dispenser table', () => {
   it('should render properly', () => {
-    render(
-      <DispenserTable
-        dispensers={[makeDispenser()]}
-        dispenserStates={{ test: makeDispenserState() }}
-      />,
-    );
+    const dispensers = [
+      makeDispenser(),
+      makeDispenser({ guid: 'test1' }),
+      makeDispenser({ guid: 'test2' }),
+      makeDispenser({ guid: 'test3' }),
+    ];
+    const dispenserStates = {
+      test: makeDispenserState(),
+      test1: makeDispenserState({ mode: RmfModels.DispenserState.BUSY }),
+      test2: makeDispenserState({ mode: RmfModels.DispenserState.OFFLINE }),
+      test3: makeDispenserState({ mode: -1 }),
+    };
+    render(<DispenserTable dispensers={dispensers} dispenserStates={dispenserStates} />);
   });
 });
--- packages/react-components/lib/dispensers/dispenser-table.tsx
+++ packages/react-components/lib/dispensers/dispenser-table.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { makeStyles, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
-
+import * as RmfModels from 'rmf-models';
 import { dispenserModeToString, DispenserRowProps, DispenserTableProps } from './utils';
 
 const useStyles = makeStyles((theme) => ({
   taskRowHover: {
     background: theme.palette.action.hover,
+  },
+  dispenserLabelIdle: {
+    color: theme.palette.success.main,
+  },
+  dispenserLabelBusy: {
+    color: theme.palette.error.main,
+  },
+  offlineLabelOffline: {
+    color: theme.palette.warning.main,
   },
 }));
 
@@ -13,6 +22,22 @@ const DispenserRow = (props: DispenserRowProps) => {
   const { dispenser, dispenserState } = props;
   const classes = useStyles();
   const [hover, setHover] = React.useState(false);
+
+  const dispenserModeLabelClasses = React.useCallback(
+    (mode: number): string => {
+      switch (mode) {
+        case RmfModels.DispenserState.IDLE:
+          return `${classes.dispenserLabelIdle}`;
+        case RmfModels.DispenserState.BUSY:
+          return `${classes.dispenserLabelBusy}`;
+        case RmfModels.DispenserState.OFFLINE:
+          return `${classes.offlineLabelOffline}`;
+        default:
+          return '';
+      }
+    },
+    [classes],
+  );
 
   return (
     <>
@@ -24,7 +49,9 @@ const DispenserRow = (props: DispenserRowProps) => {
         {dispenserState ? (
           <React.Fragment>
             <TableCell>{dispenser.guid}</TableCell>
-            <TableCell>{dispenserModeToString(dispenserState.mode)}</TableCell>
+            <TableCell className={dispenserModeLabelClasses(dispenserState.mode)}>
+              {dispenserModeToString(dispenserState.mode)}
+            </TableCell>
             <TableCell>{dispenserState.request_guid_queue.length}</TableCell>
             <TableCell>{dispenserState.request_guid_queue}</TableCell>
             <TableCell>{dispenserState.seconds_remaining}</TableCell>
