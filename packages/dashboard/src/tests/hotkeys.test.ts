c521c5502d9ffcc997a2d88c3af58f8dbbf99904
ChawinTan <chawin15@gmail.com>
2021-08-24 16:04:20 +0800

    remove unused unit test
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/dashboard/src/tests/hotkeys.test.ts
+++ packages/dashboard/src/tests/hotkeys.test.ts
@@ -1,6 +1,3 @@
-import { act } from '@testing-library/react-hooks';
-import { HotKeysEnabledProps } from 'react-hotkeys';
-import { AppController } from '../components/app-contexts';
 import { makeMockAppController } from '../components/tests/mock-app-controller';
 import { buildHotKeys, keyMap } from '../hotkeys';
 
@@ -10,28 +7,4 @@ test('build hotkeys on the correct format', () => {
   });
   if (!hotkeys.keyMap) throw new Error('An error has occurred building the hotkeys formats');
   expect(Object.keys(hotkeys.keyMap)).toEqual(Object.keys(keyMap));
-});
-
-describe('update states correctly', () => {
-  let hotkeys: HotKeysEnabledProps;
-  let appController: AppController;
-
-  beforeEach(() => {
-    appController = makeMockAppController();
-    hotkeys = buildHotKeys({
-      appController,
-    });
-  });
-
-  test('toggles Setting Panel correctly', () => {
-    act(() => {
-      hotkeys.handlers?.OPEN_SETTINGS();
-    });
-    expect(appController.setShowSettings).toBeCalledTimes(1);
-
-    act(() => {
-      hotkeys.handlers?.OPEN_SETTINGS();
-    });
-    expect(appController.setShowSettings).toBeCalledTimes(2);
-  });
 });
