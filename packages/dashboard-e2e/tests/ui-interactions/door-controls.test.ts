853f97fe5c40549867942fa12e95ab5256baf3cc
Teo Koon Peng <koonpeng@openrobotics.org>
2021-09-01 10:40:56 +0800

    upgrade wdio
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
+++ packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
@@ -3,7 +3,6 @@ import { getDoorAccordion, getOmniPanel, omniPanelMainMenu } from '../utils';
 describe('door controls', () => {
   beforeEach(omniPanelMainMenu);
 
-  // door interaction test
   it('clicking on open button opens the door', () => {
     const omnipanel = getOmniPanel();
     omnipanel.$('h5=Doors').click();
