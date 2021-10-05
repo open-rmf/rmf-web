9060e6d62e951edd1384cb32263bcc309fac7664
ChawinTan <chawin15@gmail.com>
2021-10-05 16:04:37 +0800

    fix: display error message instead of throwing an error when uploading batch task (#509)
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/dashboard/src/components/tasks/task-panel.tsx
+++ packages/dashboard/src/components/tasks/task-panel.tsx
@@ -25,6 +25,7 @@ import { UserProfileContext } from 'rmf-auth';
 import * as RmfModels from 'rmf-models';
 import { Enforcer } from '../permissions';
 import { parseTasksFile } from './utils';
+import { AppControllerContext } from '../app-contexts';
 
 const useStyles = makeStyles((theme) => ({
   tableContainer: {
@@ -96,6 +97,7 @@ export function TaskPanel({
   const [snackbarSeverity, setSnackbarSeverity] = React.useState<AlertProps['severity']>('success');
   const [autoRefresh, setAutoRefresh] = React.useState(true);
   const profile = React.useContext(UserProfileContext);
+  const { showErrorAlert } = React.useContext(AppControllerContext);
 
   const handleCancelTaskClick = React.useCallback<React.MouseEventHandler>(async () => {
     if (!cancelTask || !selectedTask) {
@@ -121,12 +123,20 @@ export function TaskPanel({
       if (!fileInputEl) {
         return [];
       }
+      let taskFiles: SubmitTask[];
       const listener = async () => {
         try {
           if (!fileInputEl.files || fileInputEl.files.length === 0) {
             return res([]);
           }
-          return res(parseTasksFile(await fileInputEl.files[0].text()));
+          try {
+            taskFiles = parseTasksFile(await fileInputEl.files[0].text());
+          } catch (err) {
+            showErrorAlert(err.message, 5000);
+            return res([]);
+          }
+          // only submit tasks when all tasks are error free
+          return res(taskFiles);
         } finally {
           fileInputEl.removeEventListener('input', listener);
           fileInputEl.value = '';
