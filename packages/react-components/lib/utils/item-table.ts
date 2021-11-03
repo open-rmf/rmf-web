fce65cf3b74e9ebcdaa33dffb992629fec6ce888
ChawinTan <chawin15@gmail.com>
2021-11-03 13:42:46 +0800

    remove unnecessary code
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/react-components/lib/utils/item-table.ts
+++ packages/react-components/lib/utils/item-table.ts
@@ -1,8 +1,7 @@
 export function tableCellStyle(flexGrow: string) {
-  const rowHeight = 31;
   return {
     flex: `${flexGrow} 0 0`,
     overflow: 'hidden',
-    height: rowHeight,
+    height: 31,
   };
 }
