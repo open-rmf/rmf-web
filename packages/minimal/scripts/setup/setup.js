43eb286b3af368942ea6f046f5047ae3b4e9d127
ChawinTan <chawin15@gmail.com>
2021-11-23 09:34:59 +0800

    infra setup
    Signed-off-by: ChawinTan <chawin15@gmail.com>

---
+++ packages/minimal/scripts/setup/setup.js
@@ -0,0 +1,23 @@
+const chalk = require('chalk');
+const fs = require('fs');
+const { getIcons, createConfigFile } = require('rmf-tools');
+
+const ProjectDir = __dirname.slice(0, __dirname.length - '/scripts/setup'.length);
+const resourcesPath = process.env.RMF_DASHBOARD_RESOURCES_FILE || `${ProjectDir}/.resources.json`;
+const configExists = fs.existsSync(resourcesPath);
+
+(async () => {
+  if (configExists) {
+    console.log(chalk`{blue Using existing resources config}`);
+  } else {
+    await createConfigFile();
+  }
+
+  const resourcesData = JSON.parse(fs.readFileSync(resourcesPath));
+  try {
+    getIcons(resourcesData, 'minimal');
+  } catch (e) {
+    console.error(e);
+    process.exit(e.status);
+  }
+})();
---
+++ packages/minimal/scripts/start-rmf.js
@@ -0,0 +1,1 @@
+require('rmf-tools').makeLauncher().launch();
