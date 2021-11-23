f8554118ff3e1574d28fcf3c7ca75079b8103286
vallq <val@openrobotics.org>
2021-11-23 17:49:27 +0800

    commit new api client lib version
    Signed-off-by: vallq <val@openrobotics.org>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: 'd67d84edd4d036c0ed7fdad3f4ce6c33a35d90f7',
-  swaggerCodegen: '3.0.25',
+  rmfServer: 'b573f717575df8f304e71690314f96dbfc6989ad',
+  openapiGenerator: '5.2.1',
 };
