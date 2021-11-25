3ebe4d63898c5334d26adaa75e57cc856a465929
ChawinTan <chawin15@gmail.com>
2021-11-25 13:29:39 +0800

    task and task form infra
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/minimal/src/app-config.ts
+++ packages/minimal/src/app-config.ts
@@ -1,8 +1,16 @@
-import { SioClient } from 'api-client';
+import { SioClient, TasksApi, Configuration } from 'api-client';
+import axios from 'axios';
 
+const axiosInst = axios.create();
 if (!process.env.REACT_APP_RMF_SERVER) {
   throw new Error('REACT_APP_RMF_SERVER is required');
 }
 
-export const rmfServerUrl = process.env.REACT_APP_RMF_SERVER;
+const rmfServerUrl = process.env.REACT_APP_RMF_SERVER;
+const apiConfig = new Configuration({
+  basePath: rmfServerUrl,
+});
+
+export const currentLocation = process.env.REACT_APP_CURRENT_LOCATION;
 export const sioClient = new SioClient(rmfServerUrl);
+export const taskApi = new TasksApi(apiConfig, undefined, axiosInst);
--- packages/minimal/src/components/task-form.tsx
+++ packages/minimal/src/components/task-form.tsx
@@ -9,7 +9,12 @@ import {
   Typography,
   Paper,
   makeStyles,
+  Snackbar,
 } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
+import { TaskType as RmfTaskType } from 'rmf-models';
+import type { SubmitTask, LoopTaskDescription } from 'api-client';
+import { currentLocation } from '../app-config';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,8 +34,27 @@ export const TaskForm = (props: TaskFormProps) => {
   const classes = useStyles();
   const { placeNames } = props;
   const [place, setPlace] = React.useState('');
+  const [showError, setShowError] = React.useState(false);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPlace(event.target.value);
+  };
+
+  const submitTask = () => {
+    if (currentLocation && place) {
+      const loopTaskDesc: LoopTaskDescription = {
+        num_loops: 1,
+        start_name: currentLocation,
+        finish_name: place,
+      };
+      const task: SubmitTask = {
+        task_type: RmfTaskType.TYPE_LOOP,
+        start_time: new Date().getTime() / 1000,
+        description: loopTaskDesc,
+      };
+    } else {
+      setShowError(true);
+    }
   };
 
   return (
@@ -40,13 +64,22 @@ export const TaskForm = (props: TaskFormProps) => {
         <FormLabel component="legend">Destination</FormLabel>
         <RadioGroup onChange={handleChange} row>
           {placeNames.map((p) => (
-            <FormControlLabel value={p} control={<Radio />} label={p} />
+            <FormControlLabel
+              value={p}
+              disabled={p === currentLocation}
+              control={<Radio />}
+              label={p}
+            />
           ))}
         </RadioGroup>
       </FormControl>
-      <Button variant="contained" color="primary" size="large">
+      <Typography variant="h6">Current Location - {currentLocation}</Typography>
+      <Button variant="contained" color="primary" size="large" onClick={() => submitTask()}>
         Submit
       </Button>
+      <Snackbar open={showError} onClose={() => setShowError(false)}>
+        <MuiAlert severity="error">You must select a destination</MuiAlert>
+      </Snackbar>
     </Paper>
   );
 };
