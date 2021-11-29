106964c3cf591be8a920031e09b09951c743cc7e
ChawinTan <chawin15@gmail.com>
2021-11-29 10:55:44 +0800

    fix console warning in unit test
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/minimal/src/App.test.tsx
+++ packages/minimal/src/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('should render app', () => {
+test('should render app', async () => {
   const node = render(<App />);
-  expect(node.container.querySelector('#appbar')).toBeTruthy();
+  expect(await waitFor(() => node.container.querySelector('#appbar'))).toBeTruthy();
   expect(screen.getByLabelText('task-display')).toBeTruthy();
   expect(screen.getByLabelText('task-form')).toBeTruthy();
 });
