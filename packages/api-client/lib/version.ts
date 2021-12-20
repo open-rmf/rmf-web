1333704b68016a5bc28e9395ba42e2b123fe3424
ChawinTan <chawin15@gmail.com>
2021-12-20 13:58:38 +0800

    fix generage-openapi script
    Signed-off-by: ChawinTan <chawin15@gmail.com>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: 'e8f1bf61ba81bb9677a3ddd2e40deeea6502d87f',
+  rmfServer: 'f3e79d6fd0b185c6a098b05d3c4b9d8683ca8c28',
   openapiGenerator: '5.2.1',
 };
