91809f1a91a3e3912d820a408f6384cdaecd1749
Teo Koon Peng <koonpeng@openrobotics.org>
2022-01-04 02:51:35 +0000

    fix tests
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/api-server/api_server/rmf_io/test_rmf_service.py
+++ packages/api-server/api_server/rmf_io/test_rmf_service.py
@@ -19,33 +19,34 @@ class TestRmfService(unittest.TestCase):
     def _create_node_id() -> str:
         return str(uuid4()).replace("-", "_")
 
-    def setUp(self) -> None:
-        self._client_context = rclpy.context.Context()
-        self._client_context.init()
-        self.client_node = rclpy.node.Node(
-            f"test_client{self._create_node_id()}", context=self._client_context
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls._client_context = rclpy.context.Context()
+        cls._client_context.init()
+        cls.client_node = rclpy.node.Node(
+            f"test_client{cls._create_node_id()}", context=cls._client_context
         )
-        self._client_executor = rclpy.executors.SingleThreadedExecutor()
+        cls._client_executor = rclpy.executors.SingleThreadedExecutor()
 
         def client():
-            while self._client_context.ok():
+            while cls._client_context.ok():
                 rclpy.spin_once(
-                    self.client_node, executor=self._client_executor, timeout_sec=0.1
+                    cls.client_node, executor=cls._client_executor, timeout_sec=0.1
                 )
 
-        self._client_thread = threading.Thread(target=client)
-        self._client_thread.start()
+        cls._client_thread = threading.Thread(target=client)
+        cls._client_thread.start()
 
-        self._server_context = rclpy.context.Context()
-        self._server_context.init()
-        self.server_node = rclpy.node.Node(
-            f"test_server_{self._create_node_id()}",
-            context=self._server_context,
+        cls._server_context = rclpy.context.Context()
+        cls._server_context.init()
+        cls.server_node = rclpy.node.Node(
+            f"test_server_{cls._create_node_id()}",
+            context=cls._server_context,
         )
-        self._server_executor = rclpy.executors.SingleThreadedExecutor()
+        cls._server_executor = rclpy.executors.SingleThreadedExecutor()
 
         def server():
-            pub = self.server_node.create_publisher(
+            pub = cls.server_node.create_publisher(
                 ApiResponse,
                 RmfService.API_RESPONSE_TOPIC,
                 rclpy.qos.QoSProfile(
@@ -61,7 +62,7 @@ class TestRmfService(unittest.TestCase):
                     ApiResponse(request_id=msg.request_id, json_msg=msg.json_msg)
                 )
 
-            self.server_node.create_subscription(
+            cls.server_node.create_subscription(
                 ApiRequest,
                 RmfService.API_REQUEST_TOPIC,
                 handle_resp,
@@ -73,29 +74,30 @@ class TestRmfService(unittest.TestCase):
                 ),
             )
 
-            while self._server_context.ok():
+            while cls._server_context.ok():
                 rclpy.spin_once(
-                    self.server_node, executor=self._server_executor, timeout_sec=0.1
+                    cls.server_node, executor=cls._server_executor, timeout_sec=0.1
                 )
 
-        self._server_thread = threading.Thread(target=server)
-        self._server_thread.start()
+        cls._server_thread = threading.Thread(target=server)
+        cls._server_thread.start()
 
         # wait for discovery
-        while self.server_node.get_name() not in self.client_node.get_node_names():
+        while cls.server_node.get_name() not in cls.client_node.get_node_names():
             time.sleep(0.1)
-        while self.client_node.get_name() not in self.server_node.get_node_names():
+        while cls.client_node.get_name() not in cls.server_node.get_node_names():
             time.sleep(0.1)
 
-        self.rmf_service = RmfService(self.client_node)
+        cls.rmf_service = RmfService(cls.client_node)
 
-    def tearDown(self) -> None:
-        self._client_context.shutdown()
-        self._client_executor.shutdown()
-        self._client_thread.join()
-        self._server_context.shutdown()
-        self._server_executor.shutdown()
-        self._server_thread.join()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls._client_context.shutdown()
+        cls._client_executor.shutdown()
+        cls._client_thread.join()
+        cls._server_context.shutdown()
+        cls._server_executor.shutdown()
+        cls._server_thread.join()
 
     def test_call(self):
         async def run():
