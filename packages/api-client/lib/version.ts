35f60d563c8781f8c2769d30b393377a1741cdbd
Teo Koon Peng <koonpeng@openrobotics.org>
2022-01-10 02:02:23 +0000

    update models
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: '1333704b68016a5bc28e9395ba42e2b123fe3424',
+  rmfServer: '9e6287bacb8cd75693910a49ab012b593703c005',
   openapiGenerator: '5.2.1',
 };
