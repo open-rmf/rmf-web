d974b40800207099b82e655120b86d0300373cf3
Teo Koon Peng <koonpeng@openrobotics.org>
2022-01-18 09:26:02 +0800

    build: tasksv2/better dev defaults (#568)
    * add ros_args options; default start with disk sqlite and use_sim_time
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>
    
    * RMF_SERVER_USE_SIM_TIME takes precedence over config
    
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/api-server/api_server/ros.py
+++ packages/api-server/api_server/ros.py
@@ -5,17 +5,20 @@ import threading
 import rclpy
 import rclpy.node
 
+from .app_config import app_config
+
 _spin_thread: threading.Thread = None  # type: ignore
 
+# TODO: remove support for `RMF_SERVER_USE_SIM_TIME`?
 use_sim_time_env = os.environ.get("RMF_SERVER_USE_SIM_TIME", None)
 if use_sim_time_env:
     use_sim_time = not use_sim_time_env.lower() in ["0", "false"]
 else:
     use_sim_time = False
 if use_sim_time:
-    rclpy.init(args=["--ros-args", "-p", "use_sim_time:=true"])
+    rclpy.init(args=["--ros-args"] + app_config.ros_args + ["-p", "use_sim_time:=true"])
 else:
-    rclpy.init()
+    rclpy.init(args=["--ros-args"] + app_config.ros_args)
 
 ros_node = rclpy.node.Node("rmf_api_server")
 
--- packages/api-server/psql_local_config.py
+++ packages/api-server/psql_local_config.py
@@ -1,7 +1,3 @@
-from copy import deepcopy
+from sqlite_local_config import config
 
-from api_server.default_config import config as default_config
-
-config = deepcopy(default_config)
-
-config["db_url"] = "postgres://postgres:postgres@127.0.0.1:5432"
+config.update({"db_url": "postgres://postgres:postgres@127.0.0.1:5432"})
