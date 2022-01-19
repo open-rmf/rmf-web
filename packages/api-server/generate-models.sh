f4e981b993aba88371456c1147eec2900f36e77b
Teo Koon Peng <koonpeng@openrobotics.org>
2022-01-19 02:46:43 +0000

    generate task description models
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/api-server/generate-models.sh
+++ packages/api-server/generate-models.sh
@@ -5,6 +5,7 @@ shopt -s globstar
 RMF_BUILDING_MAP_MSGS_VER=c5e0352e2dfd3d11e4d292a1c2901cad867c1441
 RMF_INTERNAL_MSGS_VER=0c237e1758872917661879975d7dc0acf5fa518c
 RMF_API_MSGS_VER=0f2d5082934acc56f8c352810f0f5db07103d788
+RMF_ROS2_VER=bf038461b5b0fb7d4594461a724bc9e5e7cb97c6
 
 cd "$(dirname $0)"
 source ../../scripts/rmf-helpers.sh
@@ -27,6 +28,7 @@ function fetch_sources {
 fetch_sources https://github.com/open-rmf/rmf_building_map_msgs.git $RMF_BUILDING_MAP_MSGS_VER build/colcon_ws/src/rmf_building_map_msgs
 fetch_sources https://github.com/open-rmf/rmf_internal_msgs.git $RMF_INTERNAL_MSGS_VER build/colcon_ws/src/rmf_internal_msgs
 fetch_sources https://github.com/open-rmf/rmf_api_msgs.git $RMF_API_MSGS_VER build/rmf_api_msgs
+fetch_sources https://github.com/open-rmf/rmf_ros2.git $RMF_ROS2_VER build/rmf_ros2
 
 # build and source colcon workspace
 pushd "build/colcon_ws"
@@ -54,36 +56,45 @@ version = {
   "rmf_internal_msgs": "$RMF_INTERNAL_MSGS_VER",
   "rmf_building_map_msgs": "$RMF_BUILDING_MAP_MSGS_VER",
 }
-
 EOF
 
 pipenv run isort api_server/models/ros_pydantic
 pipenv run black api_server/models/ros_pydantic
 
-# generate rmf api models from json schemas
-output='api_server/models/rmf_api'
-rm -rf "$output"
-mkdir -p "$output"
-if [[ ! -d .venv_local/lib ]]; then
-  python3 -m venv .venv_local
-  bash -c ". .venv_local/bin/activate && pip3 install wheel && pip3 install 'datamodel-code-generator~=0.11.15'"
-fi
-rm -rf api_server/models/rmf_api
-bash -c ". .venv_local/bin/activate && datamodel-codegen --disable-timestamp --input-file-type jsonschema --enum-field-as-literal one --input build/rmf_api_msgs/rmf_api_msgs/schemas --output \"$output\""
-cat << EOF > "$output/version.py"
+generate_from_json_schema() {
+  input=$1
+  output=$2
+  upstream=$3 # the name of the package that contains the schemas
+  version=$4 # the version of the upstream package
+
+  rm -rf "$output"
+  mkdir -p "$output"
+  if [[ ! -d .venv_local/lib ]]; then
+    python3 -m venv .venv_local
+    bash -c ". .venv_local/bin/activate && pip3 install wheel && pip3 install 'datamodel-code-generator~=0.11.15'"
+  fi
+  bash -c ". .venv_local/bin/activate && datamodel-codegen --disable-timestamp --input-file-type jsonschema --enum-field-as-literal one --input "$input" --output \"$output\""
+  cat << EOF > "$output/version.py"
 # THIS FILE IS GENERATED
 version = {
-  "rmf_api_msgs": "$RMF_API_MSGS_VER",
+  "$upstream": "$version",
+}
+EOF
+  pipenv run isort "$output"
+  pipenv run black "$output"
 }
 
-EOF
-pipenv run isort api_server/models/rmf_api
-pipenv run black api_server/models/rmf_api
+# generate rmf api models from json schemas
+generate_from_json_schema build/rmf_api_msgs/rmf_api_msgs/schemas api_server/models/rmf_api rmf_api_msgs $RMF_API_MSGS_VER
+
+# generate builtin task descriptions from json schemas
+generate_from_json_schema build/rmf_ros2/rmf_fleet_adapter/schemas api_server/models/rmf_ros2 rmf_ros2 $RMF_ROS2_VER
 
 echo ''
 echo 'versions:'
 echo "  rmf_internal_msgs: $RMF_INTERNAL_MSGS_VER"
 echo "  rmf_building_map_msgs: $RMF_BUILDING_MAP_MSGS_VER"
 echo "  rmf_api_msgs: $RMF_API_MSGS_VER"
+echo "  rmf_ros2: $RMF_ROS2_VER"
 echo ''
 echo 'Successfully generated ros_pydantic models'
