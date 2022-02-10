dfb000900a1a70977e52cfadd5160c060a90b435
Michael X. Grey <grey@openrobotics.org>
2022-02-11 01:31:39 +0800

    Clean up the timeline
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- packages/react-components/lib/tasks/task-timeline.tsx
+++ packages/react-components/lib/tasks/task-timeline.tsx
@@ -16,7 +16,7 @@ import {
 import { styled } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { format } from 'date-fns';
-import { TaskState, EventState } from 'api-client';
+import { TaskState, Phase, EventState } from 'api-client';
 import React from 'react';
 
 interface TimeLinePropsWithRef extends TimelineProps {
@@ -48,32 +48,23 @@ const StyledTimeLine = styled((props: TimeLinePropsWithRef) => <Timeline {...pro
   }),
 );
 
-function NestedEvents(eventsStates: { [key: string]: EventState } | undefined, child: number) {
-  if (eventsStates !== undefined) {
-    const deps = eventsStates[child] ? eventsStates[child].deps : [];
-    return (
-      <TreeItem
-        key={`event-${child}`}
-        nodeId={`parent-${eventsStates[child].id}`}
-        label={eventsStates[child].name}
-      >
-        {deps
-          ? deps.map((id) => {
-              if (eventsStates[id].deps?.length) {
-                return NestedEvents(eventsStates, id);
-              } else {
-                return (
-                  <TreeItem
-                    key={eventsStates[child].name}
-                    nodeId={`child-${id}`}
-                    label={eventsStates[id].name}
-                  ></TreeItem>
-                );
-              }
-            })
-          : null}
-      </TreeItem>
-    );
+function NestedEvents(
+  eventStates: { [key: string]: EventState } | undefined,
+  eventId: number | undefined,
+) {
+  if (eventStates !== undefined && eventStates !== null && eventId !== undefined) {
+    const event = eventStates[eventId];
+    if (event !== undefined) {
+      return (
+        <TreeItem nodeId={`event-${event.id}`} label={event.name || 'undefined'}>
+          {event.deps
+            ? event.deps.map((childId) => {
+                return NestedEvents(eventStates, childId);
+              })
+            : null}
+        </TreeItem>
+      );
+    }
   }
 }
 
@@ -99,47 +90,37 @@ export interface TaskTimelineProps {
   taskState: TaskState;
 }
 
+function RenderPhase(phase: Phase) {
+  return (
+    <TimelineItem key={phase.id}>
+      <TimelineOppositeContent color="text.secondary">
+        <Typography variant="overline" color="textSecondary" style={{ textAlign: 'justify' }}>
+          {phase.original_estimate_millis
+            ? format(new Date(phase.original_estimate_millis), "hh:mm aaaaa'm'")
+            : null}
+        </Typography>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot color={colorDot(phase.events)} />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <Typography variant="overline" color="textSecondary" style={{ textAlign: 'justify' }}>
+          {phase.id}. {phase.category}
+        </Typography>
+        <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
+          {NestedEvents(phase.events, phase.final_event_id)}
+        </TreeView>
+      </TimelineContent>
+    </TimelineItem>
+  );
+}
+
 export function TaskTimeline({ taskState }: TaskTimelineProps): JSX.Element {
   const phases = taskState.phases ? Object.values(taskState.phases) : [];
   return (
     <StyledTimeLine className={classes.timelineRoot}>
-      {phases.map((phase, idx) =>
-        phase.events ? (
-          <TimelineItem key={phase.id}>
-            <TimelineOppositeContent color="text.secondary">
-              <Typography variant="overline" color="textSecondary" style={{ textAlign: 'justify' }}>
-                {phase.original_estimate_millis
-                  ? format(new Date(phase.original_estimate_millis), "hh:mm aaaaa'm'")
-                  : null}
-              </Typography>
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot color={colorDot(phase.events)} />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <TreeView
-                defaultCollapseIcon={<ExpandMoreIcon />}
-                defaultExpandIcon={<ChevronRightIcon />}
-              >
-                {Object.values(phase.events).map((event) => (
-                  <TreeItem
-                    key={event.id}
-                    nodeId={event.id.toString()}
-                    label={event.name || 'unknown'}
-                  >
-                    {event.deps && event.deps.length > 0
-                      ? Object.values(event.deps).map((childId) =>
-                          NestedEvents(phase.events, childId),
-                        )
-                      : null}
-                  </TreeItem>
-                ))}
-              </TreeView>
-            </TimelineContent>
-          </TimelineItem>
-        ) : null,
-      )}
+      {phases.map((phase) => RenderPhase(phase))}
     </StyledTimeLine>
   );
 }
