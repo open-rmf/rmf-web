79c3abc627d92a77ecfc7929cd857515d2aae325
Mohamad <mohamad@openrobotics.org>
2022-02-11 16:02:01 +0300

    add task-log test
    Signed-off-by: Mohamad <mohamad@openrobotics.org>

--- packages/dashboard/src/components/tasks/tests/task-logs.test.tsx
+++ packages/dashboard/src/components/tasks/tests/task-logs.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from '../../tests/test-utils';
 import { TaskLogs } from '../task-logs';
-import { makeTaskEventLog, makeTaskEventState } from './make-tasks';
+import { makeTaskLog, makeTaskState } from './make-tasks';
 
 // react-leaflet doesn't work well in jsdom.
 jest.mock('./../../schedule-visualizer', () => () => null);
 
 it('renders without crashing', async () => {
   URL.createObjectURL = jest.fn();
-  const taskLog = makeTaskEventLog('0');
-  const taskState = makeTaskEventState('0');
+  const taskLog = makeTaskLog('0');
+  const taskState = makeTaskState('0');
 
   const root = render(<TaskLogs taskLog={taskLog} taskState={taskState} />);
   root.unmount();
