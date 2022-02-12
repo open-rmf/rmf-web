bda71c45864b16775c5d85259e47b9540ac34d1d
Mohamad <mohamad@openrobotics.org>
2022-02-12 17:40:55 +0300

    add simple test for tasks panel
    Signed-off-by: Mohamad <mohamad@openrobotics.org>

--- packages/dashboard/src/components/tasks/tests/task-panel.test.tsx
+++ packages/dashboard/src/components/tasks/tests/task-panel.test.tsx
@@ -2,9 +2,23 @@
 // import userEvent from '@testing-library/user-event';
 import React from 'react';
 // import { TaskSummary as RmfTaskSummary, TaskType as RmfTaskType } from 'rmf-models';
-// import { render } from '../../tests/test-utils';
-// import { TaskPanel, TaskPanelProps } from '../task-panel';
-// import { makeTaskWithPhases } from './make-tasks';
+import { render } from '../../tests/test-utils';
+import { TaskPanel, TaskPanelProps } from '../task-panel';
+import { makeTaskState } from './make-tasks';
+
+// react-leaflet doesn't work well in jsdom.
+jest.mock('./../../schedule-visualizer', () => () => null);
+
+it('renders without crashing', async () => {
+  URL.createObjectURL = jest.fn();
+  const taskState = makeTaskState('0');
+
+  const root = render(<TaskPanel tasks={[taskState]} />);
+  root.unmount();
+  (URL.createObjectURL as jest.Mock).mockReset();
+});
+
+export {};
 
 // describe('TaskPanel', () => {
 //   describe('task detail', () => {
