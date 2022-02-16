b06b85c84b0ee2381a83de29f3833d4a4908c278
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-02-16 17:32:26 +0800

    fix task cancellation in dashboard
    Signed-off-by: Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>

--- packages/dashboard/src/components/tasks/task-panel.tsx
+++ packages/dashboard/src/components/tasks/task-panel.tsx
@@ -146,7 +146,7 @@ export function TaskPanel({
       return;
     }
     try {
-      // await cancelTask(selectedTask.summary);
+      await cancelTask(selectedTask);
       setSnackbarMessage('Successfully cancelled task');
       setSnackbarSeverity('success');
       setOpenSnackbar(true);
