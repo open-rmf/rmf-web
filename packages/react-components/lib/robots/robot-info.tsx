194d877769b87360f82884390367ad053dc1708e
Aaron Chong <aaronchongth@gmail.com>
2022-04-18 17:11:35 +0800

    Justified left and working ExpandableIssue
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/robots/robot-info.tsx
+++ packages/react-components/lib/robots/robot-info.tsx
@@ -9,14 +9,13 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  // Collapse,
+  Collapse,
 } from '@mui/material';
-// import ExpandLess from '@mui/icons-material/ExpandLess';
-// import ExpandMore from '@mui/icons-material/ExpandMore';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 import ReportProblemRoundedIcon from '@mui/icons-material/ReportProblemRounded';
-import type { TaskState, RobotState } from 'api-client';
-// import React, { useState } from 'react';
-import React from 'react';
+import type { TaskState, RobotState, Issue } from 'api-client';
+import React, { useState } from 'react';
 import { CircularProgressBar } from './circular-progress-bar';
 import { LinearProgressBar } from './linear-progress-bar';
 
@@ -46,29 +45,36 @@ const StyledDiv = styled('div')(() => ({
 type TaskStatus = Required<TaskState>['status'];
 type RobotIssues = Required<RobotState>['issues'];
 
-// const ExpandableIssue = (category: string, detail: string): JSX.Element => {
-//   const [open, setOpen] = useState(false);
-//   const handleClick = () => {
-//     setOpen(!open);
-//   };
+export interface ExpandableIssueProps {
+  category: string;
+  detail: string;
+  onClick?: () => void;
+}
 
-//   return (
-//     <div>
-//       <ListItem button onClick={handleClick}>
-//         <ListItemIcon>
-//           <ReportProblemRoundedIcon />
-//         </ListItemIcon>
-//         <ListItemText>{category}</ListItemText>
-//         {open ? <ExpandLess /> : <ExpandMore />}
-//       </ListItem>
-//       <Collapse in={open} timeout="auto" unmountOnExit>
-//         <ListItem>
-//           <ListItemText>{detail}</ListItemText>
-//         </ListItem>
-//       </Collapse>
-//     </div>
-//   );
-// };
+const ExpandableIssue = (props: ExpandableIssueProps): JSX.Element => {
+  const { category, detail } = props;
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <ReportProblemRoundedIcon />
+        </ListItemIcon>
+        <ListItemText>{category}</ListItemText>
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <ListItem>
+          <ListItemText>{detail}</ListItemText>
+        </ListItem>
+      </Collapse>
+    </div>
+  );
+};
 
 export interface RobotInfoProps {
   robotName: string;
@@ -155,30 +161,30 @@ export function RobotInfo({
         </Grid>
 
         <Divider />
+        <div style={{ marginBottom: theme.spacing(1) }}></div>
 
-        <Grid container item xs={12} justifyContent="center">
+        <Grid container item xs={12} justifyContent="left">
           <Typography variant="h6" gutterBottom>
             Issues
           </Typography>
         </Grid>
-        <Grid container item xs={12} justifyContent="center">
+        <Grid container item xs={12} justifyContent="left">
           <List dense disablePadding component="div" role="list">
-            {robotIssuesArray.map((issue, i) => (
-              // <ExpandableIssue key={i}
-              //   category={issue.category !== undefined ? issue.category : 'unnamed category'}
-              //   detail={JSON.stringify(issue.detail)} />
-              <ListItem key={i}>
-                <ListItemIcon>
-                  <ReportProblemRoundedIcon />
-                </ListItemIcon>
-                <ListItemText>{JSON.stringify(issue)}</ListItemText>
-              </ListItem>
+            {robotIssuesArray.map((issue: Issue, i) => (
+              <ExpandableIssue
+                key={i}
+                category={issue.category !== undefined ? issue.category : 'unnamed category'}
+                detail={JSON.stringify(issue.detail)}
+              />
             ))}
             {robotIssuesArray.length === 0 && (
               <Typography gutterBottom>No pending issues.</Typography>
             )}
           </List>
         </Grid>
+
+        <Divider />
+        <div style={{ marginBottom: theme.spacing(1) }}></div>
       </Grid>
     </StyledDiv>
   );
