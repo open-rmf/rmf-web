3d4a4304a151ad1310c36d2d81222de6fc8c1597
Aaron Chong <aaronchongth@gmail.com>
2022-04-18 14:26:39 +0800

    Making robot issue collapsable
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/robots/robot-info.tsx
+++ packages/react-components/lib/robots/robot-info.tsx
@@ -9,9 +9,13 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  // Collapse,
 } from '@mui/material';
+// import ExpandLess from '@mui/icons-material/ExpandLess';
+// import ExpandMore from '@mui/icons-material/ExpandMore';
 import ReportProblemRoundedIcon from '@mui/icons-material/ReportProblemRounded';
 import type { TaskState, RobotState } from 'api-client';
+// import React, { useState } from 'react';
 import React from 'react';
 import { CircularProgressBar } from './circular-progress-bar';
 import { LinearProgressBar } from './linear-progress-bar';
@@ -41,6 +45,30 @@ const StyledDiv = styled('div')(() => ({
 
 type TaskStatus = Required<TaskState>['status'];
 type RobotIssues = Required<RobotState>['issues'];
+
+// const ExpandableIssue = (category: string, detail: string): JSX.Element => {
+//   const [open, setOpen] = useState(false);
+//   const handleClick = () => {
+//     setOpen(!open);
+//   };
+
+//   return (
+//     <div>
+//       <ListItem button onClick={handleClick}>
+//         <ListItemIcon>
+//           <ReportProblemRoundedIcon />
+//         </ListItemIcon>
+//         <ListItemText>{category}</ListItemText>
+//         {open ? <ExpandLess /> : <ExpandMore />}
+//       </ListItem>
+//       <Collapse in={open} timeout="auto" unmountOnExit>
+//         <ListItem>
+//           <ListItemText>{detail}</ListItemText>
+//         </ListItem>
+//       </Collapse>
+//     </div>
+//   );
+// };
 
 export interface RobotInfoProps {
   robotName: string;
@@ -135,8 +163,11 @@ export function RobotInfo({
         </Grid>
         <Grid container item xs={12} justifyContent="center">
           <List dense disablePadding component="div" role="list">
-            {robotIssuesArray.map((issue) => (
-              <ListItem>
+            {robotIssuesArray.map((issue, i) => (
+              // <ExpandableIssue key={i}
+              //   category={issue.category !== undefined ? issue.category : 'unnamed category'}
+              //   detail={JSON.stringify(issue.detail)} />
+              <ListItem key={i}>
                 <ListItemIcon>
                   <ReportProblemRoundedIcon />
                 </ListItemIcon>
