02a8fdf600b13c130c56e4d4ead40464a166df12
Aaron Chong <aaronchongth@gmail.com>
2022-06-03 00:37:19 +0800

    Reverting use of -p flag since already did a cd
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/ros-translator/ros_translator/typescript/test/test.sh
+++ packages/ros-translator/ros_translator/typescript/test/test.sh
@@ -5,6 +5,6 @@ cd $(dirname $0)
 rm -rf out
 PIPENV_MAX_DEPTH=6 pipenv run python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
 echo 'test build'
-tsc --noEmit -p "$(pwd)/tsconfig.json"
+tsc --noEmit
 echo 'ok'
 ts-node -T $(npx which jasmine) "$@"
