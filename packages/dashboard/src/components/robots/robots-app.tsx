554bf4f29470be7a87a4e1bbf0cc44320a66f6ac
Teo Koon Peng <koonpeng@openrobotics.org>
2022-06-21 17:44:29 +0800

    fully support auto refresh in robots app
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/dashboard/src/components/robots/robots-app.tsx
+++ packages/dashboard/src/components/robots/robots-app.tsx
@@ -8,54 +8,84 @@ import { RmfAppContext } from '../rmf-app';
 
 export const RobotsApp = createMicroApp('Robots', () => {
   const rmf = React.useContext(RmfAppContext);
-  const [robots, setRobots] = React.useState<RobotTableData[]>([]);
+
+  const [fleets, setFleets] = React.useState<string[]>([]);
+  const [robots, setRobots] = React.useState<Record<string, RobotTableData[]>>({});
   React.useEffect(() => {
     if (!rmf) {
       return;
     }
 
-    // TODO: support auto refresh
-    rmf.fleetsObs.subscribe(async (fleets) => {
-      // fetch active tasks
-      const taskIds = fleets
-        .flatMap(
-          (fleet) => fleet.robots && Object.values(fleet.robots).map((robot) => robot.task_id),
-        )
-        .filter((taskId) => taskId !== '');
-      const tasks =
-        taskIds.length > 0
-          ? (await rmf.tasksApi.queryTaskStatesTasksGet(taskIds.join(','))).data.reduce(
-              (acc, task) => {
-                acc[task.booking.id] = task;
-                return acc;
-              },
-              {} as Record<string, TaskState>,
-            )
-          : {};
-
-      setRobots(
-        fleets.flatMap((fleet) =>
-          fleet.robots
-            ? Object.entries(fleet.robots).map<RobotTableData>(([name, robot]) => ({
-                fleet: fleet.name || '',
-                name,
-                battery: robot.battery,
-                status: robot.status,
-                estFinishTime:
-                  robot.task_id && tasks[robot.task_id]
-                    ? tasks[robot.task_id].estimate_millis
-                    : undefined,
-              }))
-            : [],
-        ),
+    const sub = rmf.fleetsObs.subscribe((fleets) => {
+      setFleets(
+        fleets.reduce<string[]>((acc, f) => {
+          if (f.name) {
+            acc.push(f.name);
+          }
+          return acc;
+        }, []),
       );
+      setRobots({});
     });
+    return () => sub.unsubscribe();
   }, [rmf]);
+
+  React.useEffect(() => {
+    if (!rmf) {
+      return;
+    }
+    const subs = fleets.map((f) =>
+      rmf.getFleetStateObs(f).subscribe(async (fleet) => {
+        // fetch active tasks
+        const taskIds = fleet.robots
+          ? Object.values(fleet.robots).reduce<string[]>((acc, robot) => {
+              if (robot.task_id) {
+                acc.push(robot.task_id);
+              }
+              return acc;
+            }, [])
+          : [];
+
+        const tasks =
+          taskIds.length > 0
+            ? (await rmf.tasksApi.queryTaskStatesTasksGet(taskIds.join(','))).data.reduce(
+                (acc, task) => {
+                  acc[task.booking.id] = task;
+                  return acc;
+                },
+                {} as Record<string, TaskState>,
+              )
+            : {};
+
+        setRobots((prev) => {
+          if (!fleet.name) {
+            return prev;
+          }
+          return {
+            ...prev,
+            [fleet.name]: fleet.robots
+              ? Object.entries(fleet.robots).map<RobotTableData>(([name, robot]) => ({
+                  fleet: fleet.name || '',
+                  name,
+                  battery: robot.battery,
+                  status: robot.status,
+                  estFinishTime:
+                    robot.task_id && tasks[robot.task_id]
+                      ? tasks[robot.task_id].estimate_millis
+                      : undefined,
+                }))
+              : [],
+          };
+        });
+      }),
+    );
+    return () => subs.forEach((sub) => sub.unsubscribe());
+  }, [rmf, fleets]);
 
   return (
     <TableContainer>
       <RobotTable
-        robots={robots}
+        robots={Object.values(robots).flatMap((r) => r)}
         onRobotClick={(_ev, robot) => {
           AppEvents.robotSelect.next([robot.fleet, robot.name]);
         }}
