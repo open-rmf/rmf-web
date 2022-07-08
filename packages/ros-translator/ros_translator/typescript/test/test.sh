c982900cb11417ae12ef175297e6fb4f0aac1a78
Teo Koon Peng <koonpeng@openrobotics.org>
2022-07-08 10:20:41 +0800

    update ci
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- packages/ros-translator/ros_translator/typescript/test/test.sh
+++ packages/ros-translator/ros_translator/typescript/test/test.sh
@@ -3,8 +3,8 @@ set -e
 
 cd $(dirname $0)
 rm -rf out
-PIPENV_MAX_DEPTH=6 pipenv run python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
+python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
 echo 'test build'
 tsc --noEmit
 echo 'ok'
-ts-node -T $(npx which jasmine) "$@"
+ts-node -T ../../../node_modules/jasmine/bin/jasmine.js "$@"
