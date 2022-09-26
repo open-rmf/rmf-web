faea601c61962140e2c17b69a82fbf1e31f37e24
Aaron Chong <aaronchongth@gmail.com>
2022-09-27 00:36:49 +0800

    Clean tasks now submit a sequence of goto and perform_action
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/tasks/create-task.tsx
+++ packages/react-components/lib/tasks/create-task.tsx
@@ -505,29 +505,92 @@ interface CleanTaskFormProps {
 
 function CleanTaskForm({ taskDesc, cleaningZones, onChange }: CleanTaskFormProps) {
   return (
-    <Autocomplete
-      id="cleaning-zone"
-      freeSolo
-      fullWidth
-      options={cleaningZones}
-      value={taskDesc.zone}
-      onChange={(_ev, newValue) =>
-        newValue !== null &&
-        onChange({
-          ...taskDesc,
-          zone: newValue,
-        })
-      }
-      onBlur={(ev) => onChange({ ...taskDesc, zone: (ev.target as HTMLInputElement).value })}
-      renderInput={(params) => <TextField {...params} label="Cleaning Zone" margin="normal" />}
-    />
+    <>
+      <Autocomplete
+        id="clean-location"
+        freeSolo
+        fullWidth
+        options={cleaningZones}
+        onChange={(_ev, newValue) =>
+          newValue !== null &&
+          onChange(
+            defaultCleanTask(
+              newValue,
+              taskDesc.phases[0].activity.description.activities[1].description.description
+                .task_name,
+            ),
+          )
+        }
+        onBlur={(ev) =>
+          onChange(
+            defaultCleanTask(
+              (ev.target as HTMLInputElement).value,
+              taskDesc.phases[0].activity.description.activities[1].description.description
+                .task_name,
+            ),
+          )
+        }
+        renderInput={(params) => <TextField {...params} label="Clean Location" margin="normal" />}
+      />
+      <Autocomplete
+        id="cleaning-task"
+        freeSolo
+        fullWidth
+        options={[]}
+        value={taskDesc.zone}
+        onChange={(_ev, newValue) =>
+          newValue !== null &&
+          onChange(
+            defaultCleanTask(
+              taskDesc.phases[0].activity.description.activities[0].description,
+              newValue,
+            ),
+          )
+        }
+        onBlur={(ev) =>
+          onChange(
+            defaultCleanTask(
+              taskDesc.phases[0].activity.description.activities[0].description,
+              (ev.target as HTMLInputElement).value,
+            ),
+          )
+        }
+        renderInput={(params) => <TextField {...params} label="Cleaning Task" margin="normal" />}
+      />
+    </>
   );
 }
 
-function defaultCleanTask(): Record<string, any> {
+function defaultCleanTask(clean_location: string = '', clean_task_name = ''): Record<string, any> {
   return {
-    zone: '',
-    type: '',
+    category: 'clean',
+    phases: [
+      {
+        activity: {
+          category: 'sequence',
+          description: {
+            activities: [
+              {
+                category: 'go_to_place',
+                description: clean_location,
+              },
+              {
+                category: 'perform_action',
+                description: {
+                  // TODO(AA): To obtain accurate duration estimate.
+                  unix_millis_action_duration_estimate: 60000,
+                  category: 'clean',
+                  description: {
+                    clean_task_name: clean_task_name,
+                  },
+                  use_tool_sink: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
   };
 }
 
