ba287e897061218a15f2401e71a0f63d27a2a364
Aaron Chong <aaronchongth@gmail.com>
2022-10-13 11:30:24 +0800

    Manually switching to Infrastructure tab
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
+++ packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
@@ -1,17 +1,21 @@
-import { getDoorCell } from '../utils';
+import { getAppBar, getDoorCell } from '../utils';
 
 describe('door controls', () => {
   it('clicking on open button opens the door', async () => {
-    const doorCell = await getDoorCell('main_door');
+    const appBar = await getAppBar();
+    await (await appBar.$('button[aria-label="Infrastructure"]')).click();
+    const doorCell = await $('[aria-labelledby="door-cell-main_door"]');
     const openBtn = await doorCell.$('button=Open');
     await openBtn.click();
     await expect(doorCell.$('[role=status]')).toHaveText('OPEN', { wait: 30000 });
   });
 
   it('clicking on close button closes the door', async () => {
-    const doorCell = await getDoorCell('main_door');
-    const openBtn = await doorCell.$('button=Close');
-    await openBtn.click();
+    const appBar = await getAppBar();
+    await (await appBar.$('button[aria-label="Infrastructure"]')).click();
+    const doorCell = await $('[aria-labelledby="door-cell-main_door"]');
+    const closeBtn = await doorCell.$('button=Close');
+    await closeBtn.click();
     await expect(doorCell.$('[role=status]')).toHaveText('CLOSED', { wait: 30000 });
   });
 });
