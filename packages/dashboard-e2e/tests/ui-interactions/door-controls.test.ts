83ea539d94ebfb5acac0ff55f085ddfe64be5806
Aaron Chong <aaronchongth@gmail.com>
2022-11-01 22:35:22 +0800

    Added missing labels and roles to door card and status
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
+++ packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
@@ -4,16 +4,18 @@ describe('door controls', () => {
   it('clicking on open button opens the door', async () => {
     const appBar = await getAppBar();
     await (await appBar.$('button[aria-label="Infrastructure"]')).click();
-    await (await $('button[aria-label="open-main_door"]')).click();
-    await expect($('[role=status]')).toHaveText('OPEN', { wait: 30000 });
+    const doorCell = await $('[aria-labelledby="door-cell-main_door"]');
+    const openBtn = await doorCell.$('button[aria-label="open-main_door"]');
+    await openBtn.click();
+    await expect(doorCell.$('[role=status]')).toHaveText('OPEN', { wait: 30000 });
   });
 
-  // it('clicking on close button closes the door', async () => {
-  //   const appBar = await getAppBar();
-  //   await (await appBar.$('button[aria-label="Infrastructure"]')).click();
-  //   const doorCell = await $('[aria-labelledby="door-cell-main_door"]');
-  //   const closeBtn = await doorCell.$('button=Close');
-  //   await closeBtn.click();
-  //   await expect(doorCell.$('[role=status]')).toHaveText('CLOSED', { wait: 30000 });
-  // });
+  it('clicking on close button closes the door', async () => {
+    const appBar = await getAppBar();
+    await (await appBar.$('button[aria-label="Infrastructure"]')).click();
+    const doorCell = await $('[aria-labelledby="door-cell-main_door"]');
+    const closeBtn = await doorCell.$('button[aria-label="close-main_door"]');
+    await closeBtn.click();
+    await expect(doorCell.$('[role=status]')).toHaveText('CLOSED', { wait: 30000 });
+  });
 });
