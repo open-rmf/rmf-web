3dc396b7876daaebfd124c254b45a45df149fdb7
fierro <fierrofenix@gmail.com>
2022-12-16 14:23:35 -0300

    Improving style and adjusting to the design
    Signed-off-by: fierro <fierrofenix@gmail.com>

--- packages/dashboard/src/components/task-alert.tsx
+++ packages/dashboard/src/components/task-alert.tsx
@@ -1,4 +1,14 @@
-import { Button, Grid, Theme, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  LinearProgressProps,
+  Theme,
+  Typography,
+  Divider,
+  TextField,
+} from '@mui/material';
 import React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -12,18 +22,44 @@ import { Subscription } from 'rxjs';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    paper: { minWidth: theme.spacing(2) },
+    title: {
+      '&.MuiDialogTitle-root': {
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        fontWeight: 'bold',
+      },
+    },
+    subtitle: {
+      '&.MuiTypography-root': {
+        fontWeight: 'bold',
+      },
+    },
   }),
 );
 export interface AlertProps {
   robots: RobotAlert[];
 }
 
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '100%', mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value * 100,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
 export function AlertComponentStore({ robots }: AlertProps): JSX.Element {
   const rmf = React.useContext(RmfAppContext);
   const classes = useStyles();
   const [count, setCount] = React.useState(0);
-  const [completed, setCompleted] = React.useState(false);
+  const [completed, setCompleted] = React.useState(true);
 
   const handleClose = () => {
     setCount(count + 1);
@@ -40,7 +76,7 @@ export function AlertComponentStore({ robots }: AlertProps): JSX.Element {
       if (r.robot?.task_id) {
         return subs.push(
           rmf.getTaskStateObs(r.robot.task_id).subscribe((task) => {
-            if (task.status === Status.Completed) {
+            if (task.status === Status.Underway) {
               setCompleted(true);
             }
           }),
@@ -57,40 +93,61 @@ export function AlertComponentStore({ robots }: AlertProps): JSX.Element {
       console.log('Se completó la tarea');
       return (
         <Dialog open={count < robots.length} onClose={handleClose} maxWidth="xs">
-          <DialogTitle>Alert</DialogTitle>
-          <DialogTitle>Task State</DialogTitle>
+          <DialogTitle className={classes.title}>Alert</DialogTitle>
+          <Divider />
+          <DialogTitle className={classes.title}>Task State</DialogTitle>
+          <Box sx={{ width: '100%' }}>
+            <LinearProgressWithLabel value={robot.robot?.battery ? robot.robot?.battery : -1} />
+          </Box>
           <DialogContent>
-            <Grid container>
+            <Grid container mb={1} alignItems="center" spacing={1}>
               <Grid item xs={3}>
-                <Typography>Task</Typography>
-              </Grid>
-              <Grid item xs={3}>
-                <Typography>{robot.task?.booking.id}</Typography>
-              </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={3}>
-                <Typography>Location</Typography>
-              </Grid>
-              <Grid item xs={3}>
-                <Typography>{robot.robot?.location?.map}</Typography>
-              </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={3}>
-                <Typography>Message</Typography>
+                <Typography className={classes.subtitle}>Task</Typography>
               </Grid>
               <Grid item xs={9}>
-                <Typography>
-                  AMR ready for pickup at CSSD LOT3; cart not found, please push cart in CSSD LOT3
-                </Typography>
+                <TextField
+                  size="small"
+                  value={robot.task?.booking.id}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid container mb={1} alignItems="center" spacing={1}>
+              <Grid item xs={3}>
+                <Typography className={classes.subtitle}>Location</Typography>
+              </Grid>
+              <Grid item xs={9}>
+                <TextField
+                  size="small"
+                  value={robot.robot?.location?.map}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item xs={3}>
+                <Typography className={classes.subtitle}>Message</Typography>
+              </Grid>
+              <Grid item xs={9}>
+                <TextField
+                  size="small"
+                  multiline
+                  value="AMR ready for pickup at CSSD LOT3; cart not found, please push cart in CSSD LOT3"
+                  maxRows={4}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                />
               </Grid>
             </Grid>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose}>Disagree</Button>
             <Button onClick={handleClose} autoFocus>
-              Agree
+              Close
             </Button>
           </DialogActions>
         </Dialog>
