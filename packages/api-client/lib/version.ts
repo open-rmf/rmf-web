078be2b0fdd51e2d297598da8792fbed4fe8f21d
fierro <fierrofenix@gmail.com>
2023-03-13 16:26:26 -0300

    Generate api client to get the new field
    Signed-off-by: fierro <fierrofenix@gmail.com>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: '796aa7a6b92f0df4ec0872dc71df915c3a58a0d3',
+  rmfServer: '8b650451936ff9f2dcb7c18ab7022d27f5ca9ec1',
   openapiGenerator: '6.2.1',
 };
