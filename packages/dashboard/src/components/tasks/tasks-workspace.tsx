f31242cdebb5de5bd037cd7fbeb205af4c4be74c
Angatupyry <fierrofenix@gmail.com>
2023-04-18 09:31:40 -0400

    Add map microapp into task section in order to make it similar to figma
    Signed-off-by: Angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/tasks/tasks-workspace.tsx
+++ packages/dashboard/src/components/tasks/tasks-workspace.tsx
@@ -1,6 +1,12 @@
 import { WorkspaceState } from '../workspace';
 
 export const tasksWorkspace: WorkspaceState = {
-  layout: [{ i: 'tasks', x: 2, y: 0, w: 8, h: 12 }],
-  windows: [{ key: 'tasks', appName: 'Tasks' }],
+  layout: [
+    { i: 'tasks', x: 0, y: 0, w: 7, h: 12 },
+    { i: 'map', x: 8, y: 0, w: 5, h: 12 },
+  ],
+  windows: [
+    { key: 'tasks', appName: 'Tasks' },
+    { key: 'map', appName: 'Map' },
+  ],
 };
