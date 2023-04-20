86f171e44bd57d5cb90b0c0424b823c02c270bf3
Angatupyry <fierrofenix@gmail.com>
2023-04-20 14:48:15 -0400

    Remove robot microapp and set map microapps bigger
    Signed-off-by: Angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/robots/robots-workspace.tsx
+++ packages/dashboard/src/components/robots/robots-workspace.tsx
@@ -2,13 +2,11 @@ import { WorkspaceState } from '../workspace';
 
 export const robotsWorkspace: WorkspaceState = {
   layout: [
-    { i: 'robots', x: 2, y: 0, w: 6, h: 12 },
-    { i: 'robots-info', x: 8, y: 0, w: 2, h: 6 },
-    { i: 'map', x: 8, y: 6, w: 2, h: 6 },
+    { i: 'robots', x: 0, y: 0, w: 7, h: 12 },
+    { i: 'map', x: 8, y: 0, w: 5, h: 12 },
   ],
   windows: [
     { key: 'robots', appName: 'Robots' },
-    { key: 'robots-info', appName: 'Robot Info' },
     { key: 'map', appName: 'Map' },
   ],
 };
