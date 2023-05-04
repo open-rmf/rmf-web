2311f094a201a3ed221d60b267a67ae270c35849
angatupyry <fierrofenix@gmail.com>
2023-05-04 14:56:57 -0400

    Table is used insted of cards to show doors
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/doors-app.tsx
+++ packages/dashboard/src/components/doors-app.tsx
@@ -1,20 +1,27 @@
-import { CardActions, Grid } from '@mui/material';
+import { Button, SxProps, useTheme } from '@mui/material';
 import { BuildingMap, DoorState } from 'api-client';
 import React from 'react';
 import {
-  DoorCard as BaseDoorCard,
   DoorCardProps as BaseDoorCardProps,
-  DoorControls,
+  doorModeToString,
+  doorTypeToString,
 } from 'react-components';
-import { DoorMode as RmfDoorMode } from 'rmf-models';
+import { DoorMode, DoorMode as RmfDoorMode } from 'rmf-models';
 import { createMicroApp } from './micro-app';
 import { RmfAppContext } from './rmf-app';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 type DoorCardProps = Omit<BaseDoorCardProps, 'mode'>;
 
 const DoorCard = ({ children, ...otherProps }: DoorCardProps) => {
+  const theme = useTheme();
   const rmf = React.useContext(RmfAppContext);
   const [doorState, setDoorState] = React.useState<DoorState | null>(null);
+
   React.useEffect(() => {
     if (!rmf) {
       return;
@@ -23,10 +30,74 @@ const DoorCard = ({ children, ...otherProps }: DoorCardProps) => {
     return () => sub.unsubscribe();
   }, [rmf, otherProps.name]);
 
+  const labelStyle = React.useMemo<SxProps>(() => {
+    const { current_mode } = doorState ?? {};
+    const mode = current_mode?.value;
+
+    const disabled = {
+      backgroundColor: theme.palette.action.disabledBackground,
+      color: theme.palette.action.disabled,
+    };
+    const open = {
+      backgroundColor: theme.palette.success.main,
+      color: theme.palette.success.contrastText,
+    };
+    const closed = {
+      backgroundColor: theme.palette.error.main,
+      color: theme.palette.error.contrastText,
+    };
+    const moving = {
+      backgroundColor: theme.palette.warning.main,
+      color: theme.palette.warning.contrastText,
+    };
+
+    switch (mode) {
+      case DoorMode.MODE_OPEN:
+        return open;
+      case DoorMode.MODE_CLOSED:
+        return closed;
+      case DoorMode.MODE_MOVING:
+        return moving;
+      default:
+        return disabled;
+    }
+  }, [theme, doorState]);
+
   return (
-    <BaseDoorCard mode={doorState ? doorState.current_mode.value : -1} {...otherProps}>
-      {children}
-    </BaseDoorCard>
+    <TableRow key={otherProps.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+      <TableCell>{otherProps.name}</TableCell>
+      <TableCell>{otherProps.level}</TableCell>
+      <TableCell>{doorTypeToString(otherProps.type)}</TableCell>
+      <TableCell sx={labelStyle}>
+        {doorState ? doorModeToString(doorState.current_mode.value) : -1}
+      </TableCell>
+      <TableCell align="right">
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() =>
+            rmf?.doorsApi.postDoorRequestDoorsDoorNameRequestPost(otherProps.name, {
+              mode: RmfDoorMode.MODE_OPEN,
+            })
+          }
+        >
+          Open
+        </Button>
+      </TableCell>
+      <TableCell align="right">
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() =>
+            rmf?.doorsApi.postDoorRequestDoorsDoorNameRequestPost(otherProps.name, {
+              mode: RmfDoorMode.MODE_CLOSED,
+            })
+          }
+        >
+          Close
+        </Button>
+      </TableCell>
+    </TableRow>
   );
 };
 
@@ -43,36 +114,32 @@ export const DoorsApp = createMicroApp('Doors', () => {
   }, [rmf]);
 
   return (
-    <Grid container>
-      {buildingMap &&
-        buildingMap.levels.flatMap((level) =>
-          level.doors.map((door) => (
-            <DoorCard
-              key={door.name}
-              name={door.name}
-              level={level.name}
-              type={door.door_type}
-              sx={{ width: 200 }}
-              aria-labelledby={`door-cell-${door.name}`}
-            >
-              <CardActions sx={{ justifyContent: 'center' }}>
-                <DoorControls
-                  doorName={door.name}
-                  onOpenClick={() =>
-                    rmf?.doorsApi.postDoorRequestDoorsDoorNameRequestPost(door.name, {
-                      mode: RmfDoorMode.MODE_OPEN,
-                    })
-                  }
-                  onCloseClick={() =>
-                    rmf?.doorsApi.postDoorRequestDoorsDoorNameRequestPost(door.name, {
-                      mode: RmfDoorMode.MODE_CLOSED,
-                    })
-                  }
-                />
-              </CardActions>
-            </DoorCard>
-          )),
-        )}
-    </Grid>
+    <Table stickyHeader size="small" aria-label="door-table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Level</TableCell>
+          <TableCell>Type</TableCell>
+          <TableCell>Door State</TableCell>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {buildingMap &&
+          buildingMap.levels.flatMap((level) =>
+            level.doors.map((door) => (
+              <DoorCard
+                key={door.name}
+                name={door.name}
+                level={level.name}
+                type={door.door_type}
+                sx={{ width: 200 }}
+                aria-labelledby={`door-cell-${door.name}`}
+              ></DoorCard>
+            )),
+          )}
+      </TableBody>
+    </Table>
   );
 });
