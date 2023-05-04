66c60d37005918125807a9f82ec0206e19a44b79
angatupyry <fierrofenix@gmail.com>
2023-05-04 14:52:12 -0400

    Add doors and lifts to robot section
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/robots/robots-workspace.tsx
+++ packages/dashboard/src/components/robots/robots-workspace.tsx
@@ -2,13 +2,16 @@ import { WorkspaceState } from '../workspace';
 
 export const robotsWorkspace: WorkspaceState = {
   layout: [
-    { i: 'robots', x: 2, y: 0, w: 6, h: 12 },
-    { i: 'robots-info', x: 8, y: 0, w: 2, h: 6 },
-    { i: 'map', x: 8, y: 6, w: 2, h: 6 },
+    { i: 'robots', x: 0, y: 0, w: 7, h: 5 },
+    { i: 'map', x: 8, y: 0, w: 5, h: 12 },
+    { i: 'doors', x: 0, y: 0, w: 7, h: 4 },
+    { i: 'lifts', x: 0, y: 0, w: 7, h: 3 },
   ],
   windows: [
     { key: 'robots', appName: 'Robots' },
     { key: 'robots-info', appName: 'Robot Info' },
     { key: 'map', appName: 'Map' },
+    { key: 'doors', appName: 'Doors' },
+    { key: 'lifts', appName: 'Lifts' },
   ],
 };
