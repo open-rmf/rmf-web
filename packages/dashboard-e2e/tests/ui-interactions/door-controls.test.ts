b91ba5afc02c39910f2c8b8f890df7d0771fbfdf
angatupyry <fierrofenix@gmail.com>
2023-05-11 14:33:52 -0400

    Add e2e to test door behavior
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
+++ packages/dashboard-e2e/tests/ui-interactions/door-controls.test.ts
@@ -3,26 +3,29 @@ import { getAppBar } from '../utils';
 describe('door controls', () => {
   it('door starts closed', async () => {
     const appBar = await getAppBar();
-    await (await appBar.$('button[aria-label="Infrastructure"]')).click();
-    const doorCell = await $('[aria-labelledby="door-cell-coe_door"]');
-    await expect(doorCell.$('[role=status]')).toHaveText('CLOSED', { wait: 180000 });
+    await (await appBar.$('button[aria-label="System Overview"]')).click();
+
+    const doorState = $('.MuiTableCell-alignLeft:nth-child(4) p');
+
+    expect(doorState.getText('CLOSED'));
   });
 
   it('clicking on open button opens the door', async () => {
     const appBar = await getAppBar();
-    await (await appBar.$('button[aria-label="Infrastructure"]')).click();
-    const doorCell = await $('[aria-labelledby="door-cell-coe_door"]');
-    const openBtn = await doorCell.$('button[aria-label="open-coe_door"]');
-    await openBtn.click();
-    await expect(doorCell.$('[role=status]')).toHaveText('OPEN', { wait: 180000 });
+    await (await appBar.$('button[aria-label="System Overview"]')).click();
+    const openButton = $('button[aria-label="open"]');
+    openButton.click();
+    const doorState = $('.MuiTableCell-alignLeft:nth-child(4) p');
+    expect(doorState.getText('OPEN'));
   });
 
   it('clicking on close button closes the door', async () => {
     const appBar = await getAppBar();
-    await (await appBar.$('button[aria-label="Infrastructure"]')).click();
-    const doorCell = await $('[aria-labelledby="door-cell-coe_door"]');
-    const closeBtn = await doorCell.$('button[aria-label="close-coe_door"]');
-    await closeBtn.click();
-    await expect(doorCell.$('[role=status]')).toHaveText('CLOSED', { wait: 180000 });
+    await (await appBar.$('button[aria-label="System Overview"]')).click();
+    const openButton = $('button[aria-label="close"]');
+    openButton.click();
+    //test another way to get cell
+    const doorState = await $('table tr td:nth-child(4) [data-testid="door-state"]');
+    expect(doorState.getText('CLOSED'));
   });
 });
