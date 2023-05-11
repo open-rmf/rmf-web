cb047463215b8d71944cca10b7936f5ef49a8137
angatupyry <fierrofenix@gmail.com>
2023-05-11 12:13:31 -0400

    Fix aria label text in create task test
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard-e2e/tests/ui-interactions/create-task-from-any-tab.test.ts
+++ packages/dashboard-e2e/tests/ui-interactions/create-task-from-any-tab.test.ts
@@ -3,7 +3,7 @@ import { getAppBar } from '../utils';
 describe('submit task', () => {
   it('can submit task from robot tab', async () => {
     const appBar = await getAppBar();
-    await (await appBar.$('button[aria-label="Robots"]')).click();
+    await (await appBar.$('button[aria-label="System Overview"]')).click();
     await (await appBar.$('button[aria-label="new task"]')).click();
     await (await $('button=Submit')).click();
     await expect($('div=Successfully created task')).toBeDisplayed();
