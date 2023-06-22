ca0e27ecfb90f8dbf6b5cc4e9d948ca961a20835
César Rolón <37310205+Angatupyry@users.noreply.github.com>
2023-06-22 06:16:59 +0000

    Changes in the robot table (#722)
    * Add level in robot table interface
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Display current level in robot table
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Unused import
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Add style to status column and remove unused class
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Add function to convert Status2 to upperString
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Rename function name to be more precise
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Adjust design according to figma
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    ---------
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/robots/robot-table-datagrid.tsx
+++ packages/react-components/lib/robots/robot-table-datagrid.tsx
@@ -7,39 +7,11 @@ import {
   GridRowParams,
   GridCellParams,
 } from '@mui/x-data-grid';
-import { styled } from '@mui/material';
+import { Box, SxProps, Typography, useTheme } from '@mui/material';
 import * as React from 'react';
 import { Status2 } from 'api-client';
 import { RobotTableData } from './robot-table';
-
-const classes = {
-  robotErrorCell: 'MuiDataGrid-cell-error-cell',
-  robotChargingCell: 'MuiDataGrid-cell-charging-cell',
-  robotWorkingCell: 'MuiDataGrid-cell-working-cell',
-  robotIdleCell: 'MuiDataGrid-cell-idle-cell',
-  robotOfflineCell: 'MuiDataGrid-cell-offline-cell',
-  robotShutdownCell: 'MuiDataGrid-cell-shutdown-cell',
-  robotDefaultCell: 'MuiDataGrid-cell-defautl-cell',
-};
-
-const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
-  [`& .${classes.robotErrorCell}`]: {
-    backgroundColor: theme.palette.error.main,
-    color: theme.palette.getContrastText(theme.palette.success.light),
-  },
-  [`& .${classes.robotChargingCell}`]: {
-    backgroundColor: theme.palette.info.main,
-    color: theme.palette.getContrastText(theme.palette.grey[500]),
-  },
-  [`& .${classes.robotWorkingCell}`]: {
-    backgroundColor: theme.palette.success.main,
-    color: theme.palette.getContrastText(theme.palette.info.light),
-  },
-  [`& .${classes.robotDefaultCell}`]: {
-    backgroundColor: theme.palette.warning.main,
-    color: theme.palette.getContrastText(theme.palette.warning.main),
-  },
-}));
+import { robotStatusToUpperCase } from './utils';
 
 export interface RobotDataGridTableProps {
   onRobotClick?(ev: MuiEvent<React.MouseEvent<HTMLElement>>, robotName: RobotTableData): void;
@@ -56,21 +28,65 @@ export function RobotDataGridTable({ onRobotClick, robots }: RobotDataGridTableP
     }
   };
 
+  const Status = (params: GridCellParams): React.ReactNode => {
+    const theme = useTheme();
+    const statusLabelStyle: SxProps = (() => {
+      const error = {
+        color: theme.palette.error.main,
+      };
+      const charging = {
+        color: theme.palette.info.main,
+      };
+      const working = {
+        color: theme.palette.success.main,
+      };
+      const defaultColor = {
+        color: theme.palette.warning.main,
+      };
+
+      switch (params.row.status) {
+        case Status2.Error:
+          return error;
+        case Status2.Charging:
+          return charging;
+        case Status2.Working:
+          return working;
+        default:
+          return defaultColor;
+      }
+    })();
+
+    return (
+      <Box sx={statusLabelStyle}>
+        <Typography
+          data-testid="status"
+          component="p"
+          sx={{
+            fontWeight: 'bold',
+            fontSize: 14,
+          }}
+        >
+          {robotStatusToUpperCase(params.row.status)}
+        </Typography>
+      </Box>
+    );
+  };
+
   const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 150,
+      editable: false,
+      valueGetter: (params: GridValueGetterParams) => params.row.name,
+      flex: 1,
+      filterable: true,
+    },
     {
       field: 'fleet',
       headerName: 'Fleet',
       width: 90,
       valueGetter: (params: GridValueGetterParams) => params.row.fleet,
-      flex: 1,
-      filterable: true,
-    },
-    {
-      field: 'name',
-      headerName: 'Robot Name',
-      width: 150,
-      editable: false,
-      valueGetter: (params: GridValueGetterParams) => params.row.name,
       flex: 1,
       filterable: true,
     },
@@ -85,11 +101,20 @@ export function RobotDataGridTable({ onRobotClick, robots }: RobotDataGridTableP
       filterable: true,
     },
     {
+      field: 'level',
+      headerName: 'Level',
+      width: 150,
+      editable: false,
+      valueGetter: (params: GridValueGetterParams) => params.row.level,
+      flex: 1,
+      filterable: true,
+    },
+    {
       field: 'battery',
       headerName: 'Battery',
       width: 150,
       editable: false,
-      valueGetter: (params: GridValueGetterParams) => (params.row.battery * 100).toFixed(2),
+      valueGetter: (params: GridValueGetterParams) => `${params.row.battery * 100}%`,
       flex: 1,
       filterable: true,
     },
@@ -108,42 +133,21 @@ export function RobotDataGridTable({ onRobotClick, robots }: RobotDataGridTableP
       headerName: 'Status',
       editable: false,
       flex: 1,
+      renderCell: Status,
       filterable: true,
     },
   ];
 
   return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <StyledDataGrid
-        autoHeight={true}
-        getRowId={(r) => r.name}
-        rows={robots}
-        pageSize={5}
-        rowHeight={38}
-        columns={columns}
-        rowsPerPageOptions={[5]}
-        onRowClick={handleEvent}
-        getCellClassName={(params: GridCellParams<string>) => {
-          if (params.field === 'status') {
-            switch (params.value) {
-              case Status2.Error:
-                return classes.robotErrorCell;
-              case Status2.Charging:
-                return classes.robotChargingCell;
-              case Status2.Working:
-                return classes.robotWorkingCell;
-              case Status2.Idle:
-              case Status2.Offline:
-              case Status2.Shutdown:
-              case Status2.Uninitialized:
-                return classes.robotDefaultCell;
-              default:
-                return classes.robotDefaultCell;
-            }
-          }
-          return '';
-        }}
-      />
-    </div>
+    <DataGrid
+      autoHeight={true}
+      getRowId={(r) => r.name}
+      rows={robots}
+      pageSize={5}
+      rowHeight={38}
+      columns={columns}
+      rowsPerPageOptions={[5]}
+      onRowClick={handleEvent}
+    />
   );
 }
