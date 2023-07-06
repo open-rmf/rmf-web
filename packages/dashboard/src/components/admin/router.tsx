e039d1440e98018e9659b2009ed23d7b3f879b41
angatupyry <fierrofenix@gmail.com>
2023-07-06 12:15:34 -0400

    Adding nested routes
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/admin/router.tsx
+++ packages/dashboard/src/components/admin/router.tsx
@@ -1,27 +1,20 @@
-import { Route, Routes, Link, Navigate, useMatch } from 'react-router-dom';
+import { Route, Routes, Link, Navigate, useMatch, Outlet } from 'react-router-dom';
 import { AdminDrawer } from './drawer';
 import { RoleListPage } from './role-list-page';
 import { UserListPage } from './user-list-page';
 import { UserProfilePage } from './user-profile-page';
 
 export function AdminRouter(): JSX.Element {
-  const match = useMatch('/admin');
-  console.log(match);
-
   return (
     <>
-      <Routes>
-        <Route path="/admin/*" element={<Navigate to="/admin" />} />
-        {/* <Route path={':user'} element={<UserProfilePage />} /> */}
-        <Route path={'/admin/users'} element={<UserListPage />} />
-        {/* <Route path={'roles'}>
-          <RoleListPage />
-        </Route>
-        <Route>
-          <Link to={"users"} />
-        </Route> */}
-      </Routes>
       <AdminDrawer />
+      <Routes>
+        <Route index element={<UserProfilePage />} />
+        <Route path={':user'} element={<UserProfilePage />} />
+        <Route path={'users'} element={<UserListPage />} />
+        <Route path={'roles'} element={<RoleListPage />} />
+        <Route element={<Navigate to={'users'} />} />
+      </Routes>
     </>
   );
 }
