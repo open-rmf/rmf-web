06385b8fa3cfecc59ca0e0903d1aa65937437d74
angatupyry <fierrofenix@gmail.com>
2023-07-11 16:50:29 -0400

    Add describe to wrap testing and use fire event instead of userevent
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/reports/fleet-state/fleet-state-report.spec.tsx
+++ packages/react-components/lib/reports/fleet-state/fleet-state-report.spec.tsx
@@ -1,36 +1,37 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import React from 'react';
 import { TestLocalizationProvider } from '../../test/locale';
 import { getFleetLogs, reportConfigProps } from '../utils.spec';
 import { FleetStateReport } from './fleet-state-report';
 
-const getLogsPromise = async () => getFleetLogs();
+describe('Fleet State report', () => {
+  const getLogsPromise = async () => getFleetLogs();
 
-it('smoke test', async () => {
-  await waitFor(() => {
-    render(<FleetStateReport getLogs={getLogsPromise} />);
-  });
-});
-
-it('doesn`t shows the table when logs list is empty', async () => {
-  await waitFor(() => {
-    render(<FleetStateReport getLogs={async () => []} />);
+  it('smoke test', async () => {
+    await waitFor(() => {
+      render(<FleetStateReport getLogs={getLogsPromise} />);
+    });
   });
 
-  expect(screen.queryByText('Fleet State')).toBeFalsy();
-});
+  it('doesn`t shows the table when logs list is empty', async () => {
+    await waitFor(() => {
+      render(<FleetStateReport getLogs={async () => []} />);
+    });
 
-it('calls the retrieve log function when the button is clicked', async () => {
-  const getLogsPromiseMock = jasmine.createSpy();
-  const getLogsPromise = async () => {
-    getLogsPromiseMock();
-    return getFleetLogs();
-  };
-  render(<FleetStateReport getLogs={getLogsPromise} {...reportConfigProps} />, {
-    wrapper: TestLocalizationProvider,
+    expect(screen.queryByText('Fleet State')).toBeFalsy();
   });
-  expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
-  userEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
-  expect(getLogsPromiseMock).toHaveBeenCalled();
+
+  it('calls the retrieve log function when the button is clicked', async () => {
+    const getLogsPromiseMock = jasmine.createSpy();
+    const getLogsPromise = async () => {
+      getLogsPromiseMock();
+      return getFleetLogs();
+    };
+    render(<FleetStateReport getLogs={getLogsPromise} {...reportConfigProps} />, {
+      wrapper: TestLocalizationProvider,
+    });
+    expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
+    fireEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
+    expect(getLogsPromiseMock).toHaveBeenCalled();
+  });
 });
