81098c3f47b0b436beccbf64fa2d00304a9103d5
angatupyry <fierrofenix@gmail.com>
2023-07-11 18:00:29 -0400

    Wrap test into describe
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/reports/health/health-report.spec.tsx
+++ packages/react-components/lib/reports/health/health-report.spec.tsx
@@ -1,36 +1,37 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import React from 'react';
 import { TestLocalizationProvider } from '../../test/locale';
 import { getHealthLogs, reportConfigProps } from '../utils.spec';
 import { HealthReport } from './health-report';
 
-const getLogsPromise = async () => getHealthLogs();
+describe('Health State report', () => {
+  const getLogsPromise = async () => getHealthLogs();
 
-it('smoke test', async () => {
-  await waitFor(() => {
-    render(<HealthReport getLogs={getLogsPromise} />);
-  });
-});
-
-it('doesn`t shows the table when logs list is empty', async () => {
-  await waitFor(() => {
-    render(<HealthReport getLogs={async () => await []} />);
+  it('smoke test', async () => {
+    await waitFor(() => {
+      render(<HealthReport getLogs={getLogsPromise} />);
+    });
   });
 
-  expect(screen.queryByText('Health')).toBeFalsy();
-});
+  it('doesn`t shows the table when logs list is empty', async () => {
+    await waitFor(() => {
+      render(<HealthReport getLogs={async () => await []} />);
+    });
 
-it('calls the retrieve log function when the button is clicked', async () => {
-  const getLogsPromiseMock = jasmine.createSpy();
-  const getLogsPromise = async () => {
-    getLogsPromiseMock();
-    return getHealthLogs();
-  };
-  render(<HealthReport getLogs={getLogsPromise} {...reportConfigProps} />, {
-    wrapper: TestLocalizationProvider,
+    expect(screen.queryByText('Health')).toBeFalsy();
   });
-  expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
-  userEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
-  expect(getLogsPromiseMock).toHaveBeenCalled();
+
+  it('calls the retrieve log function when the button is clicked', async () => {
+    const getLogsPromiseMock = jasmine.createSpy();
+    const getLogsPromise = async () => {
+      getLogsPromiseMock();
+      return getHealthLogs();
+    };
+    render(<HealthReport getLogs={getLogsPromise} {...reportConfigProps} />, {
+      wrapper: TestLocalizationProvider,
+    });
+    expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
+    fireEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
+    expect(getLogsPromiseMock).toHaveBeenCalled();
+  });
 });
--- packages/react-components/lib/reports/lift-state/lift-state-report.spec.tsx
+++ packages/react-components/lib/reports/lift-state/lift-state-report.spec.tsx
@@ -1,36 +1,46 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import React from 'react';
 import { TestLocalizationProvider } from '../../test/locale';
 import { getLiftLogs, reportConfigProps } from '../utils.spec';
 import { LiftStateReport } from './lift-state-report';
 
-const getLogsPromise = async () => getLiftLogs();
+describe('Lift State report', () => {
+  const getLogsPromise = async () => getLiftLogs();
 
-it('smoke test', async () => {
-  await waitFor(() => {
-    render(<LiftStateReport getLogs={getLogsPromise} />);
+  it('smoke test', async () => {
+    act(() => {
+      render(<LiftStateReport getLogs={getLogsPromise} />);
+    });
+    await waitFor(() => {
+      render(<LiftStateReport getLogs={getLogsPromise} />);
+    });
   });
-});
 
-it('doesn`t shows the table when logs list is empty', async () => {
-  await waitFor(() => {
-    render(<LiftStateReport getLogs={async () => []} />);
+  it('doesn`t shows the table when logs list is empty', async () => {
+    act(() => {
+      render(<LiftStateReport getLogs={async () => []} />);
+    });
+    await waitFor(() => {
+      render(<LiftStateReport getLogs={async () => []} />);
+    });
+
+    expect(screen.queryByText('Lift State')).toBeFalsy();
   });
 
-  expect(screen.queryByText('Lift State')).toBeFalsy();
-});
-
-it('calls the retrieve log function when the button is clicked', async () => {
-  const getLogsPromiseMock = jasmine.createSpy();
-  const getLogsPromise = async () => {
-    getLogsPromiseMock();
-    return getLiftLogs();
-  };
-  render(<LiftStateReport getLogs={getLogsPromise} {...reportConfigProps} />, {
-    wrapper: TestLocalizationProvider,
+  it('calls the retrieve log function when the button is clicked', async () => {
+    const getLogsPromiseMock = jasmine.createSpy();
+    const getLogsPromise = async () => {
+      getLogsPromiseMock();
+      return getLiftLogs();
+    };
+    await act(async () => {
+      render(<LiftStateReport getLogs={getLogsPromise} {...reportConfigProps} />, {
+        wrapper: TestLocalizationProvider,
+      });
+    });
+    expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
+    fireEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
+    expect(getLogsPromiseMock).toHaveBeenCalled();
   });
-  expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
-  userEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
-  expect(getLogsPromiseMock).toHaveBeenCalled();
 });
