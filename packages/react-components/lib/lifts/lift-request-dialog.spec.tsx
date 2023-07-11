acc67d67735dfe785db31d5b9374d41d9664f271
angatupyry <fierrofenix@gmail.com>
2023-07-11 18:45:21 -0400

    Remove unnecessary function to render component
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/lifts/lift-request-dialog.spec.tsx
+++ packages/react-components/lib/lifts/lift-request-dialog.spec.tsx
@@ -1,34 +1,16 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { LiftRequestDialog } from './lift-request-dialog';
 import { requestDoorModes, requestModes } from './lift-utils';
 import { makeLift, makeLiftState } from './test-utils.spec';
 
-function renderLiftRequestForm() {
-  const mockOnClose = jasmine.createSpy();
-  const lift = makeLift();
-  const liftState = makeLiftState();
-  return render(
-    <LiftRequestDialog
-      currentLevel={liftState.current_floor}
-      availableLevels={lift.levels}
-      availableRequestTypes={requestModes}
-      availableDoorModes={requestDoorModes}
-      showFormDialog={true}
-      onClose={mockOnClose}
-    />,
-  );
-}
-
 describe('Lift request form', () => {
-  let root: ReturnType<typeof renderLiftRequestForm>;
-
-  beforeEach(() => {
+  it('destination is required', () => {
     const mockOnClose = jasmine.createSpy();
     const lift = makeLift();
     const liftState = makeLiftState();
-    root = render(
+    render(
       <LiftRequestDialog
         currentLevel={liftState.current_floor}
         availableLevels={lift.levels}
@@ -38,11 +20,8 @@ describe('Lift request form', () => {
         onClose={mockOnClose}
       />,
     );
-  });
-
-  it('destination is required', () => {
-    userEvent.type(root.getByPlaceholderText('Pick a Destination'), '{selectall}{backspace}');
-    userEvent.click(root.getByText('Request'));
-    expect(root.getByText('Destination cannot be empty')).toBeTruthy();
+    userEvent.type(screen.getByPlaceholderText('Pick a Destination'), '{selectall}{backspace}');
+    fireEvent.click(screen.getByText('Request'));
+    expect(screen.getByText('Destination cannot be empty')).toBeTruthy();
   });
 });
