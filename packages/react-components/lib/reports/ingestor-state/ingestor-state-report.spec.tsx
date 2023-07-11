c1dfce398099d02fc59e29cda17d9f65c11c17ae
angatupyry <fierrofenix@gmail.com>
2023-07-11 17:57:01 -0400

    Wrap test into describe
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/reports/ingestor-state/ingestor-state-report.spec.tsx
+++ packages/react-components/lib/reports/ingestor-state/ingestor-state-report.spec.tsx
@@ -1,36 +1,37 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import React from 'react';
 import { TestLocalizationProvider } from '../../test/locale';
 import { getIngestorLogs, reportConfigProps } from '../utils.spec';
 import { IngestorStateReport } from './ingestor-state-report';
 
-const getLogsPromise = async () => getIngestorLogs();
+describe('Ingestor State report', () => {
+  const getLogsPromise = async () => getIngestorLogs();
 
-it('smoke test', async () => {
-  await waitFor(() => {
-    render(<IngestorStateReport getLogs={getLogsPromise} />);
-  });
-});
-
-it('doesn`t shows the table when logs list is empty', async () => {
-  await waitFor(() => {
-    render(<IngestorStateReport getLogs={async () => []} />);
+  it('smoke test', async () => {
+    await waitFor(() => {
+      render(<IngestorStateReport getLogs={getLogsPromise} />);
+    });
   });
 
-  expect(screen.queryByText('Ingestor State')).toBeFalsy();
-});
+  it('doesn`t shows the table when logs list is empty', async () => {
+    await waitFor(() => {
+      render(<IngestorStateReport getLogs={async () => []} />);
+    });
 
-it('calls the retrieve log function when the button is clicked', async () => {
-  const getLogsPromiseMock = jasmine.createSpy();
-  const getLogsPromise = async () => {
-    getLogsPromiseMock();
-    return getIngestorLogs();
-  };
-  render(<IngestorStateReport getLogs={getLogsPromise} {...reportConfigProps} />, {
-    wrapper: TestLocalizationProvider,
+    expect(screen.queryByText('Ingestor State')).toBeFalsy();
   });
-  expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
-  userEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
-  expect(getLogsPromiseMock).toHaveBeenCalled();
+
+  it('calls the retrieve log function when the button is clicked', async () => {
+    const getLogsPromiseMock = jasmine.createSpy();
+    const getLogsPromise = async () => {
+      getLogsPromiseMock();
+      return getIngestorLogs();
+    };
+    render(<IngestorStateReport getLogs={getLogsPromise} {...reportConfigProps} />, {
+      wrapper: TestLocalizationProvider,
+    });
+    expect(screen.getByRole('button', { name: /Retrieve Logs/i })).toBeTruthy();
+    fireEvent.click(screen.getByRole('button', { name: /Retrieve Logs/i }));
+    expect(getLogsPromiseMock).toHaveBeenCalled();
+  });
 });
