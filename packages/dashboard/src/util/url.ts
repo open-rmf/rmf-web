084900cf3240e8f6351f5c42165995cc34c1a37b
angatupyry <fierrofenix@gmail.com>
2023-07-13 15:10:07 -0400

    Add comment to explain change
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/util/url.ts
+++ packages/dashboard/src/util/url.ts
@@ -2,6 +2,14 @@ export const BasePath =
   process.env.PUBLIC_URL === undefined || process.env.PUBLIC_URL === '/'
     ? ''
     : process.env.PUBLIC_URL;
+/**
+ * Add /* after /admin to avoid the warning
+ * <Routes> (or called `useRoutes()`) at "/admin"
+ *  (under <Route path="/admin">)
+ *  but the parent route path has no trailing "*".
+ * This means if you navigate deeper,
+ * the parent won't match anymore and therefore the child routes will never render.
+ */
 export const DashboardRoute = BasePath === '' ? '/' : BasePath;
 export const LoginRoute = `${BasePath}/login`;
 export const TasksRoute = `${BasePath}/tasks`;
