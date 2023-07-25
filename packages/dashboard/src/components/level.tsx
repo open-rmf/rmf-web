e96068838c8751ce49bf25c318bb6bdd8b99c385
angatupyry <fierrofenix@gmail.com>
2023-07-25 11:50:34 -0400

    Create Boundingbox per each level
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/level.tsx
+++ packages/dashboard/src/components/level.tsx
@@ -1,18 +1,11 @@
 import * as THREE from 'three';
 import React, { Suspense } from 'react';
 import { MeshProps } from '@react-three/fiber';
-import { Graph, GraphNode, Level } from 'api-client';
-import { Wall } from './wall';
+import { Level } from 'api-client';
+import { graphToWalls, Wall } from './wall';
 import { Door } from './door';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import {
-  PerspectiveCamera,
-  PositionalAudio,
-  OrbitControls,
-  Environment,
-  Stats,
-  Stage,
-} from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 export interface LevelProps {
   levels: Level[];
@@ -21,20 +14,6 @@ export interface LevelProps {
 export interface BuildingMapProps {
   level: Level;
   show: boolean;
-}
-
-interface BoundingBoxProps {
-  walls: Graph;
-  opacity: number;
-  elevation: number;
-}
-
-interface WallSegment {
-  position: number[];
-  width: number;
-  height: number;
-  depth: number;
-  rot: THREE.Euler;
 }
 
 function Animate({ controls, lerping, to, target }: any) {
@@ -51,112 +30,6 @@ function Animate({ controls, lerping, to, target }: any) {
   return null;
 }
 
-const distance = (v1: GraphNode, v2: GraphNode) => Math.hypot(v2.x - v1.x, v2.y - v1.y);
-const midPoint = (v1: GraphNode, v2: GraphNode) => [(v2.x + v1.x) / 2, (v2.y + v1.y) / 2];
-
-const graphToWalls = (graph: Graph, elevation: number) => {
-  const walls = [] as WallSegment[];
-  const { edges, vertices } = graph;
-  let minX = Infinity;
-  let minY = Infinity;
-  let minZ = Infinity;
-  let maxX = -Infinity;
-  let maxY = -Infinity;
-  let maxZ = -Infinity;
-
-  edges.map((edge) => {
-    const v1 = vertices[edge.v1_idx];
-    const v2 = vertices[edge.v2_idx];
-
-    const depth = distance(v1, v2);
-    const midpoint = midPoint(v1, v2);
-    const width = 0.3;
-    const height = 8;
-    const position = midpoint.concat(height / 2 + elevation);
-
-    const angle = Math.atan2(v1.y - v2.y, v1.x - v2.x) - Math.PI / 2;
-    const rot = new THREE.Euler(0, 0, angle);
-
-    walls.push({
-      position,
-      width,
-      height,
-      depth,
-      rot,
-    });
-    minX = Math.min(minX, v1.x, v2.x);
-    minY = Math.min(minY, v1.y, v2.y);
-    minZ = Math.min(minZ, position[2] - height / 2);
-    maxX = Math.max(maxX, v1.x, v2.x);
-    maxY = Math.max(maxY, v1.y, v2.y);
-    maxZ = Math.max(maxZ, position[2] + height / 2);
-  });
-
-  const boundingBox = new THREE.Box3(
-    new THREE.Vector3(minX, minY, minZ),
-    new THREE.Vector3(maxX, maxY, maxZ),
-  );
-
-  return { walls, boundingBox };
-};
-
-const BoundingBox = ({ walls, elevation, opacity }: BoundingBoxProps) => {
-  const walls_ = graphToWalls(walls, elevation);
-  const { camera } = useThree();
-  const boundingBoxRef = React.useRef<THREE.Box3>();
-  const controlsRef = React.useRef<any>();
-
-  React.useEffect(() => {
-    if (walls_.boundingBox !== boundingBoxRef.current) {
-      boundingBoxRef.current = walls_.boundingBox;
-    }
-  }, [walls_]);
-
-  useFrame(() => {
-    if (boundingBoxRef.current) {
-      const center = boundingBoxRef.current.getCenter(new THREE.Vector3());
-      const size = boundingBoxRef.current.getSize(new THREE.Vector3());
-      const maxDim = Math.max(size.x, size.y, size.z);
-      const distance = Math.max(size.x, size.y, size.z) * 0.9;
-
-      // Assuming you have a PerspectiveCamera called 'camera'
-      camera.position.set(center.x, center.y, center.z + distance);
-      camera.lookAt(center);
-      // Calculate the zoom level based on the bounding box size
-      const fov =
-        camera instanceof THREE.PerspectiveCamera
-          ? camera.fov * (Math.PI / 180)
-          : 45 * (Math.PI / 180);
-      // const distance = Math.abs(maxDim / (2 * Math.tan(fov / 2)));
-
-      // Calculate the target position for the camera
-      // const target = center.clone().add(camera.position.clone().sub(center).normalize().multiplyScalar(distance));
-      // camera.position.copy(target);
-      // camera.lookAt(center);
-    }
-  });
-
-  // useFrame(() => {
-  //   if (boundingBoxRef.current) {
-  //     const center = boundingBoxRef.current.getCenter(new THREE.Vector3());
-  //     camera.lookAt(center);
-  //   }
-  // });
-  // useFrame(({ camera }) => {
-  //   if (controlsRef.current) {
-  //     controlsRef.current.target = camera.position;
-  //   }
-  // });
-
-  return null;
-
-  // return (
-  //   <>
-  //     <OrbitControls ref={controlsRef} />
-  //   </>
-  // );
-};
-
 const BuildingMap = ({ level, show }: BuildingMapProps, props: MeshProps) => {
   const { doors, elevation } = level;
 
@@ -164,7 +37,6 @@ const BuildingMap = ({ level, show }: BuildingMapProps, props: MeshProps) => {
     <>
       {level.wall_graph.edges.length > 0 ? (
         <>
-          <BoundingBox elevation={elevation} opacity={Number(show)} walls={level.wall_graph} />
           <Wall elevation={elevation} opacity={Number(show)} walls={level.wall_graph} />
         </>
       ) : null}
@@ -177,6 +49,33 @@ const BuildingMap = ({ level, show }: BuildingMapProps, props: MeshProps) => {
   );
 };
 
+const findSceneBoundingBox = (levels: Level[]) => {
+  let minX = Infinity;
+  let minY = Infinity;
+  let minZ = Infinity;
+  let maxX = -Infinity;
+  let maxY = -Infinity;
+  let maxZ = -Infinity;
+
+  levels.forEach((level) => {
+    const walls = graphToWalls(level.wall_graph, level.elevation);
+    walls.forEach((wall) => {
+      const [x, y, z] = wall.position;
+      const width = wall.width;
+      const height = wall.height;
+
+      minX = Math.min(minX, x - width / 2);
+      minY = Math.min(minY, y - width / 2);
+      minZ = Math.min(minZ, z - height / 2);
+      maxX = Math.max(maxX, x + width / 2);
+      maxY = Math.max(maxY, y + width / 2);
+      maxZ = Math.max(maxZ, z + height / 2);
+    });
+  });
+
+  return new THREE.Box3(new THREE.Vector3(minX, minY, minZ), new THREE.Vector3(maxX, maxY, maxZ));
+};
+
 export function MapConstruction({ levels }: LevelProps) {
   const ref = React.useRef(null);
   const [lerping, setLerping] = React.useState(false);
@@ -184,22 +83,32 @@ export function MapConstruction({ levels }: LevelProps) {
   const [target, setTarget] = React.useState();
   const [selected, setSelected] = React.useState(-1);
 
+  const sceneBoundingBox = React.useMemo(() => findSceneBoundingBox(levels), [levels]);
+  const size = sceneBoundingBox.getSize(new THREE.Vector3());
+  const distance = Math.max(size.x, size.y, size.z) * 0.9;
+
   return (
     <Suspense fallback={null}>
       <Canvas
         onPointerDown={() => setLerping(false)}
         onWheel={() => setLerping(false)}
-        camera={{ position: [12, 1, 22] }}
         onCreated={({ camera }) => {
-          camera.zoom = 1;
+          const center = sceneBoundingBox.getCenter(new THREE.Vector3());
+          camera.position.set(center.x, center.y, center.z + distance);
           camera.updateProjectionMatrix();
         }}
       >
-        <OrbitControls ref={ref} enableZoom enablePan enableRotate />
+        <OrbitControls
+          ref={ref}
+          enableZoom
+          enablePan
+          enableRotate
+          target={sceneBoundingBox.getCenter(new THREE.Vector3())}
+          maxDistance={distance}
+        />
         {levels.map((level, i) => (
           <BuildingMap key={i} level={level} show={true} />
         ))}
-        <Animate controls={ref} lerping={lerping} to={to} target={target} />
         <ambientLight />
       </Canvas>
     </Suspense>
