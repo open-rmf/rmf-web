e6a28b7744f853abfeae1fe2bb73d0b1b18a0ca8
Angatupyry <fierrofenix@gmail.com>
2023-07-27 15:37:54 -0400

    Create plane robot to put it into map
    Signed-off-by: Angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/robot-loader.tsx
+++ packages/dashboard/src/components/robot-loader.tsx
@@ -1,34 +1,27 @@
 import { RobotData } from './robots-overlay';
-import * as THREE from 'three';
+import React from 'react';
+import { Sphere, Plane } from '@react-three/drei';
 
 interface RoboShapeProps {
   robots: RobotData[];
   robotLocations: Record<string, [number, number]>;
 }
 
-function Shape({ shape, rotation, position, color, opacity, index }: any) {
+function PlaneRobot({ shape, rotation, position, color, opacity, index }: any) {
   if (!position) return null;
   const v1 = position[0];
   const v2 = position[1];
   const elevation = 1;
   const height = 8;
-  const newPosition = [(v2 + v1) / 2, (v2 + v1) / 2].concat(height / 2 + elevation);
+  const newPosition = [(v2 + v1) / 2, (v2 + v1) / 2, height / 2 + elevation];
 
   return (
-    // <mesh rotation={rotation} position={[position[0], position[1], newPosition[2]]}>
-    //     <meshStandardMaterial color={color} opacity={opacity} />
-    //     <shapeGeometry args={[shape]} />
-    // </mesh>
-    <mesh
-      position={[position[0], position[1], newPosition[2]]}
-      rotation={rotation}
-      // scale={[size[0], size[1], size[2]]}
-      ref={null}
-    >
-      <planeGeometry />
-      {/* <boxGeometry /> */}
-      <meshStandardMaterial color={color || 'blue'} opacity={0.7} transparent />
-    </mesh>
+    <group position={[position[0], position[1], newPosition[2]]} rotation={rotation}>
+      <Plane scale={[2, 0.5, 1]} position={[0, 0, 0]} material-color={color} />
+      <Plane scale={[1.5, 0.5, 0.5]} position={[-0.25, 0.25, 0.75]} material-color={color} />
+      <Sphere args={[0.3, 32, 32]} position={[-0.6, -0.25, 0.75]} />
+      <Sphere args={[0.3, 32, 32]} position={[0.6, -0.25, 0.75]} />
+    </group>
   );
 }
 
@@ -39,7 +32,7 @@ export function RobotShape({ robots, robotLocations }: RoboShapeProps) {
         const robotId = `${robot.fleet}/${robot.name}`;
         const robotLocation = robotLocations[robotId];
         return (
-          <Shape
+          <PlaneRobot
             key={i}
             color={robot.color}
             opacity={1}
