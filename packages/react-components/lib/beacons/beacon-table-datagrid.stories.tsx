c76af16b70a8588379985e7e1b58927e3b844647
Aaron Chong <aaronchongth@gmail.com>
2023-08-08 09:36:50 +0000

    Added more entries to storybook
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/beacons/beacon-table-datagrid.stories.tsx
+++ packages/react-components/lib/beacons/beacon-table-datagrid.stories.tsx
@@ -4,5 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { makeBeaconState } from './test-utils.spec';
 
 storiesOf('BeaconDataGridTable', module)
-  .add('Default', () => <BeaconDataGridTable beacons={[makeBeaconState()]} />)
+  .add('Default', () => (
+    <BeaconDataGridTable
+      beacons={[
+        makeBeaconState({ id: 'test_beacon1' }),
+        makeBeaconState({ id: 'test_beacon2', activated: true }),
+        makeBeaconState({ id: 'test_beacon3', online: false }),
+      ]}
+    />
+  ))
   .add('Empty', () => <BeaconDataGridTable beacons={[]} />);
