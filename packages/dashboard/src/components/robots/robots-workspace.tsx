fc52060d7050756d9533348d496a3a50dada14f1
Aaron Chong <aaronchongth@gmail.com>
2023-08-21 04:30:15 +0000

    Add beacon microapp
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard/src/components/robots/robots-workspace.tsx
+++ packages/dashboard/src/components/robots/robots-workspace.tsx
@@ -4,13 +4,15 @@ export const robotsWorkspace: WorkspaceState = {
   layout: [
     { i: 'robots', x: 0, y: 0, w: 7, h: 5 },
     { i: 'map', x: 8, y: 0, w: 5, h: 12 },
-    { i: 'doors', x: 0, y: 0, w: 7, h: 4 },
-    { i: 'lifts', x: 0, y: 0, w: 7, h: 3 },
+    { i: 'doors', x: 0, y: 0, w: 7, h: 3 },
+    { i: 'lifts', x: 0, y: 0, w: 7, h: 2 },
+    { i: 'beacons', x: 0, y: 0, w: 7, h: 2 },
   ],
   windows: [
     { key: 'robots', appName: 'Robots' },
     { key: 'map', appName: 'Map' },
     { key: 'doors', appName: 'Doors' },
     { key: 'lifts', appName: 'Lifts' },
+    { key: 'beacons', appName: 'Beacons' },
   ],
 };
