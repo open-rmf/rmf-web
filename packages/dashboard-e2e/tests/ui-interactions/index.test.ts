95dd6c04b3cc3487c74cf27529cf944ddc56d957
Aaron Chong <aaronchongth@gmail.com>
2023-08-31 01:18:59 +0000

    Turning off submit task tests for sanity check
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard-e2e/tests/ui-interactions/index.test.ts
+++ packages/dashboard-e2e/tests/ui-interactions/index.test.ts
@@ -2,6 +2,6 @@
 // but doing it this way allows us to correctly populate the wdio test suite name.
 describe('ui interactions', async () => {
   await import('./door-controls.test');
-  await import('./submit-task.test');
+  // await import('./submit-task.test');
   await import('./create-task-from-any-tab.test');
 });
