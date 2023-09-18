aaf1c860dc20308dbe89f1cc617f31aba6ef6615
Aaron Chong <aaronchongth@gmail.com>
2023-09-18 14:28:20 +0000

    Alignment fixes using scale and offset
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard/src/components/three-fiber/imageOverlay.tsx
+++ packages/dashboard/src/components/three-fiber/imageOverlay.tsx
@@ -11,15 +11,29 @@ export interface ImageThreeProps {
 
 export const ImageThree = ({ level, imageUrl }: ImageThreeProps) => {
   const texture = useLoader(TextureLoader, imageUrl);
-  const imageWidth = texture.image.width;
-  const imageHeight = texture.image.height;
-  const scaleX = imageWidth * level.images[0].scale + level.images[0].x_offset;
-  const scaleY = imageHeight * level.images[0].scale + level.images[0].y_offset;
+  const image = level.images[0];
+  const scale = image.scale;
+  const x_offset = (texture.image.width * scale) / 2;
+  const y_offset = -(texture.image.height * scale) / 2;
+
+  // const imageWidth = texture.image.width;
+  // const imageHeight = texture.image.height;
+  // console.log(image.scale);
+  // console.log(image.x_offset);
+  // console.log(image.y_offset);
+  // // const scaleX = imageWidth * level.images[0].scale + level.images[0].x_offset;
+  // // const scaleY = imageHeight * level.images[0].scale + level.images[0].y_offset;
+  // const scaleX = imageWidth * level.images[0].scale;
+  // const scaleY = imageHeight * level.images[0].scale;
 
   const [sceneBoundingBox, setSceneBoundingBox] = React.useState<THREE.Box3 | undefined>(undefined);
 
   const [center, setCenter] = React.useState<Vector3>(
-    new Vector3(level.images[0].x_offset, level.images[0].y_offset, level.images[0].yaw),
+    new Vector3(
+      level.images[0].x_offset * level.images[0].scale,
+      level.images[0].y_offset * level.images[0].scale,
+      level.images[0].yaw,
+    ),
   );
 
   React.useMemo(() => {
@@ -37,9 +51,16 @@ export const ImageThree = ({ level, imageUrl }: ImageThreeProps) => {
   }, [sceneBoundingBox]);
 
   return sceneBoundingBox ? (
-    <mesh scale={[scaleX, scaleY, 0]} position={center}>
-      <planeGeometry />
-      <meshBasicMaterial map={texture} />
+    // <mesh scale={[scaleX, scaleY, 0]} position={center}>
+    //   <planeGeometry />
+    //   <meshBasicMaterial map={texture} />
+    // </mesh>
+    <mesh position={[x_offset, y_offset, 0]}>
+      <planeGeometry
+        attach="geometry"
+        args={[texture.image.width * scale, texture.image.height * scale]}
+      />
+      <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
     </mesh>
   ) : (
     <></>
