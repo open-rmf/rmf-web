6b6c7c262dfe718e0a31169ff285198c9d9a1858
Aaron Chong <aaronchongth@gmail.com>
2023-09-22 05:30:26 +0000

    Splitting static from run
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-server/sqlite_local_config.py
+++ packages/api-server/sqlite_local_config.py
@@ -8,7 +8,7 @@ run_dir = f"{here}/run"
 config.update(
     {
         "db_url": f"sqlite://{run_dir}/db.sqlite3",
-        "static_directory": f"{run_dir}/static",  # The directory where static files should be stored.
+        "static_directory": "static",  # The directory where static files should be stored.
         "ros_args": ["-p", "use_sim_time:=true"],
     }
 )
