c44a7317709af3e70691aa6eb34c04b750f3f1ac
angatupyry <fierrofenix@gmail.com>
2023-09-26 16:30:54 -0400

    Hover and zoom text
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/map/three-fiber/shape-three-rendering.tsx
+++ packages/react-components/lib/map/three-fiber/shape-three-rendering.tsx
@@ -1,4 +1,4 @@
-import { Circle, Text } from '@react-three/drei';
+import { Circle, Html } from '@react-three/drei';
 import React from 'react';
 
 interface ShapeThreeRenderingProps {
@@ -17,6 +17,18 @@ export const ShapeThreeRendering = ({
   const HEIGHT = 8;
   const ELEVATION = 0;
   const positionZ = HEIGHT / 2 + ELEVATION;
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handlePointerOver = () => {
+    setIsHovered(true);
+  };
+
+  const handlePointerOut = () => {
+    setIsHovered(false);
+  };
+
+  const scaleFactor = isHovered ? 2 : 1.0;
+
   return (
     <>
       {circleShape ? (
@@ -25,13 +37,31 @@ export const ShapeThreeRendering = ({
         </Circle>
       ) : (
         <group position={position}>
-          {text && (
-            <Text position={[0, 0.7, positionZ]} color="orange" fontSize={0.6}>
-              {text}
-            </Text>
-          )}
-          <mesh position={[0, 0, positionZ]} scale={[0.7, 0.7, 0.7]}>
-            <boxGeometry args={[0.3, 0.3, 0.3]} />
+          <mesh
+            position={[-1, 0, positionZ]}
+            onPointerOver={handlePointerOver}
+            onPointerOut={handlePointerOut}
+          >
+            {text && isHovered && (
+              <Html zIndexRange={[0, 0, 1]}>
+                {text && (
+                  <div
+                    style={{
+                      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                      padding: '0.2rem 0.5rem',
+                      borderRadius: '4px',
+                      fontSize: '0.6rem',
+                      transform: `scale(${scaleFactor})`,
+                      transition: 'transform 0.3s',
+                      zIndex: isHovered ? 0.1 : 6,
+                    }}
+                  >
+                    {text}
+                  </div>
+                )}
+              </Html>
+            )}
+            <boxGeometry args={[0.8, 0.8, 0.8]} />
             <meshStandardMaterial color={color} opacity={1} />
           </mesh>
         </group>
