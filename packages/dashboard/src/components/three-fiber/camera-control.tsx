cc5ef2e18a4e338e715df87d523f2a77c29ec021
angatupyry <fierrofenix@gmail.com>
2023-10-05 11:21:01 -0300

    Add wheel function to get the zoom value
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/three-fiber/camera-control.tsx
+++ packages/dashboard/src/components/three-fiber/camera-control.tsx
@@ -2,6 +2,8 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import React, { useEffect, useRef } from 'react';
 import { MOUSE, Vector3 } from 'three';
+import { AppEvents } from '../app-events';
+import { debounce } from 'react-components';
 
 interface CameraControlProps {
   zoom: number;
@@ -10,6 +12,11 @@ interface CameraControlProps {
 export const CameraControl: React.FC<CameraControlProps> = ({ zoom }) => {
   const { camera, gl } = useThree();
   const controlsRef = useRef<OrbitControls | null>(null);
+  const [internalZoom, setInternalZoom] = React.useState(zoom);
+
+  const updateExternalStateDebounced = debounce((zoomValue: number) => {
+    AppEvents.zoom.next(zoomValue);
+  }, 10);
 
   useEffect(() => {
     const controls = new OrbitControls(camera, gl.domElement);
@@ -27,8 +34,16 @@ export const CameraControl: React.FC<CameraControlProps> = ({ zoom }) => {
     camera.zoom = zoom;
     camera.updateProjectionMatrix();
 
-    const handleWheel = (event: any) => {
-      console.log(event);
+    const handleWheel = (event: WheelEvent) => {
+      const delta = event.deltaY;
+      const sensitivity = 0.1;
+
+      setInternalZoom((prevZoom) => {
+        const newZoom = prevZoom - delta * sensitivity;
+        return Math.max(controls.minZoom, Math.min(controls.maxZoom, newZoom));
+      });
+
+      updateExternalStateDebounced(internalZoom);
     };
 
     gl.domElement.addEventListener('wheel', handleWheel);
@@ -37,7 +52,7 @@ export const CameraControl: React.FC<CameraControlProps> = ({ zoom }) => {
       controls.dispose();
       gl.domElement.removeEventListener('wheel', handleWheel);
     };
-  }, [camera, gl.domElement, zoom]);
+  }, [camera, gl.domElement, zoom, internalZoom, updateExternalStateDebounced]);
 
   useFrame(() => {
     if (controlsRef.current) {
