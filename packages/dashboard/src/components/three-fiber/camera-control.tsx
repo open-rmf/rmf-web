b32f61c9e9e80785a2657435b23a238ed955efe6
angatupyry <fierrofenix@gmail.com>
2023-10-09 14:19:15 -0300

    Remove zoom implementation
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/dashboard/src/components/three-fiber/camera-control.tsx
+++ packages/dashboard/src/components/three-fiber/camera-control.tsx
@@ -2,8 +2,6 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import React, { useEffect, useRef } from 'react';
 import { MOUSE, Vector3 } from 'three';
-import { AppEvents } from '../app-events';
-import { debounce } from 'react-components';
 
 interface CameraControlProps {
   zoom: number;
@@ -15,11 +13,6 @@ export const updateZoom = (currentZoom: number, increment: number) =>
 export const CameraControl: React.FC<CameraControlProps> = ({ zoom }) => {
   const { camera, gl } = useThree();
   const controlsRef = useRef<OrbitControls | null>(null);
-  const [internalZoom, setInternalZoom] = React.useState(zoom);
-
-  const updateExternalStateDebounced = debounce((zoomValue: number) => {
-    AppEvents.zoom.next(zoomValue);
-  }, 10);
 
   useEffect(() => {
     const controls = new OrbitControls(camera, gl.domElement);
@@ -34,28 +27,31 @@ export const CameraControl: React.FC<CameraControlProps> = ({ zoom }) => {
     };
 
     controlsRef.current = controls;
-    camera.zoom = zoom;
-    camera.updateProjectionMatrix();
 
-    const handleWheel = (event: WheelEvent) => {
-      const delta = event.deltaY;
-      const sensitivity = 0.1;
+    // [CR]: Remove the icons for now. Code is left for future implementation.
 
-      setInternalZoom((prevZoom) => {
-        const newZoom = prevZoom - delta * sensitivity;
-        return Math.max(controls.minZoom, Math.min(controls.maxZoom, newZoom));
-      });
+    // camera.zoom = zoom;
+    // camera.updateProjectionMatrix();
 
-      updateExternalStateDebounced(internalZoom);
-    };
+    // const handleWheel = (event: WheelEvent) => {
+    //   const delta = event.deltaY;
+    //   const sensitivity = 0.1;
 
-    gl.domElement.addEventListener('wheel', handleWheel);
+    //   setInternalZoom((prevZoom) => {
+    //     const newZoom = prevZoom - delta * sensitivity;
+    //     return Math.max(controls.minZoom, Math.min(controls.maxZoom, newZoom));
+    //   });
+
+    //   updateExternalStateDebounced(internalZoom);
+    // };
+
+    // gl.domElement.addEventListener('wheel', handleWheel);
 
     return () => {
       controls.dispose();
-      gl.domElement.removeEventListener('wheel', handleWheel);
+      // gl.domElement.removeEventListener('wheel', handleWheel);
     };
-  }, [camera, gl.domElement, zoom, internalZoom, updateExternalStateDebounced]);
+  }, [camera, gl.domElement, zoom]);
 
   useFrame(() => {
     if (controlsRef.current) {
