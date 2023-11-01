2b650e306611859f589cf0fe4589711e8393ef4e
Aaron Chong <aaronchongth@gmail.com>
2023-11-01 21:06:08 +0000

    Cast to proper bool type from ttm
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-server/api_server/models/beacons.py
+++ packages/api-server/api_server/models/beacons.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 from . import tortoise_models as ttm
 from .ros_pydantic import rmf_fleet_msgs
 
@@ -5,7 +7,13 @@ from .ros_pydantic import rmf_fleet_msgs
 class BeaconState(rmf_fleet_msgs.BeaconState):
     @staticmethod
     def from_tortoise(tortoise: ttm.BeaconState) -> "BeaconState":
-        return BeaconState(**tortoise.data)
+        return BeaconState(
+            id=tortoise.id,
+            online=cast(bool, tortoise.online),
+            category=tortoise.category,
+            activated=cast(bool, tortoise.activated),
+            level=tortoise.level,
+        )
 
     async def save(self) -> None:
         await ttm.BeaconState.update_or_create(
