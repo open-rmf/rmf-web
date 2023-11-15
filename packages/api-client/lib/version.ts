03bc92a1998e4fbfbffcb9c39ac2440b8123f1b9
César Rolón <37310205+Angatupyry@users.noreply.github.com>
2023-11-15 05:37:58 -0300

    Add query filter for delivery alerts (#830)
    * Add filter query for delivery alerts
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    
    * Generated new delivery alert query API
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Only processing waiting delivery alerts
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Keeping track of just closed(cancelled) error dialogs, to not re-introduce them
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Checking if undefined, just in case alert id is undefined
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Checking if there are updated alerts with the same task id
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Cancelling old delivery alerts as new ones are generated
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Using book keeper as single source of handling db
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    ---------
    
    Signed-off-by: angatupyry <fierrofenix@gmail.com>
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    Co-authored-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: '3f5c17324194bb52c9884661dcc3b9f546194e4e',
+  rmfServer: '9c6122b74319a51ceff1ca27a493e1cbaf35aa42',
   openapiGenerator: '6.2.1',
 };
--- packages/api-server/api_server/routes/delivery_alerts.py
+++ packages/api-server/api_server/routes/delivery_alerts.py
@@ -1,10 +1,11 @@
 # pyright: reportGeneralTypeIssues=false
 
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Query
 from rx import operators as rxops
+from tortoise.exceptions import FieldError
 
 from api_server.fast_io import FastIORouter, SubscriptionRequest
 from api_server.gateway import rmf_gateway
@@ -33,6 +34,38 @@ async def get_delivery_alerts():
     ]
 
 
+@router.get("/query", response_model=List[ttm.DeliveryAlertPydantic])
+async def query_delivery_alerts(
+    category: Optional[str] = Query(
+        None, description="comma separated list of alert categories"
+    ),
+    tier: Optional[str] = Query(None, description="comma separated of tier"),
+    task_id: Optional[str] = Query(None, description="comma separated list of task_id"),
+    action: Optional[str] = Query(None, description="comma separated list of action"),
+    message: Optional[str] = Query(None, description="comma separated of message"),
+):
+    filters = {}
+    if category is not None:
+        filters["category__in"] = category.split(",")
+    if tier is not None:
+        filters["tier__in"] = tier.split(",")
+    if task_id is not None:
+        filters["task_id__in"] = task_id.split(",")
+    if action is not None:
+        filters["action__in"] = action.split(",")
+    if message is not None:
+        filters["message__in"] = message.split(",")
+
+    try:
+        delivery_alerts = await ttm.DeliveryAlert.filter(**filters)
+        return [
+            await ttm.DeliveryAlertPydantic.from_tortoise_orm(a)
+            for a in delivery_alerts
+        ]
+    except FieldError as e:
+        raise HTTPException(422, str(e)) from e
+
+
 @router.get("/{delivery_alert_id}", response_model=ttm.DeliveryAlertPydantic)
 async def get_delivery_alert(delivery_alert_id: str):
     delivery_alert = await ttm.DeliveryAlert.get_or_none(id=delivery_alert_id)
@@ -48,21 +81,60 @@ async def get_delivery_alert(delivery_alert_id: str):
 
 @router.post("", status_code=201, response_model=ttm.DeliveryAlertPydantic)
 async def create_delivery_alert(category: str, tier: str, task_id: str, message: str):
+    # # Get the previous delivery alert for this task ID
+    # prev_waiting_delivery_alert = None
+    # if len(task_id) != 0:
+    #     try:
+    #         prev_delivery_alerts = await ttm.DeliveryAlert.get_or_none(
+    #             task_id=task_id, action="waiting"
+    #         )
+    #     except FieldError as e:
+    #         raise HTTPException(422, str(e)) from e
+
+    # timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    # delivery_alert_id = f"delivery-alert-{timestamp}"
+    # try:
+    #     delivery_alert = await ttm.DeliveryAlert.create(
+    #         id=delivery_alert_id,
+    #         category=category,
+    #         tier=tier,
+    #         task_id=task_id,
+    #         action="waiting",
+    #         message=message,
+    #     )
+    # except Exception as e:
+    #     raise HTTPException(400, f"Could not create delivery alert: {e}") from e
+
+    # # After creating a new delivery alert for this task ID, if there was a
+    # # previous delivery alert, we cancel it
+    # if prev_waiting_delivery_alert is not None:
+    #     prev_waiting_delivery_alert.update_from_dict({"action": "cancelled"})
+    #     await prev_waiting_delivery_alert.save()
+
+    # Return the newly created delivery alert
+    # delivery_alert_pydantic = DeliveryAlert.from_tortoise(delivery_alert)
+
+    # Fill up the model, let the book keeper handle saving and cancelling old
+    # delivery alerts
     timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     delivery_alert_id = f"delivery-alert-{timestamp}"
-    try:
-        delivery_alert = await ttm.DeliveryAlert.create(
-            id=delivery_alert_id,
-            category=category,
-            tier=tier,
-            task_id=task_id,
-            action="waiting",
-            message=message,
-        )
-    except Exception as e:
-        raise HTTPException(400, f"Could not create delivery alert: {e}") from e
+    delivery_alert_pydantic = DeliveryAlert(
+        id=delivery_alert_id,
+        category=category,
+        tier=tier,
+        task_id=task_id,
+        action="waiting",
+        message=message,
+    )
+    # delivery_alert_pydantic = DeliveryAlert(
+    #     id=delivery_alert_id
+    #     category=category,  # pyright: ignore[reportGeneralTypeIssues]
+    #     tier=tier,  # pyright: ignore[reportGeneralTypeIssues]
+    #     task_id=task_id,  # pyright: ignore[reportGeneralTypeIssues]
+    #     action="waiting",  # pyright: ignore[reportGeneralTypeIssues]
+    #     message=message,  # pyright: ignore[reportGeneralTypeIssues]
+    # )
 
-    delivery_alert_pydantic = DeliveryAlert.from_tortoise(delivery_alert)
     rmf_events.delivery_alerts.on_next(delivery_alert_pydantic)
     return delivery_alert_pydantic
 
