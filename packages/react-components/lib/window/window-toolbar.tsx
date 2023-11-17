a90b3feb049b153b958165041b8abc59a292fd0e
angatupyry <fierrofenix@gmail.com>
2023-11-17 15:17:14 -0300

    Change appbar size when window changes its resolution
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/window/window-toolbar.tsx
+++ packages/react-components/lib/window/window-toolbar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, AppBarProps, Toolbar, Typography } from '@mui/material';
+import { AppBar, AppBarProps, Toolbar, Typography, useMediaQuery } from '@mui/material';
 import React from 'react';
 
 export interface WindowToolbarProps extends AppBarProps {
@@ -6,10 +6,20 @@ export interface WindowToolbarProps extends AppBarProps {
 }
 
 export const WindowToolbar: React.FC<WindowToolbarProps> = ({ title, children, ...otherProps }) => {
+  const isSmallerThan1000 = useMediaQuery('(max-width:1000px)');
   return (
-    <AppBar position="static" elevation={0} {...otherProps}>
+    <AppBar
+      position="static"
+      elevation={0}
+      style={{ height: isSmallerThan1000 ? 30 : 50 }}
+      {...otherProps}
+    >
       <Toolbar variant="dense" style={{ paddingRight: 0 }}>
-        <Typography variant="h5" style={{ flexGrow: 1 }}>
+        <Typography
+          fontSize={`${isSmallerThan1000 ? '1rem' : '1.7rem'}`}
+          style={{ flexGrow: 1 }}
+          mb={isSmallerThan1000 ? 2 : 0}
+        >
           {title}
         </Typography>
         {children}
