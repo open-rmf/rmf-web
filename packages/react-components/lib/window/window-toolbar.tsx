a5cdac5eafbd9ca29f55543ef52ce21f7b5f8894
angatupyry <fierrofenix@gmail.com>
2023-11-20 13:41:43 -0300

    Change toolbar if height is less than 800
    Signed-off-by: angatupyry <fierrofenix@gmail.com>

--- packages/react-components/lib/window/window-toolbar.tsx
+++ packages/react-components/lib/window/window-toolbar.tsx
@@ -6,19 +6,19 @@ export interface WindowToolbarProps extends AppBarProps {
 }
 
 export const WindowToolbar: React.FC<WindowToolbarProps> = ({ title, children, ...otherProps }) => {
-  const isSmallerThan1000 = useMediaQuery('(max-width:1000px)');
+  const isScreenHeightLessThan800 = useMediaQuery('(max-height:800px)');
   return (
     <AppBar
       position="static"
       elevation={0}
-      style={{ height: isSmallerThan1000 ? 30 : 50 }}
+      style={{ height: isScreenHeightLessThan800 ? 30 : 50 }}
       {...otherProps}
     >
       <Toolbar variant="dense" style={{ paddingRight: 0 }}>
         <Typography
-          fontSize={`${isSmallerThan1000 ? '1rem' : '1.7rem'}`}
+          fontSize={`${isScreenHeightLessThan800 ? '1rem' : '1.7rem'}`}
           style={{ flexGrow: 1 }}
-          mb={isSmallerThan1000 ? 2 : 0}
+          mb={isScreenHeightLessThan800 ? 2 : 0}
         >
           {title}
         </Typography>
