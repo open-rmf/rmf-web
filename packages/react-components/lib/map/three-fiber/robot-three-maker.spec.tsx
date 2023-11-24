10878b26b0e0cdb6361e7f0550917b38392d3824
Aaron Chong <aaronchongth@gmail.com>
2023-11-24 07:23:14 +0000

    Fix test since now we have more than 1 mesh
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/map/three-fiber/robot-three-maker.spec.tsx
+++ packages/react-components/lib/map/three-fiber/robot-three-maker.spec.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { RobotThreeMaker } from './robot-three-maker';
+import { RobotData, RobotThreeMaker } from './robot-three-maker';
 import { Euler, Vector3 } from 'three';
 import ReactThreeTestRenderer from '@react-three/test-renderer';
 
 describe('RobotThreeMaker', () => {
-  it('renders color properly', async () => {
-    const robot = {
+  it('renders robot properly', async () => {
+    const robot: RobotData = {
       fleet: 'Fleet 1',
       name: 'Robot 1',
       model: 'Model 1',
       footprint: 1.0,
       color: 'blue',
+      scale: 1.0,
     };
 
     const renderer = await ReactThreeTestRenderer.create(
@@ -19,11 +20,37 @@ describe('RobotThreeMaker', () => {
         position={new Vector3(0, 0, 0)}
         rotation={new Euler(0, 0, 0)}
         circleSegment={64}
+        robotLabel={true}
       />,
     );
 
     const mesh = renderer.scene.children[0].allChildren;
 
-    expect(mesh.length).toBe(1);
+    expect(mesh.length).toBe(3);
+  });
+
+  it('renders robot without label', async () => {
+    const robot: RobotData = {
+      fleet: 'Fleet 1',
+      name: 'Robot 1',
+      model: 'Model 1',
+      footprint: 1.0,
+      color: 'blue',
+      scale: 1.0,
+    };
+
+    const renderer = await ReactThreeTestRenderer.create(
+      <RobotThreeMaker
+        robot={robot}
+        position={new Vector3(0, 0, 0)}
+        rotation={new Euler(0, 0, 0)}
+        circleSegment={64}
+        robotLabel={false}
+      />,
+    );
+
+    const mesh = renderer.scene.children[0].allChildren;
+
+    expect(mesh.length).toBe(2);
   });
 });
