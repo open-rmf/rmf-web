1970313b2544aa6fb1ee65c9356f2eface9b174b
Aaron Chong <aaronchongth@gmail.com>
2023-11-27 07:53:50 +0000

    Regenerating api-client
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: 'd6099b09fb9f13e06e029014f35a8ff1c4810abf',
+  rmfServer: '3a67ed6377eef8d31c181986e67618fa7c5dec70',
   openapiGenerator: '6.2.1',
 };
