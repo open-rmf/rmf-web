c60fee337a06ce61038949d9141018292bf67a4e
Aaron Chong <aaronchongth@gmail.com>
2023-11-28 13:41:08 +0000

    Revert test
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/tasks/task-schedule-event-edit-delete-popup.spec.tsx
+++ packages/react-components/lib/tasks/task-schedule-event-edit-delete-popup.spec.tsx
@@ -25,22 +25,4 @@ describe('EventEditDeletePopup', () => {
     // Check if the onChange function was called with the correct value
     expect(onChange).toHaveBeenCalled();
   });
-
-  it('all events change disabled for non-admin', () => {
-    const { getByLabelText } = render(
-      <EventEditDeletePopup
-        currentValue={currentValue}
-        allValue={allValue}
-        value={value}
-        isAdmin={false}
-        onChange={onChange}
-      />,
-    );
-
-    // Simulate a change event by clicking on a radio button
-    fireEvent.click(getByLabelText('All events in this schedule'));
-
-    // Check that onChange was not called since the button is disabled
-    expect(onChange).not.toHaveBeenCalled();
-  });
 });
