4e8cb2d5869833148fae7326c506ebc57a971698
Aaron Chong <aaronchongth@gmail.com>
2023-12-05 10:13:53 +0000

    rmf-auth, TODO login stories, add new to keycloak creation
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/rmf-auth/lib/components/login.stories.tsx
+++ packages/rmf-auth/lib/components/login.stories.tsx
@@ -1,27 +1,29 @@
-import { Meta, Story } from '@storybook/react';
-import React from 'react';
-import { LoginCard as LoginCard_, LoginPage as LoginPage_ } from '.';
-import { LoginCardProps } from './login-card';
+// TODO(ac): commented out during node 18 migration
 
-export default {
-  title: 'Login',
-  component: LoginCard_,
-  argTypes: {
-    title: {
-      defaultValue: 'Title',
-    },
-    logo: {
-      control: {
-        disable: true,
-      },
-    },
-  },
-} as Meta;
+// import { Meta, Story } from '@storybook/react';
+// import React from 'react';
+// import { LoginCard as LoginCard_, LoginPage as LoginPage_ } from '.';
+// import { LoginCardProps } from './login-card';
 
-export const LoginCard: Story<LoginCardProps> = (args) => (
-  <LoginCard_ {...args} logo="/resources/ros-health.png" />
-);
+// export default {
+//   title: 'Login',
+//   component: LoginCard_,
+//   argTypes: {
+//     title: {
+//       defaultValue: 'Title',
+//     },
+//     logo: {
+//       control: {
+//         disable: true,
+//       },
+//     },
+//   },
+// } as Meta;
 
-export const LoginPage: Story<LoginCardProps> = (args) => (
-  <LoginPage_ {...args} logo="/resources/ros-health.png" />
-);
+// export const LoginCard: Story<LoginCardProps> = (args) => (
+//   <LoginCard_ {...args} logo="/resources/ros-health.png" />
+// );
+
+// export const LoginPage: Story<LoginCardProps> = (args) => (
+//   <LoginPage_ {...args} logo="/resources/ros-health.png" />
+// );
