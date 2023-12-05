9ea3f915cf30be8230ff15e0debf158e003ecbdc
Aaron Chong <aaronchongth@gmail.com>
2023-12-05 09:00:07 +0000

    Remove dashboard leaflet overlays
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard/src/components/trajectories-overlay.tsx
+++ packages/dashboard/src/components/trajectories-overlay.tsx
@@ -1,34 +1,12 @@
-import React from 'react';
-import {
-  SVGOverlay,
-  SVGOverlayProps,
-  TrajectoryMarker as TrajectoryMarker_,
-  TrajectoryMarkerProps,
-  viewBoxFromLeafletBounds,
-} from 'react-components';
+import { Trajectory } from 'react-components';
 
-const TrajectoryMarker = React.memo(TrajectoryMarker_);
-
-export type TrajectoryData = TrajectoryMarkerProps;
-
-export interface TrajectoriesOverlayProps extends Omit<SVGOverlayProps, 'viewBox'> {
-  trajectoriesData: TrajectoryData[];
+export interface TrajectoryData {
+  trajectory: Trajectory;
+  color: string;
+  conflict?: boolean;
+  loopAnimation?: boolean;
+  /**
+   * default: 1
+   */
+  animationScale?: number;
 }
-
-export const TrajectoriesOverlay = React.memo(
-  ({ trajectoriesData, ...otherProps }: TrajectoriesOverlayProps): JSX.Element => {
-    const viewBox = viewBoxFromLeafletBounds(otherProps.bounds);
-
-    return (
-      <SVGOverlay viewBox={viewBox} {...otherProps}>
-        {trajectoriesData.map((trajData) => (
-          <TrajectoryMarker
-            key={trajData.trajectory.id}
-            aria-label={`trajectory ${trajData.trajectory.id}`}
-            {...trajData}
-          />
-        ))}
-      </SVGOverlay>
-    );
-  },
-);
