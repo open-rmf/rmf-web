bdc538a48c340906eb67cbdcfe293074d58619e8
Aaron Chong <aaronchongth@gmail.com>
2023-12-11 14:11:18 +0800

    Revert naming of database name
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-server/psql_local_config.py
+++ packages/api-server/psql_local_config.py
@@ -7,7 +7,7 @@ run_dir = f"{here}/run"
 
 config.update(
     {
-        "db_url": "postgres://postgres:postgres@127.0.0.1:5432/rmf-web-rmf-server",
+        "db_url": "postgres://postgres:postgres@127.0.0.1:5432",
         "cache_directory": f"{run_dir}/cache",  # The directory where cached files should be stored.
     }
 )
