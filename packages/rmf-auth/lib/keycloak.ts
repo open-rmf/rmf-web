0aa5d0193bb21e55764209c5c8d1f1afb1687799
Aaron Chong <aaronchongth@gmail.com>
2023-12-14 16:20:35 +0000

    Logging in rmf-auth
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/rmf-auth/lib/keycloak.ts
+++ packages/rmf-auth/lib/keycloak.ts
@@ -55,19 +55,23 @@ export class KeycloakAuthenticator
   async init(): Promise<void> {
     if (this._initialized) {
       debug('already initialized');
+      console.log('already initialized');
       return;
     }
 
     debug('initializing authenticator');
+    console.log('initializing authenticator');
 
     this._inst.onAuthSuccess = async () => {
       this._user = this._getUser();
       debug('authenticated as', this._user);
+      console.log('authenticated as', this._user);
       this.emit('userChanged', this._user);
     };
 
     this._inst.onAuthLogout = () => {
       debug('logout');
+      console.log('logout');
       this._user = undefined;
       this.emit('userChanged', null);
     };
@@ -79,8 +83,10 @@ export class KeycloakAuthenticator
     try {
       const refreshed = await this._inst.updateToken(30);
       refreshed && debug('token refreshed');
+      console.log('token refreshed 30');
     } catch {
       debug('token not refreshed');
+      console.log('token not refreshed 30');
     }
 
     this._user = this._inst.tokenParsed && this._getUser();
@@ -97,8 +103,10 @@ export class KeycloakAuthenticator
         this._user = this._getUser();
         this._isAdmin = this._isUserAdmin();
         this.emit('tokenRefresh', null);
+        console.log('token refreshed 5');
       } else {
-        debug('token not refreshed');
+        console.error('token not refreshed');
+        // debug('token not refreshed');
       }
     }
     return;
