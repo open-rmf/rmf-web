6668e4e0a8cc54cacd913bd3f767622fe73e8867
Aaron Chong <aaronchongth@gmail.com>
2023-12-29 06:59:15 +0000

    Change API call for newer concurrently version
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard-e2e/scripts/test-e2e.js
+++ packages/dashboard-e2e/scripts/test-e2e.js
@@ -13,11 +13,16 @@ const wdioArgs = process.argv
   .map((arg) => `"${arg}"`)
   .join(' ');
 
-concurrently([...services, `wdio ${wdioArgs}`], {
+const { result } = concurrently([...services, `wdio ${wdioArgs}`], {
   killOthers: ['success', 'failure'],
   successCondition: 'first',
   prefix: 'none',
-}).catch((e) => {
-  console.error(e);
-  process.exitCode = -1;
 });
+
+result.then(
+  () => {},
+  () => {
+    console.error('End-to-end test failed.');
+    process.exitCode = -1;
+  },
+);
