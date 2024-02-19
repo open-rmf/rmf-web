92db3cd19cb545f26be058b8f18b74ed8481b582
Aaron Chong <aaronchongth@gmail.com>
2024-02-19 09:52:16 +0800

    Wrapping map image loading in try-catch, to prevent dashboard crashing when building-map-server is down
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/react-components/lib/map/three-fiber/image-maker.tsx
+++ packages/react-components/lib/map/three-fiber/image-maker.tsx
@@ -9,27 +9,32 @@ export interface ImageThreeProps {
 }
 
 export const ReactThreeFiberImageMaker = ({ level, imageUrl }: ImageThreeProps): JSX.Element => {
-  const texture: Texture | undefined = useLoader(TextureLoader, imageUrl, undefined, (err) => {
-    console.error(`Error loading image from ${imageUrl}:`, err);
-  });
+  try {
+    const texture: Texture | undefined = useLoader(TextureLoader, imageUrl, undefined, (err) => {
+      console.error(`Error loading image from ${imageUrl}:`, err);
+    });
 
-  if (!texture) {
+    if (!texture) {
+      console.error(`Failed to create image texture with ${imageUrl}.`);
+      return <></>;
+    }
+
+    const image = level.images[0];
+    const scale = image.scale;
+    const x_offset = (texture.image.width * scale) / 2 + image.x_offset;
+    const y_offset = -(texture.image.height * scale) / 2 + image.y_offset;
+
+    return (
+      <mesh position={[x_offset, y_offset, 0]}>
+        <planeGeometry
+          attach="geometry"
+          args={[texture.image.width * scale, texture.image.height * scale]}
+        />
+        <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
+      </mesh>
+    );
+  } catch (e) {
     console.error(`Failed to create image texture with ${imageUrl}.`);
     return <></>;
   }
-
-  const image = level.images[0];
-  const scale = image.scale;
-  const x_offset = (texture.image.width * scale) / 2 + image.x_offset;
-  const y_offset = -(texture.image.height * scale) / 2 + image.y_offset;
-
-  return (
-    <mesh position={[x_offset, y_offset, 0]}>
-      <planeGeometry
-        attach="geometry"
-        args={[texture.image.width * scale, texture.image.height * scale]}
-      />
-      <meshBasicMaterial attach="material" map={texture} toneMapped={false} />
-    </mesh>
-  );
 };
