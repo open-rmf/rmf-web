a0e69c7bee70ce3feaa12a36f59402f4766e9297
Aaron Chong <aaronchongth@gmail.com>
2024-04-15 21:04:33 +0800

    Generated openapi
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-client/lib/version.ts
+++ packages/api-client/lib/version.ts
@@ -3,6 +3,6 @@ import { version as rmfModelVer } from 'rmf-models';
 
 export const version = {
   rmfModels: rmfModelVer,
-  rmfServer: 'b33a07c7af58cfc62924ebeb12d513948ac9665b',
+  rmfServer: '9f16396b53bbc1327493cf67f0cd93fa9de503fa',
   openapiGenerator: '6.2.1',
 };
