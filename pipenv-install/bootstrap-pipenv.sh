0224578dfb76eb12e2c8eb138f1782a4e6cffbf8
Teo Koon Peng <teokoonpeng@gmail.com>
2024-06-26 05:38:37 +0000

    force install pipenv even if it already exist on the system
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- pipenv-install/bootstrap-pipenv.sh
+++ pipenv-install/bootstrap-pipenv.sh
@@ -6,5 +6,5 @@ cd "$(dirname "$0")/.."
 if [ ! -d .venv ]; then
   echo 'creating virtualenv at .venv'
   python3 -m venv .venv --system-site-packages --prompt rmf-web
-  .venv/bin/pip3 install pipenv
+  .venv/bin/pip3 install pipenv --ignore-installed
 fi
