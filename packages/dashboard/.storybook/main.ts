48d31d215dfe25203bc57b3fca94f3d6cbe1014b
Aaron Chong <aaronchongth@gmail.com>
2024-07-16 08:39:34 +0000

    Migrate dashboard deps, configs, scripts, storybook
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

---
+++ packages/dashboard/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-babel',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
