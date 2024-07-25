042a1137e3fbcabe5885cd39df60770bc69d90a6
Teo Koon Peng <teokoonpeng@gmail.com>
2024-07-25 07:29:50 +0000

    more cleanup
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- packages/dashboard/src/components/tests/mock-app-controller.ts
+++
@@ -1,9 +0,0 @@
-import { AppController } from '../app-contexts';
-
-export function makeMockAppController(): AppController {
-  return {
-    updateSettings: jest.fn(),
-    showAlert: jest.fn(),
-    setExtraAppbarIcons: jest.fn(),
-  };
-}
