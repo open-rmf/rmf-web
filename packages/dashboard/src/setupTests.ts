fe74cc9404c686a0a36791331be39771ee890851
Teo Koon Peng <teokoonpeng@gmail.com>
2024-07-25 06:32:01 +0000

    more cleanup
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- packages/dashboard/src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-
-const env = import.meta.env as any;
-env.VITE_TRAJECTORY_SERVER = 'http://localhost';
-env.VITE_AUTH_PROVIDER = '';
-env.VITE_KEYCLOAK_CONFIG = '';
-env.VITE_RMF_SERVER = 'http://localhost';
-env.PUBLIC_URL = 'http://localhost';
