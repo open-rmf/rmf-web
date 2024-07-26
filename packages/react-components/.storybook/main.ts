da7431fa1cae787b7d2bb8e22f445819515cdb71
Teo Koon Peng <teokoonpeng@gmail.com>
2024-07-26 06:28:28 +0000

    migrate storybook to vite
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- packages/react-components/.storybook/main.ts
+++ packages/react-components/.storybook/main.ts
@@ -1,18 +1,14 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
+  framework: '@storybook/react-vite',
   docs: {
     autodocs: 'tag',
   },
