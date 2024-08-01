682936507944600b6032e100da900e034e75471d
Aaron Chong <aaronchongth@gmail.com>
2024-08-01 14:12:08 +0800

    Ac/clean up and fixes (#981)
    * Remove beacons micro-app as default
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    * Fix custom compose failing check due to empty string
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>
    
    ---------
    
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard/src/components/robots/robots-workspace.tsx
+++ packages/dashboard/src/components/robots/robots-workspace.tsx
@@ -2,19 +2,17 @@ import { WorkspaceState } from '../workspace';
 
 export const robotsWorkspace: WorkspaceState = {
   layout: [
-    { i: 'robots', x: 0, y: 0, w: 7, h: 3 },
-    { i: 'map', x: 8, y: 0, w: 5, h: 9 },
-    { i: 'doors', x: 0, y: 0, w: 7, h: 3 },
-    { i: 'lifts', x: 0, y: 0, w: 7, h: 3 },
-    { i: 'beacons', x: 0, y: 0, w: 7, h: 3 },
-    { i: 'mutexGroups', x: 8, y: 0, w: 5, h: 3 },
+    { i: 'robots', x: 0, y: 0, w: 7, h: 4 },
+    { i: 'map', x: 8, y: 0, w: 5, h: 8 },
+    { i: 'doors', x: 0, y: 0, w: 7, h: 4 },
+    { i: 'lifts', x: 0, y: 0, w: 7, h: 4 },
+    { i: 'mutexGroups', x: 8, y: 0, w: 5, h: 4 },
   ],
   windows: [
     { key: 'robots', appName: 'Robots' },
     { key: 'map', appName: 'Map' },
     { key: 'doors', appName: 'Doors' },
     { key: 'lifts', appName: 'Lifts' },
-    { key: 'beacons', appName: 'Beacons' },
     { key: 'mutexGroups', appName: 'Mutex Groups' },
   ],
 };
