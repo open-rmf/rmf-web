5386df01c9b0db64193ce6234d09807fcd5f2917
Aaron Chong <aaronchongth@gmail.com>
2024-08-16 04:23:35 +0000

    Use configurable configs, inject envs before start
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- .github/dashboard/99-inject-env.sh
+++ .github/dashboard/99-inject-env.sh
@@ -3,4 +3,3 @@ set -e
 
 sed -i "s,__RMF_SERVER_URL__,${RMF_SERVER_URL},g" /opt/dashboard/index.html
 sed -i "s,__TRAJECTORY_SERVER_URL__,${TRAJECTORY_SERVER_URL},g" /opt/dashboard/index.html
-sed -i "s,__KEYCLOAK_URL__,${KEYCLOAK_URL},g" /opt/dashboard/index.html
