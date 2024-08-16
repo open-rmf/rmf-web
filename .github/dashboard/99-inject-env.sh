7dafc12a40968a824a8a641adea4879e01a85bab
Aaron Chong <aaronchongth@gmail.com>
2024-08-16 03:54:11 +0000

    Inject env script
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

---
+++ .github/dashboard/99-inject-env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+set -e
+
+sed -i "s,__RMF_SERVER_URL__,${RMF_SERVER_URL},g" /opt/dashboard/index.html
+sed -i "s,__TRAJECTORY_SERVER_URL__,${TRAJECTORY_SERVER_URL},g" /opt/dashboard/index.html
+sed -i "s,__KEYCLOAK_URL__,${KEYCLOAK_URL},g" /opt/dashboard/index.html
