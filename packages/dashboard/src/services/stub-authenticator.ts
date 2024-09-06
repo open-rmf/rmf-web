78cbccdf2dd52c103956539a51ec236042c91eb3
Teo Koon Peng <teokoonpeng@gmail.com>
2024-09-06 03:39:14 +0000

    comments
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- packages/dashboard/src/services/stub-authenticator.ts
+++ packages/dashboard/src/services/stub-authenticator.ts
@@ -13,7 +13,21 @@ export class StubAuthenticator
   constructor() {
     super();
     this.user = 'stub';
-    // hardcoded token that expires in 2035-01-01
+    // hardcoded token using the secret 'rmfisawesome', expires in 2035-01-01
+    // header:
+    // {
+    //   "alg": "HS256",
+    //   "typ": "JWT"
+    // }
+    // payload:
+    // {
+    //   "sub": "stub",
+    //   "preferred_username": "stub",
+    //   "iat": 1516239022,
+    //   "aud": "rmf_api_server",
+    //   "iss": "stub",
+    //   "exp": 2051222400
+    // }
     this.token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHViIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic3R1YiIsImlhdCI6MTUxNjIzOTAyMiwiYXVkIjoicm1mX2FwaV9zZXJ2ZXIiLCJpc3MiOiJzdHViIiwiZXhwIjoyMDUxMjIyNDAwfQ.dN7xOpbeN2A4QXM8Mmc2ZzWqC8w1XNm8IYsJ0FdeKCc';
   }
