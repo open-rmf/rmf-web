6d7383ab73cd07d6894f92703afa61cc0ba6f412
Aaron Chong <aaronchongth@gmail.com>
2024-10-07 16:54:50 +0800

    Check response error fields
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/dashboard/src/services/rmf-api.ts
+++ packages/dashboard/src/services/rmf-api.ts
@@ -122,7 +122,7 @@ export class DefaultRmfApi implements RmfApi {
     axiosInst.interceptors.response.use(
       (response) => response,
       (error) => {
-        if (error.response.status === 401) {
+        if (error && error.response && error.response.status === 401) {
           window.location.href = '/';
         }
       },
