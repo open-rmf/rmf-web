e78909dafcaef6f5e0928909a94e066b97460390
Aaron Chong <aaronchongth@gmail.com>
2024-11-27 13:12:10 +0800

    camera-control
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/rmf-dashboard-framework/src/components/map/map.test.tsx
+++ packages/rmf-dashboard-framework/src/components/map/map.test.tsx
@@ -1,5 +1,4 @@
-import { render } from '@testing-library/react';
-import { waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import React, { act } from 'react';
 import { describe, expect, it } from 'vitest';
 
