6f9f9e39b210613973407497f296788e0c15673a
Aaron Chong <aaronchongth@gmail.com>
2026-02-20 16:30:12 +0800

    use pushd popd
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/ros-translator/ros_translator/pydantic/test/test.sh
+++ packages/ros-translator/ros_translator/pydantic/test/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
+pushd $(dirname $0)
 rm -rf out
 python -m ros_translator -t=pydantic -o=out ros_translator_test_msgs
 echo 'test build'
 echo 'ok'
 pnpm exec pyright ros_translator/pydantic/test/out
+popd
--- packages/ros-translator/ros_translator/typescript/test/test.sh
+++ packages/ros-translator/ros_translator/typescript/test/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
+pushd $(dirname $0)
 rm -rf out
 python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
 echo 'test build'
 tsc --noEmit
 echo 'ok'
 ts-node -T ../../../node_modules/jasmine/bin/jasmine.js "$@"
+popd
