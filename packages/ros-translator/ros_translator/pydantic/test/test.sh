47d4e9efc05fe3ed0a05fd5aa9e45c56e5ce31d4
Aaron Chong <aaronchongth@gmail.com>
2026-02-20 16:49:15 +0800

    use out directly without path
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/ros-translator/ros_translator/pydantic/test/test.sh
+++ packages/ros-translator/ros_translator/pydantic/test/test.sh
@@ -2,10 +2,8 @@
 set -e
 
 cd $(dirname $0)
-realpath .
 rm -rf out
 python -m ros_translator -t=pydantic -o=out ros_translator_test_msgs
 echo 'test build'
 echo 'ok'
-realpath .
-pnpm exec pyright ros_translator/pydantic/test/out
+pnpm exec pyright out
--- packages/ros-translator/ros_translator/typescript/test/test.sh
+++ packages/ros-translator/ros_translator/typescript/test/test.sh
@@ -2,11 +2,9 @@
 set -e
 
 cd $(dirname $0)
-realpath .
 rm -rf out
 python -m ros_translator -t=typescript -o=out ros_translator_test_msgs
 echo 'test build'
 tsc --noEmit
 echo 'ok'
-realpath .
 ts-node -T ../../../node_modules/jasmine/bin/jasmine.js "$@"
