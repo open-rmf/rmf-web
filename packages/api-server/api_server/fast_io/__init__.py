0365b43e96dbe725ae6709a9b0f331493e8ac0c8
Aaron Chong <aaronchongth@gmail.com>
2026-08-10 16:09:32 +0800

    removed deprecated pylintrc suggestion-mode, and adding doc and pylint ignore for runtime initialized route description
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- packages/api-server/api_server/fast_io/__init__.py
+++ packages/api-server/api_server/fast_io/__init__.py
@@ -225,9 +225,10 @@ The message must be of the form:
 {docstring.strip()}
 {response_schema}
 """
-            self._sio_route.description += (
-                room_descriptions  # pylint: disable=no-member
-            )
+            # Route's description is initialized at runtime, so it doesn't exist
+            # at the time of linting
+            # pylint: disable=no-member
+            self._sio_route.description += room_descriptions
 
     @staticmethod
     def _parse_sub_data(data: dict) -> SubscriptionData:
